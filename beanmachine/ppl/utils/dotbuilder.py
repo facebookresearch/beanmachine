d046be4ef276a566f6cad523dfe323762d35dd1c
Eric Lippert <ericlippert@fb.com>
2019-09-26 10:40:56 -0700

    Add a helper class for visualizing Python objects as graphs
    Summary:
    I've added a class that will help us visualize graphs of objects for debugging or demo purposes.
    
    We have a mutable `DotBuilder` class which allows for imperatively adding nodes, edges and subgraphs; the result is then a string in the DOT language, which can be turned into a graphic with `dot`, or `webgraphviz.com` or `https://our.intern.facebook.com/intern/graphviz/`.
    
    I've also added a helper function for simple cases; `print_graph` takes a list of roots and functions for producing the outgoing labeled edges, node identifiers and node labels, and produces a graph.
    
    For example:
    
            bar = {"blah": [2, 3, {"abc": (6, 7, (5, 5, 6))}]}
            d = {"foo": 2, "bar1": bar, "bar2": bar}
            d["self"] = d
            print_graph([d])
    
    produces the graph below:
    
    {F210813866}
    
    Reviewed By: janewangfb
    
    Differential Revision: D17509283
    
    fbshipit-source-id: 364e7f79320ba2ddea5c82b658c83ce23e1a9619

---
+++ beanmachine/ppl/utils/dotbuilder.py
@@ -0,0 +1,242 @@
+#!/usr/bin/env python3
+"""A builder for the graphviz DOT language"""
+import json
+import re
+from typing import Any, Callable, Dict, List, Set, Tuple
+
+from beanmachine.ppl.utils.treeprinter import _is_named_tuple, _to_string
+
+
+def _get_children(n: Any) -> List[Tuple[str, Any]]:
+    if isinstance(n, dict):
+        return list(n.items())
+    if _is_named_tuple(n):
+        return [(k, getattr(n, k)) for k in type(n)._fields]
+    if isinstance(n, tuple) or isinstance(n, list):
+        return [(str(ind), item) for (ind, item) in enumerate(n)]
+    return []
+
+
+def print_graph(
+    roots: List[Any],
+    get_children: Callable[[Any], List[Tuple[str, Any]]] = _get_children,
+    to_node_name: Callable[[Any], str] = None,
+    to_label: Callable[[Any], str] = _to_string,
+) -> str:
+    """
+This function converts an object representing a graph into a string
+in the DOT graph display language.
+
+The roots are a set of nodes in the graph; the final graph description will
+contain the transitive closure of the children of all roots.
+
+get_children returns a list of (edge_label, node) pairs; if no argument
+is supplied then a default function that can handle lists, tuples and
+dictionaries is used.
+
+to_node_name returns a *unique* string used to identify the node in the
+graph; if no argument is supplied then we use Python's id() for object
+identity.
+
+to_label gives a *not necessarily unique* label for a node in a graph.
+Again if not supplied, a default that can handle dictionaries, lists and
+tuples is used.
+    """
+
+    def _to_node_name(node: Any) -> str:
+        n = str(id(node))
+        if n not in map:
+            map[n] = "N" + str(len(map))
+        return map[n]
+
+    if to_node_name is None:
+        to_node_name = _to_node_name
+    builder: DotBuilder = DotBuilder()
+    stack: List[Any] = []
+    stack.extend(roots)
+    done: Set[str] = set()
+    map: Dict[str, str] = {}
+    for root in roots:
+        builder.with_node(to_node_name(root), to_label(root))
+    while len(stack) > 0:
+        current = stack.pop()
+        current_node = to_node_name(current)
+        if current_node not in done:
+            for (edge_label, child) in get_children(current):
+                child_node = to_node_name(child)
+                builder.with_node(child_node, to_label(child))
+                builder.with_edge(current_node, child_node, edge_label)
+                stack.append(child)
+            done.add(current_node)
+    return str(builder)
+
+
+class DotBuilder:
+    def __init__(
+        self, name: str = "graph", is_subgraph: bool = False, is_cluster: bool = False
+    ):
+        self.name = name
+        self.is_subgraph = is_subgraph
+        self.is_cluster = is_cluster
+        self._label = ""
+        self._node_map = {}
+        self._edges = set()
+        self._comments = []
+        self._subgraphs = []
+        self._nodes = []
+        self._current_subgraph = None
+
+    def with_label(self, label: str):
+        if self._current_subgraph is None:
+            self._label = label
+        else:
+            self._current_subgraph.with_label(label)
+        return self
+
+    def start_subgraph(self, name: str, is_cluster: bool):
+        if self._current_subgraph is None:
+            self._current_subgraph = DotBuilder(name, True, is_cluster)
+            self._subgraphs.append(self._current_subgraph)
+        else:
+            self._current_subgraph.start_subgraph(name, is_cluster)
+        return self
+
+    def end_subgraph(self):
+        if self._current_subgraph is None:
+            raise ValueError("Cannot end a non-existing subgraph.")
+        if self._current_subgraph._current_subgraph is None:
+            self._current_subgraph = None
+        else:
+            self._current_subgraph.end_subgraph()
+        return self
+
+    def _get_node(self, name: str):
+        if name in self._node_map:
+            return self._node_map[name]
+        new_node = DotNode(name, "", "")
+        self._node_map[name] = new_node
+        self._nodes.append(new_node)
+        return new_node
+
+    def with_comment(self, comment: str):
+        if self._current_subgraph is None:
+            self._comments.append(comment)
+        else:
+            self._current_subgraph.with_comment(comment)
+        return self
+
+    def with_node(self, name: str, label: str, color: str = ""):
+        if self._current_subgraph is None:
+            n = self._get_node(name)
+            n.label = label
+            n.color = color
+        else:
+            self._current_subgraph.with_node(name, label, color)
+        return self
+
+    def with_edge(
+        self,
+        frm: str,
+        to: str,
+        label: str = "",
+        color: str = "",
+        constrained: bool = True,
+    ):
+        if self._current_subgraph is None:
+            f = self._get_node(frm)
+            t = self._get_node(to)
+            self._edges.add(DotEdge(f, t, label, color, constrained))
+        else:
+            self._current_subgraph.with_edge(frm, to, label, color, constrained)
+        return self
+
+    def _to_string(self, indent: str, sb: List[str]):
+        new_indent = indent + "  "
+        sb.append(indent)
+        sb.append("subgraph" if self.is_subgraph else "digraph")
+        i = ""
+        has_name = len(self.name) > 0
+        if has_name and self.is_cluster:
+            i = smart_quote("cluster_" + self.name)
+        elif has_name:
+            i = smart_quote(self.name)
+        elif self.is_cluster:
+            i = "cluster"
+        if len(i) > 0:
+            sb.append(" " + i)
+        sb.append(" {\n")
+        for c in self._comments:
+            sb.append(new_indent + "// " + c + "\n")
+        if len(self._label) > 0:
+            sb.append(new_indent + "label=" + smart_quote(self._label) + "\n")
+        for n in self._nodes:
+            sb.append(new_indent + str(n) + "\n")
+        edges = [new_indent + str(e) + "\n" for e in self._edges]
+        edges.sort()
+        sb.extend(edges)
+        for db in self._subgraphs:
+            sb = db._to_string(new_indent, sb)
+        sb.append(indent + "}\n")
+        return sb
+
+    def __str__(self):
+        return "".join(self._to_string("", []))
+
+
+class DotNode:
+    def __init__(self, name: str, label: str, color: str):
+        self.name = name
+        self.label = label
+        self.color = color
+
+    def __str__(self):
+        props = []
+        if len(self.label) != 0:
+            props.append("label=" + smart_quote(self.label))
+        if len(self.color) != 0:
+            props.append("color=" + smart_quote(self.label))
+        p = "" if len(props) == 0 else "[" + " ".join(props) + "]"
+        return smart_quote(self.name) + p + ";"
+
+
+class DotEdge:
+    def __init__(
+        self, frm: DotNode, to: DotNode, label: str, color: str, constrained: bool
+    ):
+        self.frm = frm
+        self.to = to
+        self.label = label
+        self.color = color
+        self.constrained = constrained
+
+    def __str__(self):
+        props = []
+        if len(self.label) != 0:
+            props.append("label=" + smart_quote(self.label))
+        if len(self.color) != 0:
+            props.append("color=" + smart_quote(self.label))
+        if not self.constrained:
+            props.append("constraint=false")
+        p = "" if len(props) == 0 else "[" + " ".join(props) + "]"
+        return smart_quote(self.frm.name) + " -> " + smart_quote(self.to.name) + p + ";"
+
+
+_keywords: List[str] = ["digraph", "edge", "graph", "node", "strict", "subgraph"]
+_alphanum = re.compile("^[A-Za-z_][A-Za-z_0-9]*$")
+_numeric = re.compile("^[-]?(\\.[0-9]+|[0-9]+(\\.[0-9]*)?)$")
+
+
+def smart_quote(s: str) -> str:
+    if s is None or len(s) == 0:
+        return '""'
+    if s.lower() in _keywords:
+        return json.dumps(s)
+    if _alphanum.match(s):
+        return s
+    if _numeric.match(s):
+        return s
+    return json.dumps(s)
+
+
+if __name__ == "__main__":
+    print("dotbuilder.py")
---
+++ beanmachine/ppl/utils/dotbuilder_test.py
@@ -0,0 +1,45 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+"""Tests for print_graph from dotbuilder.py"""
+import unittest
+
+from beanmachine.ppl.utils.dotbuilder import print_graph
+
+
+class GraphPrinterTest(unittest.TestCase):
+    def test_print_tree(self) -> None:
+        """Tests for print_graph from dotbuilder.py"""
+        bar = {"blah": [2, 3, {"abc": (6, 7, (5, 5, 6))}]}
+        d = {"foo": 2, "bar1": bar, "bar2": bar}
+        d["self"] = d
+        observed = print_graph([d])
+        expected = """
+digraph "graph" {
+  N0[label=dict];
+  N1[label=2];
+  N2[label=dict];
+  N3[label=list];
+  N4[label=3];
+  N5[label=dict];
+  N6[label=tuple];
+  N7[label=6];
+  N8[label=7];
+  N9[label=tuple];
+  N10[label=5];
+  N0 -> N0[label=self];
+  N0 -> N1[label=foo];
+  N0 -> N2[label=bar1];
+  N0 -> N2[label=bar2];
+  N2 -> N3[label=blah];
+  N3 -> N1[label=0];
+  N3 -> N4[label=1];
+  N3 -> N5[label=2];
+  N5 -> N6[label=abc];
+  N6 -> N7[label=0];
+  N6 -> N8[label=1];
+  N6 -> N9[label=2];
+  N9 -> N10[label=0];
+  N9 -> N10[label=1];
+  N9 -> N7[label=2];
+}
+"""
+        self.assertEqual(observed.strip(), expected.strip())
