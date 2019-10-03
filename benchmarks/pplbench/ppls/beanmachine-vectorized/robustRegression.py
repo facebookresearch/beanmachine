e4d0db1af05abc2d2ec8c9cd9e50849eaa4b011d
Kinjal Shah <kshah97@fb.com>
2019-10-03 11:01:26 -0700

    Add vectorized bean machine implementation of robust regression
    Summary: Added a vectorized implementation of robust regression in bean machine in PPLBench.
    
    Reviewed By: nimar
    
    Differential Revision: D17643656
    
    fbshipit-source-id: 1628fcff3c7f72150d5538f4ad66ef6b5d86fda6

---
+++ benchmarks/pplbench/ppls/beanmachine-vectorized/robustRegression.py
@@ -0,0 +1,143 @@
+import time
+from typing import Any, Dict, List, Tuple
+
+import torch
+import torch.distributions as dist
+import torch.tensor as tensor
+from beanmachine.ppl.inference.single_site_ancestral_mh import (
+    SingleSiteAncestralMetropolisHastings,
+)
+from beanmachine.ppl.model.statistical_model import sample
+from torch import Tensor
+
+
+"""
+For model definition, see models/robustRegressionModel.py
+"""
+
+
+class RobustRegressionModel(object):
+    def __init__(
+        self,
+        N: int,
+        K: int,
+        scale_alpha: int,
+        scale_beta: List[int],
+        loc_sigma: int,
+        num_samples: int,
+        inference_type: str,
+        X: Tensor,
+        Y: Tensor,
+    ):
+        self.N = N
+        self.K = K
+        self.scale_alpha = scale_alpha
+        self.scale_beta = scale_beta
+        self.loc_sigma = loc_sigma
+        self.num_samples = num_samples
+        self.inference_type = inference_type
+        self.X = X
+        self.Y = Y
+
+    @sample
+    def nu(self):
+        return dist.Gamma(2.0, 0.1)
+
+    @sample
+    def sigma(self):
+        return dist.Exponential(self.loc_sigma)
+
+    @sample
+    def alpha(self):
+        return dist.Normal(0, self.scale_alpha)
+
+    @sample
+    def beta(self):
+        return dist.Normal(
+            tensor([0.0] * self.K).view(self.K, 1),
+            tensor(self.scale_beta).view(self.K, 1),
+        )
+
+    @sample
+    def y(self, i):
+        # Compute X * Beta
+        mu = self.alpha() + torch.FloatTensor(self.X[:, i]).view(1, self.K).mm(
+            self.beta()
+        )
+        return dist.StudentT(self.nu(), mu, self.sigma())
+
+    def infer(self):
+        dict_y = {self.y(i): self.Y[i] for i in range(self.N)}
+        if self.inference_type == "mcmc":
+            mh = SingleSiteAncestralMetropolisHastings()
+            start_time = time.time()
+            samples = mh.infer(
+                [self.beta(), self.nu(), self.sigma(), self.alpha()],
+                dict_y,
+                self.num_samples,
+            )
+        elif self.inference_type == "vi":
+            print("ImplementationError; exiting...")
+            exit(1)
+        elapsed_time_sample_beanmachine = time.time() - start_time
+        return (samples, elapsed_time_sample_beanmachine)
+
+
+def obtain_posterior(
+    data_train: Tuple[Any, Any], args_dict: Dict, model: RobustRegressionModel
+) -> Tuple[List, Dict]:
+    """
+    Beanmachine impmementation of robust regression model.
+
+    :param data_train: tuple of np.ndarray (x_train, y_train)
+    :param args_dict: a dict of model arguments
+    :returns: samples_beanmachine(dict): posterior samples of all parameters
+    :returns: timing_info(dict): compile_time, inference_time
+    """
+    # shape of x_train: (num_features, num_samples)
+    x_train, y_train = data_train
+    y_train = [tensor(y) for y in y_train]
+    N = int(x_train.shape[1])
+    K = int(x_train.shape[0])
+
+    alpha_scale = (args_dict["model_args"])[0]
+    beta_scale = [(args_dict["model_args"])[1]] * K
+    sigma_loc = (args_dict["model_args"])[3]
+    num_samples = args_dict["num_samples_beanmachine-vectorized"]
+    inference_type = args_dict["inference_type"]
+
+    start_time = time.time()
+    robust_regression_model = RobustRegressionModel(
+        N,
+        K,
+        alpha_scale,
+        beta_scale,
+        sigma_loc,
+        num_samples,
+        inference_type,
+        x_train,
+        y_train,
+    )
+    elapsed_time_compile_beanmachine = time.time() - start_time
+    samples, elapsed_time_sample_beanmachine = robust_regression_model.infer()
+
+    # repackage samples into format required by PPLBench
+    # List of dict, where each dict has key = param (string), value = value of param
+    param_keys = ["beta", "nu", "sigma", "alpha"]
+    samples_formatted = []
+    for i in range(num_samples):
+        sample_dict = {}
+        for j, parameter in enumerate(samples.keys()):
+            if j == 0:
+                sample_dict[param_keys[j]] = (
+                    samples[parameter][i].detach().numpy().reshape(1, K)
+                )
+            else:
+                sample_dict[param_keys[j]] = samples[parameter][i].item()
+        samples_formatted.append(sample_dict)
+
+    timing_info = {
+        "compile_time": elapsed_time_compile_beanmachine,
+        "inference_time": elapsed_time_sample_beanmachine,
+    }
+    return (samples_formatted, timing_info)
