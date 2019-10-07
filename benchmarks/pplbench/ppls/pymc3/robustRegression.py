a66655d999b81f5b87405c62a8b7203e59b525de
SourabhKul <skulkarni@umass.edu>
2019-10-07 15:40:40 -0700

    pymc3_robustRegression (#23)
    Summary:
    ### Motivation
    Add pymc3 implementation of robustRegression to PPLBench
    
    ### Changes proposed
    added ppls/pymc3/robustRegression.py
    Pull Request resolved: https://github.com/facebookincubator/BeanMachine/pull/23
    
    Test Plan:
    `python PPLBench.py -l jags,stan,pymc3 --iterations 5 -t 100`
    ### Types of changes
    - [ ] Docs change / refactoring / dependency upgrade
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [X] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing functionality to change)
    
    ### Checklist
    - [X] My code follows the code style of this project.
    - [ ] My change requires a change to the documentation.
    - [ ] I have updated the documentation accordingly.
    - [X] I have read the **[CONTRIBUTING](https://github.com/facebookincubator/BeanMachine/blob/master/CONTRIBUTING.md)** document.
    - [ ] I have added tests to cover my changes.
    - [X] All new and existing tests passed.
    - [X] The title of my pull request is a short description of the requested changes.
    
    Differential Revision: D17798930
    
    Pulled By: nimar
    
    fbshipit-source-id: b589a88785aa9fa97b1ed452adc7ee55b24b6e19

---
+++ benchmarks/pplbench/ppls/pymc3/robustRegression.py
@@ -0,0 +1,57 @@
+# Copyright(C) Facebook, Inc. and its affiliates. All Rights Reserved.
+
+import time
+
+import pymc3 as pm
+
+
+def obtain_posterior(data_train, args_dict, model=None):
+    """
+    PyMC3 implementation of robust regression model.
+
+    Inputs:
+    - data_train(tuple of np.ndarray): x_train, y_train
+    - args_dict: a dict of model arguments
+    Returns:
+    - samples(dict): posterior samples of all parameters
+    - timing_info(dict): compile_time, inference_time
+    """
+    x_train, y_train = data_train
+    K = int(x_train.shape[0])
+    thinning = args_dict["thinning_pymc3"]
+    alpha_scale, beta_scale, beta_loc, sigma_loc = args_dict["model_args"]
+    num_samples = int(args_dict["num_samples_pymc3"])
+
+    # Define model and sample
+    if args_dict["inference_type"] == "mcmc":
+        start_time = time.time()
+        with pm.Model():
+            alpha = pm.Normal("alpha", mu=0, sigma=alpha_scale)
+            beta = pm.Normal("beta", mu=beta_loc, sigma=beta_scale, shape=K)
+            sigma = pm.Exponential("sigma", lam=sigma_loc)
+            nu = pm.Gamma("nu", alpha=2, beta=10)
+            mean = (alpha + x_train.T * beta).T
+            pm.StudentT("y_observed", nu=nu, mu=mean, sigma=sigma, observed=y_train)
+            elapsed_time_compile_pymc3 = time.time() - start_time
+            start_time = time.time()
+            samples_pymc3 = pm.sample(
+                num_samples, cores=1, chains=1, discard_tuned_samples=False
+            )
+
+    elif args_dict["inference_type"] == "vi":
+        raise NotImplementedError
+
+    elapsed_time_sample_pymc3 = time.time() - start_time
+    # repackage samples into shape required by PPLBench
+    samples = []
+    for i in range(0, int(args_dict["num_samples_pymc3"]), thinning):
+        sample_dict = {}
+        for parameter in ["alpha", "beta", "nu", "sigma"]:
+            sample_dict[parameter] = samples_pymc3[parameter][i]
+        samples.append(sample_dict)
+    timing_info = {
+        "compile_time": elapsed_time_compile_pymc3,
+        "inference_time": elapsed_time_sample_pymc3,
+    }
+
+    return (samples, timing_info)
