fix log likelihood calculation for CLARA
Summary: Before, we were averaging over samples and observations, but we only want to average over samples.

Reviewed By: nimar

Differential Revision: D18584992

fbshipit-source-id: d7fcb1435c36704c9b7022b492e5dd224dcce422

benchmarks/pplbench/models/crowdSourcedAnnotationModel.py
@@ -209,6 +209,6 @@ def evaluate_posterior_predictive(samples, data_test, model=None):
                     P_k *= sample["theta"][j, k, y[i][index]]
                 P_i += sample["pi"][k] * P_k
             sample_pred_log_lik += np.log(P_i)
-        pred_log_lik_array.append(sample_pred_log_lik / n_items)
+        pred_log_lik_array.append(sample_pred_log_lik)
     # return as a numpy array
     return np.array(pred_log_lik_array)