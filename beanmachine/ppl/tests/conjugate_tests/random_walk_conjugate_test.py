1cf65601a8b3e1bc6c89ea9e624e3a7f8a9a8c54
David Noursi <dcalifornia@fb.com>
2019-12-04 14:27:20 -0800

    Refactor RWMH Conjugate Tests
    Summary: Refactored the conjugate tests for RWMH to use the new helper functions in `abstract_conjugate.py` created in D18066790
    
    Reviewed By: nazanint
    
    Differential Revision: D18276839
    
    fbshipit-source-id: 07ecb1651c27ed14ca61ef7fc7363c6fa38e3c1a

--- beanmachine/ppl/tests/conjugate_tests/random_walk_conjugate_test.py
+++ beanmachine/ppl/tests/conjugate_tests/random_walk_conjugate_test.py
@@ -8,58 +8,26 @@ from beanmachine.ppl.tests.conjugate_tests.abstract_conjugate import (
 
 
 class SingleSiteRandomWalkConjugateTest(unittest.TestCase, AbstractConjugateTests):
+    def setUp(self):
+        self.mh = SingleSiteRandomWalk(step_size=1.0)
+
     def test_beta_binomial_conjugate_run(self):
-        expected_mean, expected_std, queries, observations = (
-            self.compute_beta_binomial_moments()
-        )
-        mh = SingleSiteRandomWalk(step_size=2.0)
-        predictions = mh.infer(queries, observations, 2000)
-        mean, _ = self.compute_statistics(predictions.get_chain()[queries[0]])
-        # Smaller delta, within reasonable time, would likely require a burn-in period
-        self.assertAlmostEqual(abs((mean - expected_mean).sum().item()), 0, delta=0.5)
+        mh = SingleSiteRandomWalk(step_size=0.3)
+        self.beta_binomial_conjugate_run(mh, num_samples=2000, delta=0.5)
 
     def test_gamma_gamma_conjugate_run(self):
-        expected_mean, expected_std, queries, observations = (
-            self.compute_gamma_gamma_moments()
-        )
-        mh = SingleSiteRandomWalk(step_size=1.0)
-        predictions = mh.infer(queries, observations, 2000)
-        mean, _ = self.compute_statistics(predictions.get_chain()[queries[0]])
-        self.assertAlmostEqual(abs((mean - expected_mean).sum().item()), 0, delta=0.2)
+        self.gamma_gamma_conjugate_run(self.mh, num_samples=2000, delta=0.2)
 
     def test_gamma_normal_conjugate_run(self):
-        # Converges with 10k and more iterations but will use a bigger delta for
-        # now to have a faster test.
-        expected_mean, expected_std, queries, observations = (
-            self.compute_gamma_normal_moments()
-        )
-        mh = SingleSiteRandomWalk(step_size=1.0)
-        predictions = mh.infer(queries, observations, 2000)
-        mean, _ = self.compute_statistics(predictions.get_chain()[queries[0]])
-        self.assertAlmostEqual(abs((mean - expected_mean).sum().item()), 0, delta=0.5)
+        self.gamma_normal_conjugate_run(self.mh, num_samples=2000, delta=0.5)
 
     def test_normal_normal_conjugate_run(self):
-        # Converges with 10k and more iterations but will use a bigger delta for
-        # now to have a faster test.
-        expected_mean, expected_std, queries, observations = (
-            self.compute_normal_normal_moments()
-        )
-        mh = SingleSiteRandomWalk()
-        predictions = mh.infer(queries, observations, 1000)
-        mean, _ = self.compute_statistics(predictions.get_chain()[queries[0]])
-        self.assertAlmostEqual(abs((mean - expected_mean).sum().item()), 0, delta=0.3)
+        mh = SingleSiteRandomWalk(step_size=1.5)
+        self.normal_normal_conjugate_run(mh, num_samples=1000, delta=0.3)
 
     def test_distant_normal_normal_conjugate_run(self):
-        # Converges with 10k and more iterations but will use a bigger delta for
-        # now to have a faster test.
-        expected_mean, expected_std, queries, observations = (
-            self.compute_distant_normal_normal_moments()
-        )
         mh = SingleSiteRandomWalk(step_size=3.0)
-        predictions = mh.infer(queries, observations, 5000)
-        mean, _ = self.compute_statistics(predictions.get_chain()[queries[0]])
-        # Smaller delta, within reasonable time, would likely require a burn-in period
-        self.assertAlmostEqual(abs((mean - expected_mean).sum().item()), 0, delta=1.0)
+        self.normal_normal_conjugate_run(mh, num_samples=5000, delta=1.0)
 
     def test_dirichlet_categorical_conjugate_run(self):
         pass
