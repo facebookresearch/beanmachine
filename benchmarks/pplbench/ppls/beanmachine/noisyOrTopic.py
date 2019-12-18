00cf40db72f6a30c04759de586bfba41c4d46a3b
Kinjal Shah <kshah97@fb.com>
2019-12-18 08:58:51 -0800

    fix bug in noisyOrTopic (#60)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/BeanMachine/pull/60
    
    Fixed bug in noisyOrTopic.
    
    Reviewed By: nimar
    
    Differential Revision: D19159436
    
    fbshipit-source-id: 4abc77a9c81314c5baaa65bc490a02f64a03fa00

--- benchmarks/pplbench/ppls/beanmachine/noisyOrTopic.py
+++ benchmarks/pplbench/ppls/beanmachine/noisyOrTopic.py
@@ -41,17 +41,17 @@ class NoisyOrModel(object):
 
     def infer(self):
         dict_y = {self.y(k + self.T): self.words[k] for k in range(len(self.words))}
-        dict_y[self.y(0)] = torch.tensor(1.0)
+        dict_y[self.node(0)] = torch.tensor(1.0)
         if self.inference_type == "mcmc":
             nmc = SingleSiteNewtonianMonteCarlo()
             start_time = time.time()
             samples = nmc.infer(
-                [self.node(i) for i in range(self.T)], dict_y, self.iterations, 1
+                [self.node(i) for i in range(1, self.T)], dict_y, self.iterations, 1
             ).get_chain()
+            elapsed_time_sample_beanmachine = time.time() - start_time
         elif self.inference_type == "vi":
             print("ImplementationError; exiting...")
             exit(1)
-        elapsed_time_sample_beanmachine = time.time() - start_time
         return (samples, elapsed_time_sample_beanmachine)
 
 
@@ -83,9 +83,9 @@ def obtain_posterior(data_train, args_dict, model):
 
     samples_formatted = []
     all_node_samples = np.zeros((iterations, T))
-    for t in range(T):
+    for t in range(1, T):
         all_node_samples[:, t] = samples[noisy_or_model.node(t)].detach().numpy()
-
+    all_node_samples[:, 0] = 1.0
     for i in range(iterations):
         sample_dict = {}
         sample_dict["node"] = all_node_samples[i]
