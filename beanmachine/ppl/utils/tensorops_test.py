63d7b5d735e6abd856401b891c7639363299e38f
Nim Arora <nimarora@fb.com>
2020-01-24 17:18:10 -0800

    compute 1st and 2nd gradient in C++ (#69)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/BeanMachine/pull/69
    
    Updated the torch version for github testing.
    
    Differential Revision: D19412861
    
    fbshipit-source-id: fd15fd3fed1c4acfb0a556722f68f9feec1194ea

---
+++ beanmachine/ppl/utils/tensorops_test.py
@@ -0,0 +1,37 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+"""Tests for tensorops.cpp"""
+import unittest
+
+import torch
+from beanmachine.ppl.utils import tensorops
+
+
+class TensorOpsTest(unittest.TestCase):
+    def test_gradients(self) -> None:
+        for type_ in [torch.float32, torch.float64]:
+            x = torch.randn(3, requires_grad=True, dtype=type_)
+            prec = torch.Tensor([[1, 0.1, 0], [0.1, 2, 0.5], [0, 0.5, 3]]).to(type_)
+            mu = torch.randn(3, dtype=type_)
+            # first gradient is `-(x - mu) @ prec`, second gradient is `- prec`
+            f = -(x - mu) @ prec @ (x - mu) / 2
+            grad, hess = tensorops.gradients(f, x)
+            self.assertTrue(grad.allclose(-(x - mu) @ prec))
+            self.assertTrue(hess.allclose(-prec))
+            self.assertEqual(grad.dtype, type_, "gradient dtype must match input")
+            self.assertEqual(hess.dtype, type_, "hessian dtype must match input")
+
+    def test_gradients_negative(self) -> None:
+        # output must have one element
+        x = torch.randn(3, requires_grad=True)
+        with self.assertRaises(ValueError) as cm:
+            tensorops.gradients(2 * x, x)
+        self.assertTrue(
+            "output tensor must have exactly one element" in str(cm.exception)
+        )
+        # input must be 1-d
+        x = torch.randn(3, 3, requires_grad=True)
+        with self.assertRaises(ValueError) as cm:
+            tensorops.gradients(x.sum(), x)
+        self.assertTrue(
+            "input tensor must have exactly one dimension" in str(cm.exception)
+        )
