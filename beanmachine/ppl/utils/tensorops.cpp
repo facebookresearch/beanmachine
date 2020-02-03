9da8db877cb5123905151f84b4325b842b26da8a
Nazanin Khosravani Tehrani <nazaninkt@fb.com>
2020-02-02 22:31:53 -0800

    Moving Hessian to C++ for Half space and Simplex (#83)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/BeanMachine/pull/83
    
    Moving Hessian to C++ for Half space and Simplex
    
    Reviewed By: nimar
    
    Differential Revision: D19689177
    
    fbshipit-source-id: a73dbea4dd9ba27c7a10938ce49222b1ebd862c0

--- beanmachine/ppl/utils/tensorops.cpp
+++ beanmachine/ppl/utils/tensorops.cpp
@@ -46,6 +46,58 @@ std::tuple<torch::Tensor, torch::Tensor> gradients(
   return std::make_tuple(grad1, hessian);
 }
 
+std::tuple<torch::Tensor, torch::Tensor> simplex_gradients(
+    torch::Tensor output,
+    torch::Tensor input) {
+  // based on: https://github.com/pytorch/pytorch/issues/32045
+  pybind11::gil_scoped_release no_gil;
+  if (output.numel() != 1) {
+    throw std::invalid_argument(
+        "output tensor must have exactly one element, got " +
+        std::to_string(output.numel()));
+  }
+  // compute the first gradient
+  torch::Tensor grad1 =
+      torch::autograd::grad({output}, {input}, {}, true, true, true)[0];
+  // for each element of the first gradient compute the second gradient
+  grad1 = grad1.reshape(-1);
+  int64_t input_len = grad1.size(0);
+  torch::Tensor hessian_diag = torch::zeros({input_len}, grad1.dtype());
+  for (int64_t i = 0; i < input_len; i++) {
+    torch::Tensor second_grad = (torch::autograd::grad({grad1[i]}, {input}, {}, true, true, true)[0]).reshape(-1);
+    hessian_diag[i] = second_grad[i];
+    second_grad[i] = second_grad.min();
+    hessian_diag[i] -= second_grad.max();
+  }
+
+  return std::make_tuple(grad1, hessian_diag);
+}
+
+std::tuple<torch::Tensor, torch::Tensor> halfspace_gradients(
+    torch::Tensor output,
+    torch::Tensor input) {
+  // based on: https://github.com/pytorch/pytorch/issues/32045
+  pybind11::gil_scoped_release no_gil;
+  if (output.numel() != 1) {
+    throw std::invalid_argument(
+        "output tensor must have exactly one element, got " +
+        std::to_string(output.numel()));
+  }
+  // compute the first gradient
+  torch::Tensor grad1 =
+      torch::autograd::grad({output}, {input}, {}, true, true, true)[0];
+  // for each element of the first gradient compute the second gradient
+  grad1 = grad1.reshape(-1);
+  int64_t input_len = grad1.size(0);
+  torch::Tensor hessian_diag = torch::zeros({input_len}, grad1.dtype());
+  for (int64_t i = 0; i < input_len; i++) {
+    torch::Tensor second_grad = (torch::autograd::grad({grad1[i]}, {input}, {}, true, true, true)[0]).reshape(-1);
+    hessian_diag[i] = second_grad[i];
+  }
+
+  return std::make_tuple(grad1, hessian_diag);
+}
+
 PYBIND11_MODULE(tensorops, module) {
   module.doc() = "module for operations on tensors";
 
@@ -55,6 +107,19 @@ PYBIND11_MODULE(tensorops, module) {
       "Compute the first and the second gradient of the output Tensor w.r.t. the input Tensor.",
       pybind11::arg("output"),
       pybind11::arg("input"));
+  module.def(
+      "simplex_gradients",
+      &simplex_gradients,
+      "Compute the first and the second gradient of the output Tensor w.r.t. the input Tensor for simplex.",
+      pybind11::arg("output"),
+      pybind11::arg("input"));
+  module.def(
+      "halfspace_gradients",
+      &halfspace_gradients,
+      "Compute the first and the second gradient of the output Tensor w.r.t. the input Tensor for half space.",
+      pybind11::arg("output"),
+      pybind11::arg("input"));
+
 }
 
 } // namespace tensorops
