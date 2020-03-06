63f57d0af9445958ca1089a0ad1a05831e16079c
Nim Arora <nimarora@fb.com>
2020-03-06 12:20:30 -0800

    add binomial distribution (#111)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/BeanMachine/pull/111
    
    Add the binomial distribution to bmgraph.
    
    Reviewed By: ericlippert
    
    Differential Revision: D20297706
    
    fbshipit-source-id: 4c1902848d01b9f146551dfcc159768cd21c66cf

---
+++ beanmachine/graph/binomial.h
@@ -0,0 +1,19 @@
+// Copyright (c) Facebook, Inc. and its affiliates.
+#pragma once
+#include "beanmachine/graph/distribution.h"
+
+namespace beanmachine {
+namespace distribution {
+
+class Binomial : public Distribution {
+ public:
+  Binomial(
+      graph::AtomicType sample_type,
+      const std::vector<graph::Node*>& in_nodes);
+  ~Binomial() override {}
+  graph::AtomicValue sample(std::mt19937& gen) const override;
+  double log_prob(const graph::AtomicValue& value) const override;
+};
+
+} // namespace distribution
+} // namespace beanmachine
