2587b54ec3cb633735c9aa5d35f1bbc7d2dd9584
Nim Arora <nimarora@fb.com>
2020-03-06 09:45:16 -0800

    add Beta distribution
    Summary: Add a Beta distribution to BMGraph
    
    Differential Revision: D20242158
    
    fbshipit-source-id: 0cb694964bc2563ed49e706bf092f61b1a4033a0

--- beanmachine/graph/pybindings.cpp
+++ beanmachine/graph/pybindings.cpp
@@ -41,7 +41,8 @@ PYBIND11_MODULE(graph, module) {
   py::enum_<DistributionType>(module, "DistributionType")
       .value("TABULAR", DistributionType::TABULAR)
       .value("BERNOULLI", DistributionType::BERNOULLI)
-      .value("BERNOULLI_NOISY_OR", DistributionType::BERNOULLI_NOISY_OR);
+      .value("BERNOULLI_NOISY_OR", DistributionType::BERNOULLI_NOISY_OR)
+      .value("BETA", DistributionType::BETA);
 
   py::enum_<NodeType>(module, "NodeType")
       .value("CONSTANT", NodeType::CONSTANT)
