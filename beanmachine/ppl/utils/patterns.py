1ced06f18b1cfa74adde270e2c425e7c199ff42b
Walid Taha <wtaha@fb.com>
2020-03-06 17:02:39 -0800

    <Added comments to both Success and Fail classes>
    Summary: This is done just for the sake of completeness
    
    Reviewed By: ericlippert
    
    Differential Revision: D20316153
    
    fbshipit-source-id: 3121af0ba797fd8583d583274c0bdc220409865c

--- beanmachine/ppl/utils/patterns.py
+++ beanmachine/ppl/utils/patterns.py
@@ -56,6 +56,7 @@ class MatchResult(ABC):
 
 
 class Fail(MatchResult):
+    """A pattern that always fails."""
     # TODO: If we save the patterns that failed as well, then we can build a
     # TODO: diagnostic engine that describes why a value failed to match against
     # TODO: a complex pattern.
@@ -72,6 +73,7 @@ class Fail(MatchResult):
 
 
 class Success(MatchResult):
+    """A pattern that always succeeds."""
     def __init__(
         self, test: Any = None, submatches: Optional[Dict[str, MatchResult]] = None
     ) -> None:
