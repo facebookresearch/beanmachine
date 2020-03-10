a0415dafebf381932f2ef698bc71810f8ebe413c
Eric Lippert <ericlippert@fb.com>
2020-03-10 15:27:03 -0700

    Remove unused import
    Summary: My recent change to fix the incorrect usage of MatchResult in rules.py accidentally left a now-unused import behind, which the linter is complaining about.
    
    Reviewed By: yucenli
    
    Differential Revision: D20373051
    
    fbshipit-source-id: e7f4ee03dc3f0767522bfa2550074473938d1cb3

--- beanmachine/ppl/utils/rules.py
+++ beanmachine/ppl/utils/rules.py
@@ -4,7 +4,6 @@ from abc import ABC, abstractmethod
 from typing import Any, Callable, Dict, Iterable, List, Tuple
 
 from beanmachine.ppl.utils.patterns import (
-    MatchResult,
     Pattern,
     anyPattern,
     failPattern,
