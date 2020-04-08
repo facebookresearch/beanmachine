5497d9e4b0f813c5886e40a64e97517fed6bd1b2
Nim Arora <nimarora@fb.com>
2020-04-08 11:59:15 -0700

    add Normal distribution (#134)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/BeanMachine/pull/134
    
    Add a Normal distribution to bmgraph and extend NMC to support reals.
    
    Reviewed By: ericlippert
    
    Differential Revision: D20775422
    
    fbshipit-source-id: cb3316b9e5238e1ac3df99bf9892ce9d0de27ff2

---
+++ beanmachine/graph/proposer/normal.cpp
@@ -0,0 +1,21 @@
+// Copyright (c) Facebook, Inc. and its affiliates.
+#include <cmath>
+#include <random>
+
+#include "beanmachine/graph/proposer/normal.h"
+
+namespace beanmachine {
+namespace proposer {
+
+graph::AtomicValue Normal::sample(std::mt19937& gen) const {
+  std::normal_distribution<double> dist(mu, sigma);
+  return graph::AtomicValue(graph::AtomicType::REAL, dist(gen));
+}
+
+double Normal::log_prob(graph::AtomicValue& value) const {
+  return -std::log(sigma) - 0.5 * std::log(2 * M_PI)
+    - 0.5 * (value._double - mu) * (value._double - mu) / (sigma * sigma);
+}
+
+} // namespace proposer
+} // namespace beanmachine
---
+++ beanmachine/graph/proposer/normal.h
@@ -0,0 +1,34 @@
+// Copyright (c) Facebook, Inc. and its affiliates.
+#pragma once
+#include "beanmachine/graph/proposer/proposer.h"
+
+namespace beanmachine {
+namespace proposer {
+
+class Normal : public Proposer {
+ public:
+  /*
+  Constructor for Normal class.
+  :param mu: mean of Normal
+  :param sigma: std. dev of Normal
+  */
+  Normal(double mu, double sigma):  Proposer(), mu(mu), sigma(sigma) {}
+  /*
+  Sample a value from the proposer.
+  :param gen: Random number generator.
+  :returns: A value.
+  */
+  graph::AtomicValue sample(std::mt19937& gen) const override;
+  /*
+  Compute the log_prob of a value.
+  :param value: The value to evaluate the distribution.
+  :returns: log probability of value.
+  */
+  double log_prob(graph::AtomicValue& value) const override;
+ private:
+  double mu;
+  double sigma;
+};
+
+} // namespace distribution
+} // namespace beanmachine
