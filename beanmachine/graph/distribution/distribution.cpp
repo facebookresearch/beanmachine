7045e862d103d9860c020824ae8a4ebc9c7c1a21
Nim Arora <nimarora@fb.com>
2020-04-08 08:49:40 -0700

    add flat distribution
    Summary: Add a Flat distribution.
    
    Differential Revision: D20716708
    
    fbshipit-source-id: 3dee616a5865501eb8322a10ba6a2b316d42d6c1

--- beanmachine/graph/distribution/distribution.cpp
+++ beanmachine/graph/distribution/distribution.cpp
@@ -5,6 +5,7 @@
 #include "beanmachine/graph/distribution/binomial.h"
 #include "beanmachine/graph/distribution/distribution.h"
 #include "beanmachine/graph/distribution/tabular.h"
+#include "beanmachine/graph/distribution/flat.h"
 
 namespace beanmachine {
 namespace distribution {
@@ -30,6 +31,8 @@ std::unique_ptr<Distribution> Distribution::new_distribution(
     return std::make_unique<Beta>(sample_type, in_nodes);
   } else if (dist_type == graph::DistributionType::BINOMIAL) {
     return std::make_unique<Binomial>(sample_type, in_nodes);
+  } else if (dist_type == graph::DistributionType::FLAT) {
+    return std::make_unique<Flat>(sample_type, in_nodes);
   }
   throw std::invalid_argument(
       "Unknown distribution " + std::to_string(static_cast<int>(dist_type)));
---
+++ beanmachine/graph/distribution/flat.cpp
@@ -0,0 +1,78 @@
+// Copyright (c) Facebook, Inc. and its affiliates.
+#include <string>
+
+#include "beanmachine/graph/distribution/flat.h"
+
+namespace beanmachine {
+namespace distribution {
+
+using namespace graph;
+
+Flat::Flat(
+    AtomicType sample_type,
+    const std::vector<Node*>& in_nodes)
+    : Distribution(DistributionType::FLAT, sample_type) {
+  // a Flat distribution has no parents
+  if (in_nodes.size() != 0) {
+    throw std::invalid_argument(
+        "Flat distribution has no parents");
+  }
+  // almost all types are supported
+  if (sample_type == AtomicType::TENSOR) {
+    throw std::invalid_argument("Flat doesn't support Tensor samples");
+  }
+}
+
+AtomicValue Flat::sample(std::mt19937& gen) const {
+  AtomicValue value;
+  switch(sample_type) {
+    case AtomicType::BOOLEAN: {
+      std::bernoulli_distribution dist(0.5);
+      value = AtomicValue(dist(gen));
+      break;
+    }
+    case AtomicType::PROBABILITY: {
+      std::uniform_real_distribution<double> dist(0, 1);
+      value = AtomicValue(AtomicType::PROBABILITY, dist(gen));
+      break;
+    }
+    case AtomicType::REAL: {
+      std::uniform_real_distribution<double> dist(
+          std::numeric_limits<double>::lowest(), std::numeric_limits<double>::max());
+      value = AtomicValue(dist(gen));
+      break;
+    }
+    case AtomicType::POS_REAL: {
+      std::uniform_real_distribution<double> dist(0, std::numeric_limits<double>::max());
+      value = AtomicValue(AtomicType::POS_REAL, dist(gen));
+      break;
+    }
+    case AtomicType::NATURAL: {
+      std::uniform_int_distribution<natural_t> dist(0, std::numeric_limits<natural_t>::max());
+      value = AtomicValue(AtomicType::NATURAL, dist(gen));
+      break;
+    }
+    default: {
+      assert(false);
+    }
+  }
+  return value;
+}
+
+// A Flat distribution is really easy in terms of computing the log_prob and the
+// gradients of the log_prob. These are all zero!
+
+double Flat::log_prob(const AtomicValue& /* value */) const {
+  return 0;
+}
+
+void Flat::gradient_log_prob_value(
+    const AtomicValue& /* value */, double& /* grad1 */, double& /* grad2 */) const {
+}
+
+void Flat::gradient_log_prob_param(
+    const AtomicValue& /* value */, double& /* grad1 */, double& /* grad2 */) const {
+}
+
+} // namespace distribution
+} // namespace beanmachine
