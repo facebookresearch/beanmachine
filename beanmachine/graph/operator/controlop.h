4eb2bffc9d2b8c8a71217515c31890fa716fad6d
Nim Arora <nimarora@fb.com>
2020-04-20 13:02:27 -0700

    add if_then_else operator
    Summary: Add the if_then_else operator.
    
    Differential Revision: D21065013
    
    fbshipit-source-id: d8fbb87009f3b97cd9ffe66e8224d9b5ce3f46cc

---
+++ beanmachine/graph/operator/controlop.h
@@ -0,0 +1,11 @@
+// Copyright (c) Facebook, Inc. and its affiliates.
+#pragma once
+#include "beanmachine/graph/graph.h"
+
+namespace beanmachine {
+namespace oper {
+
+void if_then_else(graph::Node* node);
+
+} // namespace oper
+} // namespace beanmachine
