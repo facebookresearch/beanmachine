1ee21778030e0f00332d642411c512db6df2d99b
Nim Arora <nimarora@fb.com>
2020-04-29 10:22:26 -0700

    add NMC support for booleans
    Summary: Added a simple proposer for boolean types
    
    Reviewed By: nazanint
    
    Differential Revision: D21070108
    
    fbshipit-source-id: a3264d05e17ed77169d178713346ab16623a9032

---
+++ beanmachine/graph/proposer/delta.h
@@ -0,0 +1,34 @@
+// Copyright (c) Facebook, Inc. and its affiliates.
+#pragma once
+#include "beanmachine/graph/proposer/proposer.h"
+
+namespace beanmachine {
+namespace proposer {
+
+class Delta : public Proposer {
+ public:
+  /*
+  Delta puts all its mass at a single point
+  :param alpha: point
+  */
+  Delta(graph::AtomicValue point):  Proposer(), point(point) {}
+  /*
+  Sample a value from the proposer.
+  :param gen: Random number generator.
+  :returns: A value.
+  */
+  graph::AtomicValue sample(std::mt19937& gen) const override {return point;}
+  /*
+  Compute the log_prob of a value.
+  :param value: The value to evaluate the distribution.
+  :returns: log probability of value.
+  */
+  double log_prob(graph::AtomicValue& value) const override {
+    return value == point ? 0.0 : - std::numeric_limits<double>::infinity();
+  };
+ private:
+  graph::AtomicValue point;
+};
+
+} // namespace distribution
+} // namespace beanmachine
