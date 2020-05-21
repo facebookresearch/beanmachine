9534dab02d3310805edb950ee938801ccb3bab3a
Eric Lippert <ericlippert@fb.com>
2020-05-20 17:22:15 -0700

    Add some comments to ast tools helper module (#181)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/beanmachine/pull/181
    
    I've just added a small number of explanatory comments to my module of helper functions that I use when debugging the compiler.
    
    I've also deleted a function that I don't plan on using again soon.
    
    Differential Revision: D21676052
    
    fbshipit-source-id: bb5dd6aa74215fbfc244da94391e357fc7fcab88

--- beanmachine/ppl/compiler/ast_tools.py
+++ beanmachine/ppl/compiler/ast_tools.py
@@ -1,8 +1,10 @@
 # Copyright (c) Facebook, Inc. and its affiliates.
 """Debugging tools for working with ASTs"""
 
+# This module just has some helpful tools that can be used for visualizing
+# Python ASTs when debugging the compilation process.
+
 import ast
-import copy
 from ast import AST
 from typing import Any, List, Tuple
 
@@ -19,6 +21,9 @@ def _get_name(node: Any) -> str:
 
 
 def print_tree(node: AST, unicode: bool = True) -> str:
+    """Takes an AST and produces a string containing a hierarchical
+view of the tree structure."""
+
     def get_children(node: Any) -> List[Any]:
         if isinstance(node, list):
             return node
@@ -30,6 +35,9 @@ def print_tree(node: AST, unicode: bool = True) -> str:
 
 
 def print_graph(node: AST) -> str:
+    """Takes an AST and produces a string containing a DOT
+representation of the tree as a graph."""
+
     def get_children(node: Any) -> List[Tuple[str, Any]]:
         if isinstance(node, list):
             return [(str(i), a) for i, a in enumerate(node)]
@@ -38,9 +46,3 @@ def print_graph(node: AST) -> str:
         return []
 
     return db.print_graph([node], get_children, None, _get_name)
-
-
-def with_args(node: ast.Call, args: List) -> ast.Call:
-    c: ast.Call = copy.copy(node)
-    c.args = args
-    return c
