8c097135f4151968401c07530fafbd30d288f63e
Feynman Liang <feynmanl@fb.com>
2020-05-26 13:42:23 -0700

    Export inference classes from top-level package (#179)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/beanmachine/pull/179
    
    Pull Request: https://github.com/facebookincubator/beanmachine/pull/184
    
    As a beanmachine user, I should be able to write a beam machine model without
    worrying too much about the internal package / sub-package structure used in
    beam machine.
    
    This diff (1) adds some of the other inference classes to the `ppl.inference` package,  (2) exports these inference classes on the `ppl` top-level package, and (3) uses `bm` for references to package imports
    
    Reviewed By: nimar
    
    Differential Revision: D21667490
    
    fbshipit-source-id: ac80ce906ad34884f328322b65065865c45dfb69

--- beanmachine/ppl/tests/smoke_test.py
+++ beanmachine/ppl/tests/smoke_test.py
@@ -1,17 +1,20 @@
 # Copyright (c) Facebook, Inc. and its affiliates.
 import unittest
 
-import beanmachine.ppl as bmp
+import beanmachine.ppl as bm
 import torch.distributions as dist
 
 
 class ToplevelSmokeTest(unittest.TestCase):
     def test_toplevel_package_imports(self):
         # these decorators should execute without error
-        @bmp.random_variable
+        @bm.random_variable
         def foo(i):
             return dist.Bernoulli(0.5)
 
-        @bmp.functional
+        @bm.functional
         def foo_sum(n):
             return sum(foo(i) for i in range(n))
+
+        # exercise invocation from top-level package directly
+        bm.CompositionalInference().infer([foo_sum(3)], {foo(0): False}, 1000, 1)
