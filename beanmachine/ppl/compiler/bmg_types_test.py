712f8da7a34567e54b13882371fa4354d93d0401
Eric Lippert <ericlippert@fb.com>
2020-06-17 17:31:13 -0700

    Implement constant value classification for BMG type system
    Summary:
    To implement my type inferencing engine for converting graphs to BMG typed graphs, one of the tools I need is a function that classifies a value based on the smallest BMG type that can hold it, based on the rules:
    
    * one-element tensors are treated as if they were scalars
    * other tensors are Tensor
    * 0, 0.0, 1 and 1.0 are treated the same as True and False -- they are treated as bools
    * Non-negative integers are Natural
    * Reals between 0.0 and 1.0 are Probability
    * other positive reals are PositiveReal
    * negative reals are reals
    
    Differential Revision: D22107506
    
    fbshipit-source-id: c83ff03d33680269d15053f7a66fb273a5f42a80

--- beanmachine/ppl/compiler/bmg_types_test.py
+++ beanmachine/ppl/compiler/bmg_types_test.py
@@ -7,8 +7,9 @@ from beanmachine.ppl.compiler.bmg_types import (
     PositiveReal,
     Probability,
     supremum,
+    type_of_value,
 )
-from torch import Tensor
+from torch import Tensor, tensor
 
 
 class BMGTypesTest(unittest.TestCase):
@@ -20,3 +21,41 @@ class BMGTypesTest(unittest.TestCase):
         self.assertEqual(PositiveReal, supremum(Probability, Natural))
         self.assertEqual(float, supremum(Natural, Probability, float))
         self.assertEqual(Tensor, supremum(float, Tensor, Natural, bool))
+
+    def test_type_of_value(self) -> None:
+        """test_type_of_value"""
+
+        self.assertEqual(bool, type_of_value(True))
+        self.assertEqual(bool, type_of_value(False))
+        self.assertEqual(bool, type_of_value(0))
+        self.assertEqual(bool, type_of_value(1))
+        self.assertEqual(bool, type_of_value(0.0))
+        self.assertEqual(bool, type_of_value(1.0))
+        self.assertEqual(bool, type_of_value(tensor(True)))
+        self.assertEqual(bool, type_of_value(tensor(False)))
+        self.assertEqual(bool, type_of_value(tensor(0)))
+        self.assertEqual(bool, type_of_value(tensor(1)))
+        self.assertEqual(bool, type_of_value(tensor(0.0)))
+        self.assertEqual(bool, type_of_value(tensor(1.0)))
+        self.assertEqual(bool, type_of_value(tensor([[True]])))
+        self.assertEqual(bool, type_of_value(tensor([[False]])))
+        self.assertEqual(bool, type_of_value(tensor([[0]])))
+        self.assertEqual(bool, type_of_value(tensor([[1]])))
+        self.assertEqual(bool, type_of_value(tensor([[0.0]])))
+        self.assertEqual(bool, type_of_value(tensor([[1.0]])))
+        self.assertEqual(Natural, type_of_value(2))
+        self.assertEqual(Natural, type_of_value(2.0))
+        self.assertEqual(Natural, type_of_value(tensor(2)))
+        self.assertEqual(Natural, type_of_value(tensor(2.0)))
+        self.assertEqual(Natural, type_of_value(tensor([[2]])))
+        self.assertEqual(Natural, type_of_value(tensor([[2.0]])))
+        self.assertEqual(Probability, type_of_value(0.5))
+        self.assertEqual(Probability, type_of_value(tensor(0.5)))
+        self.assertEqual(Probability, type_of_value(tensor([[0.5]])))
+        self.assertEqual(PositiveReal, type_of_value(1.5))
+        self.assertEqual(PositiveReal, type_of_value(tensor(1.5)))
+        self.assertEqual(PositiveReal, type_of_value(tensor([[1.5]])))
+        self.assertEqual(float, type_of_value(-1.5))
+        self.assertEqual(float, type_of_value(tensor(-1.5)))
+        self.assertEqual(float, type_of_value(tensor([[-1.5]])))
+        self.assertEqual(Tensor, type_of_value(tensor([[0, 0]])))
