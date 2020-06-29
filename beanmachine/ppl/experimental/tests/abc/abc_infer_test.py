bef4e9aa124e605195545b49083ce6ea3e74ec37
Sourabh Kulkarni <sourabhkul@fb.com>
2020-06-29 15:25:51 -0700

    Add ABC inference to Bean Machine (#228)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/beanmachine/pull/228
    
    This diff adds ABC inference to Bean Machine.
    Added 4 tests to check.
    Notebook showing two examples of ABC: N290426
    
    Reviewed By: kshah1997
    
    Differential Revision: D22261752
    
    fbshipit-source-id: 0ee4fe86df220a74e2902aab868cd093c84b19a3

---
+++ beanmachine/ppl/experimental/tests/abc/abc_infer_test.py
@@ -0,0 +1,101 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+import unittest
+
+import beanmachine.ppl as bm
+import torch
+import torch.distributions as dist
+from beanmachine.ppl.experimental.abc.abc_infer import ApproximateBayesianComputation
+
+
+class ApproximateBayesianComputationTest(unittest.TestCase):
+    class CoinTossModel:
+        def __init__(self, observation_shape):
+            self.observation_shape = observation_shape
+
+        @bm.random_variable
+        def bias(self):
+            return dist.Beta(0.5, 0.5)
+
+        @bm.random_variable
+        def coin_toss(self):
+            return dist.Bernoulli(self.bias().repeat(self.observation_shape))
+
+        def toss_head_count(self, toss_vals):
+            return torch.sum(toss_vals)
+
+        def toss_mean(self, toss_vals):
+            return torch.mean(toss_vals)
+
+        @bm.functional
+        def num_heads(self):
+            return self.toss_head_count(self.coin_toss())
+
+        @bm.functional
+        def mean_value(self):
+            return self.toss_mean(self.coin_toss())
+
+    def test_abc_inference(self):
+        model = self.CoinTossModel(observation_shape=100)
+        COIN_TOSS_DATA = dist.Bernoulli(0.9).sample([100])
+        abc = ApproximateBayesianComputation(
+            tolerance={model.num_heads(): 10, model.mean_value(): 0.1}
+        )
+        observations = {
+            model.num_heads(): model.toss_head_count(COIN_TOSS_DATA),
+            model.mean_value(): model.toss_mean(COIN_TOSS_DATA),
+        }
+        queries = [model.bias()]
+        samples = abc.infer(
+            queries, observations, num_samples=100, num_chains=1, verbose=None
+        )
+        mean = torch.mean(samples[model.bias()][0])
+        self.assertTrue(mean.item() > 0.75)
+
+    def test_abc_inference_with_singleton_arguments(self):
+        model = self.CoinTossModel(observation_shape=100)
+        COIN_TOSS_DATA = dist.Bernoulli(0.9).sample([100])
+        abc = ApproximateBayesianComputation(
+            distance_function=torch.dist, tolerance=10.0
+        )
+        observations = {
+            model.num_heads(): model.toss_head_count(COIN_TOSS_DATA),
+            model.mean_value(): model.toss_mean(COIN_TOSS_DATA),
+        }
+        queries = [model.bias()]
+        samples = abc.infer(
+            queries, observations, num_samples=100, num_chains=1, verbose=None
+        )
+        mean = torch.mean(samples[model.bias()][0])
+        self.assertTrue(mean.item() > 0.75)
+
+    def test_single_inference_step(self):
+        model = self.CoinTossModel(observation_shape=10)
+        abc = ApproximateBayesianComputation(tolerance={model.num_heads(): 0.1})
+        abc.observations_ = {model.num_heads(): torch.tensor(2)}
+        self.assertEqual(abc._single_inference_step(), 0)
+        abc.reset()
+
+    def test_max_attempts(self):
+        model = self.CoinTossModel(observation_shape=100)
+        COIN_TOSS_DATA = dist.Bernoulli(0.9).sample([100])
+        abc = ApproximateBayesianComputation(
+            tolerance={model.num_heads(): 0.1}, max_attempts_per_sample=2
+        )
+        observations = {model.num_heads(): model.toss_head_count(COIN_TOSS_DATA)}
+        queries = [model.bias()]
+        with self.assertRaises(RuntimeError):
+            abc.infer(
+                queries, observations, num_samples=100, num_chains=1, verbose=None
+            )
+        abc.reset()
+
+    def test_shape_mismatch(self):
+        model = self.CoinTossModel(observation_shape=100)
+        abc = ApproximateBayesianComputation(tolerance={model.num_heads(): 0.1})
+        observations = {model.num_heads(): torch.tensor([3, 4])}
+        queries = [model.bias()]
+        with self.assertRaises(ValueError):
+            abc.infer(
+                queries, observations, num_samples=100, num_chains=1, verbose=None
+            )
+        abc.reset()
