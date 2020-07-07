928382a4ffcc37eeabbcd11fd8f855ba488f48d6
Eric Lippert <ericlippert@fb.com>
2020-07-06 17:58:17 -0700

    Start adding code for compiler error reporting
    Summary:
    This code is a start at tracking errors that we *expect to find in a program*, that must be reported to the user. It's not for errors where the compiler itself fails. Remember, error reporting is *not an exceptional case* for a compiler; programs contain errors.
    
    Since I expect to have multiple kinds of errors I've made a base class and so far one derived class that will be for the scenario where a BMG graph cannot be generated because an edge requirement is always violated.  There will be more.
    
    This code will be properly exercised in an upcoming diff.
    
    Differential Revision: D22404528
    
    fbshipit-source-id: bd632bb76c2681aff0e33fe4f17ac4fcb8f49a6e

---
+++ beanmachine/ppl/compiler/error_report_test.py
@@ -0,0 +1,20 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+"""Tests for error_report.py"""
+import unittest
+
+from beanmachine.ppl.compiler.error_report import ErrorReport, Violation
+from beanmachine.ppl.utils.bm_graph_builder import BMGraphBuilder
+
+
+class ErrorReportTest(unittest.TestCase):
+    def test_error_report(self) -> None:
+        """test_error_report"""
+        bmg = BMGraphBuilder()
+        r = bmg.add_real(-2.5)
+        b = bmg.add_bernoulli(r)
+        v = Violation(r, b.requirements[0], b, b.edges[0])
+        e = ErrorReport()
+        e.add_error(v)
+        expected = """
+The probability of a Bernoulli is required to be a probability but is a real."""
+        self.assertEqual(expected.strip(), str(e).strip())
