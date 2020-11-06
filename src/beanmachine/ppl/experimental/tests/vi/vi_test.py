Fix entropy calculation for non-Normal IAF

src/beanmachine/ppl/experimental/tests/vi/vi_test.py
@@ -30,11 +30,13 @@ class VariationalInferTest(unittest.TestCase):
         nf_samples = nf.sample((20,)).squeeze().numpy()
         vi_samples = vi.sample((20,)).detach().numpy()
 
-        self.assertTrue(
-            scipy.stats.ks_2samp(nf_samples[:, 0], vi_samples[:, 0]).pvalue >= 0.05
+        self.assertGreaterEqual(
+            scipy.stats.ks_2samp(nf_samples[:, 0], vi_samples[:, 0]).pvalue,
+            0.05
         )
-        self.assertTrue(
-            scipy.stats.ks_2samp(nf_samples[:, 1], vi_samples[:, 1]).pvalue >= 0.05
+        self.assertGreaterEqual(
+            scipy.stats.ks_2samp(nf_samples[:, 1], vi_samples[:, 1]).pvalue,
+            0.05
         )
 
     def test_normal_normal(self):
@@ -59,4 +61,4 @@ class VariationalInferTest(unittest.TestCase):
 
         mu_approx = vi_dicts[mu()]
         sample_mean = mu_approx.sample((100, 1)).mean()
-        self.assertTrue(sample_mean > 2.0)
+        self.assertTrue(sample_mean > 8.0)

src/beanmachine/ppl/experimental/vi/VariationalInfer.py
@@ -29,20 +29,19 @@ class VariationalApproximation(dist.distribution.Distribution):
         return dict()
 
     def train(self, epochs=100, lr=1e-2):
-        dim = self.flow_stack.base_dist.event_shape[0] if len(self.flow_stack.base_dist.event_shape) == 1 else 1
-        sample_shape = (100, dim)
         optim = torch.optim.Adam(self.flow_stack.parameters(), lr=lr)
 
         for i in range(epochs):
-            z0, zk, mu, log_var, ldj = self.flow_stack(shape=sample_shape)
+            z0, zk, mu, log_var, ldj = self.flow_stack(shape=(100,))
+            n, d = z0.shape
+            std = torch.exp(0.5 * log_var)
 
             # negative ELBO loss
 
             # entropy H(Q)
-            loss = (
-                dist.Normal(mu, torch.exp(0.5 * log_var)).log_prob(z0).sum()
-            )  # Q(z_0)
-            loss -= ldj.sum()  # transport to Q(z_k) via - sum log det jac
+            loss = self.flow_stack.base_dist.log_prob((z0 - mu) / std).sum()  # Q((z_0 - mu)/sigma)
+            loss -= n * log_var.sum() / 2.0  # jac from standardizing z0
+            loss -= ldj.sum()  # change of variable zk -> z0
 
             # negative cross-entropy -H(Q,P)
             loss -= self.target_log_prob(zk).sum()