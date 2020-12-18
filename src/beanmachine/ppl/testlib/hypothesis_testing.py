03e43baf8e8f680446321244a6928d5d89bf22d5
Walid Taha <wtaha@fb.com>
2020-12-18 13:25:38 -0800

    Added definitions of test and confidence interval calculations for variance equality
    Differential Revision: D24626996
    
    fbshipit-source-id: 36f09be891b071ef72387c119a9ee80995354f9f

--- src/beanmachine/ppl/testlib/hypothesis_testing.py
+++ src/beanmachine/ppl/testlib/hypothesis_testing.py
@@ -16,10 +16,7 @@ def inverse_chi2_cdf(df, p):
     return stats.chi2(df).ppf(p)
 
 
-# The hypothesis test proper
-#
-# The definition of the function for performing the hypothesis test of
-# mean equality.
+# Hypothesis test for equality of sample mean to a true mean
 def mean_equality_hypothesis_test(
     sample_mean: torch.Tensor,
     true_mean: torch.Tensor,
@@ -28,9 +25,9 @@ def mean_equality_hypothesis_test(
     p_value,
 ):
     """Test for the null hypothesis that the mean of a Gaussian
-    distribution is within the central 1 - p-value confidence
-    interval (CI) for a sample of size sample_size. An adjustment
-    just takes into account that we do the test pointwise independently
+    distribution is within the central 1 - alpha confidence
+    interval (CI) for a sample of size sample_size. We also apply an adjustment
+    that takes into account that we do the test pointwise independently
     for each element of the tensor. This is basically the Dunn-Šidák
     correction,
     https://en.wikipedia.org/wiki/%C5%A0id%C3%A1k_correction"""
@@ -56,8 +53,8 @@ def mean_equality_hypothesis_confidence_interval(
 ):
     """Computes the central 1 - p_value confidence interval in which the sample mean
     can fall without causing us to reject the null hypothesis that the mean of
-    interval (CI) for a sample of size sample_size. An adjustment
-    just takes into account that we do the test pointwise independently
+    a Gaussian distribution for a sample of size sample_size. We also apply
+    an adjustment that takes into account that we do the test pointwise independently
     for each element of the tensor. This is basically the Dunn-Šidák
     correction,
     https://en.wikipedia.org/wiki/%C5%A0id%C3%A1k_correction"""
@@ -78,4 +75,67 @@ def mean_equality_hypothesis_confidence_interval(
     # functions should be matched for consistency
     lower_bound = true_mean - bound_std * z_score
     upper_bound = true_mean + bound_std * z_score
+    return lower_bound, upper_bound
+
+
+# Hypothesis test for equality of sample variance to a true variance
+def variance_equality_hypothesis_test(
+    sample_std: torch.Tensor,
+    true_std: torch.Tensor,
+    degrees_of_freedom: torch.Tensor,
+    alpha,
+):
+    """Test for the null hypothesis that the variance of a Gaussian
+    distribution is within the central 1 - alpha confidence
+    interval (CI) for a sample of effective sample size (ESS)
+    degrees_of_freedom. We also apply an adjustment that takes
+    into account that we do the test pointwise independently
+    for each element of the tensor. This is basically the Dunn-Šidák
+    correction,
+    https://en.wikipedia.org/wiki/%C5%A0id%C3%A1k_correction"""
+    if torch.min(degrees_of_freedom).item() <= 0:
+        return False
+    dimensions = torch.prod(torch.tensor(torch.Tensor.size(true_std))).item()
+    if dimensions == 0:
+        return False
+    if torch.max(true_std <= 0).item():
+        return False
+    adjusted_alpha = 1 - (1 - alpha) ** (1.0 / dimensions)
+    test_statistic = degrees_of_freedom * (sample_std / true_std) ** 2
+    lower_bound = inverse_chi2_cdf(degrees_of_freedom, adjusted_alpha / 2)
+    upper_bound = inverse_chi2_cdf(degrees_of_freedom, 1 - adjusted_alpha / 2)
+    lower_bound_result = lower_bound <= torch.min(test_statistic).item()
+    upper_bound_result = torch.max(test_statistic).item() <= upper_bound
+    test_result = lower_bound_result and upper_bound_result
+    return test_result
+
+
+# The following function explicitly constructs a confidence interval.
+# This provides an alternative way for performing the hypothesis test,
+# but which also makes reporting test failures easier.
+def variance_equality_hypothesis_confidence_interval(
+    true_std: torch.Tensor, degrees_of_freedom: torch.Tensor, alpha
+):
+    """Computes the central 1 - alpha confidence interval in which the sample
+    variance can fall without causing us to reject the null hypothesis that the variance
+    of a Gaussian distribution for a sample of size sample_size. We also apply
+    an adjustment that takes into account that we do the test pointwise independently
+    for each element of the tensor. This is basically the Dunn-Šidák
+    correction,
+    https://en.wikipedia.org/wiki/%C5%A0id%C3%A1k_correction"""
+    if torch.min(degrees_of_freedom).item() <= 0:
+        return None
+    dimensions = torch.prod(torch.tensor(torch.Tensor.size(true_std))).item()
+    if dimensions == 0:
+        return None
+    if torch.max(true_std == 0).item():
+        return None
+    adjusted_alpha = 1 - (1 - alpha) ** (1.0 / dimensions)
+    lower_bound = (
+        inverse_chi2_cdf(degrees_of_freedom, adjusted_alpha / 2) / degrees_of_freedom
+    ) ** 0.5 * true_std
+    upper_bound = (
+        inverse_chi2_cdf(degrees_of_freedom, 1 - adjusted_alpha / 2)
+        / degrees_of_freedom
+    ) ** 0.5 * true_std
     return lower_bound, upper_bound
