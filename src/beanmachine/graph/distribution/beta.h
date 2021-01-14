f32734e924a9bb82aec9b0ad1238c59983599dbe
Detian Deng <ddeng@fb.com>
2021-01-14 14:49:16 -0800

    Add backprop support to Beta distribution (#460)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/beanmachine/pull/460
    
    - added backward methods to Beta distribution
    
    Reviewed By: ericlippert
    
    Differential Revision: D24850108
    
    fbshipit-source-id: 082e9a7d13321877d6df3881723deff0800ea8d3

--- src/beanmachine/graph/distribution/beta.h
+++ src/beanmachine/graph/distribution/beta.h
@@ -13,6 +13,8 @@ class Beta : public Distribution {
   ~Beta() override {}
   double _double_sampler(std::mt19937& gen) const override;
   double log_prob(const graph::NodeValue& value) const override;
+  void log_prob_iid(const graph::NodeValue& value, Eigen::MatrixXd& log_probs)
+      const override;
   void gradient_log_prob_value(
       const graph::NodeValue& value,
       double& grad1,
@@ -21,17 +23,35 @@ class Beta : public Distribution {
       const graph::NodeValue& value,
       double& grad1,
       double& grad2) const override;
-
   void compute_jacobian_hessian(
       const graph::NodeValue& value,
       Eigen::Matrix<double, 1, 2>& jacobian,
       Eigen::Matrix2d& hessian) const;
-  void _gradient_log_prob_value(
-      const double& val,
+
+  void backward_value(
+      const graph::NodeValue& value,
+      graph::DoubleMatrix& back_grad,
+      double adjunct = 1.0) const override;
+  void backward_value_iid(
+      const graph::NodeValue& value,
+      graph::DoubleMatrix& back_grad) const override;
+  void backward_value_iid(
+      const graph::NodeValue& value,
+      graph::DoubleMatrix& back_grad,
+      Eigen::MatrixXd& adjunct) const override;
+
+  void backward_param(const graph::NodeValue& value, double adjunct = 1.0)
+      const override;
+  void backward_param_iid(const graph::NodeValue& value) const override;
+  void backward_param_iid(
+      const graph::NodeValue& value,
+      Eigen::MatrixXd& adjunct) const override;
+
+  static void _grad1_log_prob_value(
       double& grad1,
-      double& grad2,
-      const double& param_a,
-      const double& param_b) const;
+      double x,
+      double param_a,
+      double param_b);
 };
 
 } // namespace distribution
