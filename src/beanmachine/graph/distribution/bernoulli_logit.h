4e95fafc55177ceb92a507514c2ed1fb8be189d2
Detian Deng <ddeng@fb.com>
2021-01-14 14:53:16 -0800

    Update the interface of backward_param_iid and backward_value_iid (#465)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/beanmachine/pull/465
    
    - Added log_prob_iid to Distribution
    - Updated the interface of backward_param_iid and backward_value_iid of Distribution to support backprop thru Bimixture.
    
    Differential Revision: D25075542
    
    fbshipit-source-id: a917cf02c30f971e64ebf265d2117d983132e364

--- src/beanmachine/graph/distribution/bernoulli_logit.h
+++ src/beanmachine/graph/distribution/bernoulli_logit.h
@@ -13,6 +13,8 @@ class BernoulliLogit : public Distribution {
   ~BernoulliLogit() override {}
   bool _bool_sampler(std::mt19937& gen) const override;
   double log_prob(const graph::NodeValue& value) const override;
+  void log_prob_iid(const graph::NodeValue& value, Eigen::MatrixXd& log_probs)
+      const override;
   void gradient_log_prob_value(
       const graph::NodeValue& value,
       double& grad1,
@@ -26,14 +28,20 @@ class BernoulliLogit : public Distribution {
       const graph::NodeValue& /* value */,
       graph::DoubleMatrix& /* back_grad */,
       double /* adjunct */ = 1.0) const override {}
-  void backward_param(const graph::NodeValue& value, double adjunct = 1.0)
-      const override;
+  void backward_value_iid(
+      const graph::NodeValue& /* value */,
+      graph::DoubleMatrix& /* back_grad */) const override {}
   void backward_value_iid(
       const graph::NodeValue& /* value */,
       graph::DoubleMatrix& /* back_grad */,
-      double /* adjunct */ = 1.0) const override {}
-  void backward_param_iid(const graph::NodeValue& value, double adjunct = 1.0)
+      Eigen::MatrixXd& /* adjunct */) const override {}
+
+  void backward_param(const graph::NodeValue& value, double adjunct = 1.0)
       const override;
+  void backward_param_iid(const graph::NodeValue& value) const override;
+  void backward_param_iid(
+      const graph::NodeValue& value,
+      Eigen::MatrixXd& adjunct) const override;
 
   static double _grad1_log_prob_param(bool x, double l);
 };
