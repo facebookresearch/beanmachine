db910978211cf2d8ee6c3028ca69ee3fae59a31d
Detian Deng <ddeng@fb.com>
2021-01-14 14:51:52 -0800

    Add backward methods to Bernoulli distribution
    Summary: - added backward methods to Bernoulli distribution
    
    Differential Revision: D24768951
    
    fbshipit-source-id: 6e1bb8fda11efe5b221240e4f7368d17d0270a6f

--- src/beanmachine/graph/distribution/bernoulli.cpp
+++ src/beanmachine/graph/distribution/bernoulli.cpp
@@ -32,7 +32,31 @@ bool Bernoulli::_bool_sampler(std::mt19937& gen) const {
 
 double Bernoulli::log_prob(const graph::NodeValue& value) const {
   double prob = in_nodes[0]->value._double;
-  return value._bool ? std::log(prob) : std::log(1 - prob);
+
+  if (value.type.variable_type == graph::VariableType::SCALAR) {
+    return value._bool ? std::log(prob) : std::log(1 - prob);
+  } else if (
+      value.type.variable_type == graph::VariableType::BROADCAST_MATRIX) {
+    int size = value._bmatrix.size();
+    int n_positive = value._bmatrix.count();
+    return std::log(prob) * n_positive +
+        std::log(1 - prob) * (size - n_positive);
+  } else {
+    throw std::runtime_error(
+        "Bernoulli::log_prob applied to invalid variable type");
+  }
+}
+
+void Bernoulli::log_prob_iid(
+    const graph::NodeValue& value,
+    Eigen::MatrixXd& log_probs) const {
+  assert(value.type.variable_type == graph::VariableType::BROADCAST_MATRIX);
+  double prob = in_nodes[0]->value._double;
+  double pos_val = std::log(prob);
+  double neg_val = std::log(1 - prob);
+  log_probs = Eigen::MatrixXd::Constant(
+      value._bmatrix.rows(), value._bmatrix.cols(), neg_val);
+  log_probs = value._bmatrix.select(pos_val, log_probs);
 }
 
 // Note: Bernoulli log_prob is defined as  x log(p) + (1-x) log(1-p)
@@ -41,27 +65,74 @@ double Bernoulli::log_prob(const graph::NodeValue& value) const {
 // grad w.r.t p is   (x/p) * p' - ((1-x)/(1-p)) * p'
 // grad2 w.r.t. p is -(x/p^2) * p'^2 + (x/p) * p'' - ((1-x)/(1-p)^2) * p'^2 -
 // ((1-x)/(1-p)) * p''
+// First order chain rule: f(g(x))' = f'(g(x)) g'(x),
+// - In backward propagation, f'(g(x)) is given by adjunct, the above equation
+// computes g'(x). [g is the current function f is the final target]
+// - In forward propagation, g'(x) is given by in_nodes[x]->grad1,
+// the above equation computes f'(g) [f is the current function g is the input]
 void Bernoulli::gradient_log_prob_value(
     const graph::NodeValue& /* value */,
     double& grad1,
     double& /* grad2 */) const {
+  assert(value.type.variable_type == graph::VariableType::SCALAR);
   double prob = in_nodes[0]->value._double;
   grad1 += std::log(prob) - std::log(1 - prob);
   // grad2 += 0
+}
+
+double Bernoulli::_grad1_log_prob_param(bool x, double p) {
+  return x ? 1 / p : -1 / (1 - p);
 }
 
 void Bernoulli::gradient_log_prob_param(
     const graph::NodeValue& value,
     double& grad1,
     double& grad2) const {
+  assert(value.type.variable_type == graph::VariableType::SCALAR);
   double val = value._bool ? 1.0 : 0.0;
   double prob = in_nodes[0]->value._double;
   double prob1 = in_nodes[0]->grad1;
   double prob2 = in_nodes[0]->grad2;
-  grad1 += ((val / prob) - ((1 - val) / (1 - prob))) * prob1;
+  grad1 += _grad1_log_prob_param(value._bool, prob) * prob1;
   grad2 += ((val / prob) - ((1 - val) / (1 - prob))) * prob2 +
       (-(val / (prob * prob)) - ((1 - val) / ((1 - prob) * (1 - prob)))) *
           prob1 * prob1;
+}
+
+void Bernoulli::backward_param(const graph::NodeValue& value, double adjunct)
+    const {
+  assert(value.type.variable_type == graph::VariableType::SCALAR);
+  if (in_nodes[0]->needs_gradient()) {
+    bool x = value._bool;
+    double prob = in_nodes[0]->value._double;
+    in_nodes[0]->back_grad1._double += adjunct * _grad1_log_prob_param(x, prob);
+  }
+}
+
+void Bernoulli::backward_param_iid(const graph::NodeValue& value) const {
+  assert(value.type.variable_type == graph::VariableType::BROADCAST_MATRIX);
+  if (in_nodes[0]->needs_gradient()) {
+    double prob = in_nodes[0]->value._double;
+    int size = value._bmatrix.size();
+    int n_positive = value._bmatrix.count();
+    in_nodes[0]->back_grad1._double +=
+        (1 / prob * n_positive - 1 / (1 - prob) * (size - n_positive));
+  }
+}
+
+void Bernoulli::backward_param_iid(
+    const graph::NodeValue& value,
+    Eigen::MatrixXd& adjunct) const {
+  assert(value.type.variable_type == graph::VariableType::BROADCAST_MATRIX);
+  if (in_nodes[0]->needs_gradient()) {
+    double prob = in_nodes[0]->value._double;
+    double sum_adjunct = adjunct.sum();
+    double sum_pos_adjunct =
+        (value._bmatrix.cast<double>().array() * adjunct.array()).sum();
+    in_nodes[0]->back_grad1._double +=
+        (1 / prob * sum_pos_adjunct -
+         1 / (1 - prob) * (sum_adjunct - sum_pos_adjunct));
+  }
 }
 
 } // namespace distribution
--- src/beanmachine/graph/distribution/bernoulli.h
+++ src/beanmachine/graph/distribution/bernoulli.h
@@ -13,6 +13,8 @@ class Bernoulli : public Distribution {
   ~Bernoulli() override {}
   bool _bool_sampler(std::mt19937& gen) const override;
   double log_prob(const graph::NodeValue& value) const override;
+  void log_prob_iid(const graph::NodeValue& value, Eigen::MatrixXd& log_probs)
+      const override;
   void gradient_log_prob_value(
       const graph::NodeValue& value,
       double& grad1,
@@ -21,6 +23,27 @@ class Bernoulli : public Distribution {
       const graph::NodeValue& value,
       double& grad1,
       double& grad2) const override;
+
+  void backward_value(
+      const graph::NodeValue& /* value */,
+      graph::DoubleMatrix& /* back_grad */,
+      double /* adjunct */ = 1.0) const override {}
+  void backward_value_iid(
+      const graph::NodeValue& /* value */,
+      graph::DoubleMatrix& /* back_grad */) const override {}
+  void backward_value_iid(
+      const graph::NodeValue& /* value */,
+      graph::DoubleMatrix& /* back_grad */,
+      Eigen::MatrixXd& /* adjunct */) const override {}
+
+  void backward_param(const graph::NodeValue& value, double adjunct = 1.0)
+      const override;
+  void backward_param_iid(const graph::NodeValue& value) const override;
+  void backward_param_iid(
+      const graph::NodeValue& value,
+      Eigen::MatrixXd& adjunct) const override;
+
+  static double _grad1_log_prob_param(bool x, double p);
 };
 
 } // namespace distribution
