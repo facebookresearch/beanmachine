ed7ec0d2073aab057e67d03fd056ba6fad22b10e
Detian Deng <ddeng@fb.com>
2021-01-14 14:48:03 -0800

    Add backprop support to Bimixture distribution (#457)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/beanmachine/pull/457
    
    - Added backward_param, backward_value, backward_param_iid, backward_value_iid and log_prob_iid methods to Bimixture distribution.
    - Updated the backward_vector_linearmodel test case.
    - Added tests for bimixture of normal, half cauchy, bernoulli-logit.
    
    Differential Revision: D24795121
    
    fbshipit-source-id: cb46d0e2e47132f7575e4e18249ac566c4f4cb30

--- src/beanmachine/graph/distribution/bimixture.h
+++ src/beanmachine/graph/distribution/bimixture.h
@@ -20,6 +20,8 @@ class Bimixture : public Distribution {
   graph::natural_t _natural_sampler(std::mt19937& gen) const override;
 
   double log_prob(const graph::NodeValue& value) const override;
+  void log_prob_iid(const graph::NodeValue& value, Eigen::MatrixXd& log_probs)
+      const override;
   void gradient_log_prob_value(
       const graph::NodeValue& value,
       double& grad1,
@@ -28,6 +30,25 @@ class Bimixture : public Distribution {
       const graph::NodeValue& value,
       double& grad1,
       double& grad2) const override;
+
+  void backward_value(
+      const graph::NodeValue& value,
+      graph::DoubleMatrix& back_grad,
+      double adjunct = 1.0) const override;
+  void backward_value_iid(
+      const graph::NodeValue& value,
+      graph::DoubleMatrix& back_grad) const override;
+  void backward_value_iid(
+      const graph::NodeValue& value,
+      graph::DoubleMatrix& back_grad,
+      Eigen::MatrixXd& adjunct) const override;
+
+  void backward_param(const graph::NodeValue& value, double adjunct = 1.0)
+      const override;
+  void backward_param_iid(const graph::NodeValue& value) const override;
+  void backward_param_iid(
+      const graph::NodeValue& value,
+      Eigen::MatrixXd& adjunct) const override;
 };
 
 } // namespace distribution
