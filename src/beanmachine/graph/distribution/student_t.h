d6e5f803aa092473999f1f60c700a4bbe6d5b1f8
Detian Deng <ddeng@fb.com>
2021-01-14 14:49:38 -0800

    Add backprop support to T distribution (#458)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/beanmachine/pull/458
    
    - Add backprop support to T distribution
    
    Reviewed By: ericlippert
    
    Differential Revision: D24804978
    
    fbshipit-source-id: fe4a13383d356d4d71bc89630a832a2ce0832c9f

--- src/beanmachine/graph/distribution/student_t.h
+++ src/beanmachine/graph/distribution/student_t.h
@@ -13,6 +13,8 @@ class StudentT : public Distribution {
   ~StudentT() override {}
   double _double_sampler(std::mt19937& gen) const override;
   double log_prob(const graph::NodeValue& value) const override;
+  void log_prob_iid(const graph::NodeValue& value, Eigen::MatrixXd& log_probs)
+      const override;
   void gradient_log_prob_value(
       const graph::NodeValue& value,
       double& grad1,
@@ -21,6 +23,36 @@ class StudentT : public Distribution {
       const graph::NodeValue& value,
       double& grad1,
       double& grad2) const override;
+
+  void backward_value(
+      const graph::NodeValue& value,
+      graph::DoubleMatrix& back_grad,
+      double adjunct = 1.0) const override;
+  void backward_value_iid(
+      const graph::NodeValue& value,
+      graph::DoubleMatrix& back_grad) const override;
+  void backward_value_iid(
+      const graph::NodeValue& value,
+      graph::DoubleMatrix& back_grad,
+      Eigen::MatrixXd& adjunct) const override;
+
+  void backward_param(const graph::NodeValue& value, double adjunct = 1.0)
+      const override;
+  void backward_param_iid(const graph::NodeValue& value) const override;
+  void backward_param_iid(
+      const graph::NodeValue& value,
+      Eigen::MatrixXd& adjunct) const override;
+
+  static void _grad1_log_prob_value(
+      double& grad1,
+      double x,
+      double n,
+      double l,
+      double n_s_sq_p_x_m_l_sq);
+  static double _grad1_log_prob_n(double n, double s, double n_s_sq_p_x_m_l_sq);
+  static double
+  _grad1_log_prob_l(double x, double n, double l, double n_s_sq_p_x_m_l_sq);
+  static double _grad1_log_prob_s(double n, double s, double n_s_sq_p_x_m_l_sq);
 };
 
 } // namespace distribution
