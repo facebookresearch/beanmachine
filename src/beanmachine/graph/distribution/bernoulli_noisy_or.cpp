73286a6a92f1d96b4ffc741ad48d61357700dfcf
Lily Li <yucenli@fb.com>
2021-01-26 11:51:16 -0800

    Add backprop support for BernoulliNoisyOr
    Summary: add support for BernoulliNoisyOr
    
    Reviewed By: ericlippert
    
    Differential Revision: D25601961
    
    fbshipit-source-id: 2dd8da6cbcd3dd797455d3e2362714de1a3b9f5e

--- src/beanmachine/graph/distribution/bernoulli_noisy_or.cpp
+++ src/beanmachine/graph/distribution/bernoulli_noisy_or.cpp
@@ -50,11 +50,32 @@ bool BernoulliNoisyOr::_bool_sampler(std::mt19937& gen) const {
 
 double BernoulliNoisyOr::log_prob(const graph::NodeValue& value) const {
   double param = in_nodes[0]->value._double;
-  if (not value._bool) {
-    return -param;
+  int size;
+  double sum_x, result;
+
+  if (value.type.variable_type == graph::VariableType::SCALAR) {
+    size = 1;
+    sum_x = (double)value._bool;
+  } else if (
+      value.type.variable_type == graph::VariableType::BROADCAST_MATRIX) {
+    size = value._bmatrix.size();
+    sum_x = value._bmatrix.count();
   } else {
-    return log1mexpm(param);
+    throw std::runtime_error(
+        "Normal::log_prob applied to invalid variable type");
   }
+  result = sum_x * log1mexpm(param) + (size - sum_x) * (-param);
+  return result;
+}
+
+void BernoulliNoisyOr::log_prob_iid(
+    const graph::NodeValue& value,
+    Eigen::MatrixXd& log_probs) const {
+  double param = in_nodes[0]->value._double;
+  double logterm = log1mexpm(param);
+  Eigen::MatrixXd value_double = value._bmatrix.cast<double>();
+  log_probs =
+      value_double.array() * logterm + (1 - value_double.array()) * (-param);
 }
 
 // x ~ BernoulliNoisyOr(y) = Bernoulli(1 - exp(-y))
@@ -85,6 +106,44 @@ void BernoulliNoisyOr::gradient_log_prob_param(
   grad1 += grad_param * in_nodes[0]->grad1;
   grad2 += grad2_param * in_nodes[0]->grad1 * in_nodes[0]->grad1 +
       grad_param * in_nodes[0]->grad2;
+}
+
+void BernoulliNoisyOr::backward_param(
+    const graph::NodeValue& value,
+    double adjunct) const {
+  assert(value.type.variable_type == graph::VariableType::SCALAR);
+  if (in_nodes[0]->needs_gradient()) {
+    double param = in_nodes[0]->value._double;
+    double mexpm1m = -std::expm1(-param); // 1 - exp(-param)
+    double val = (double)value._bool;
+    double grad_param = val / mexpm1m - 1;
+    in_nodes[0]->back_grad1._double += adjunct * grad_param;
+  }
+}
+
+void BernoulliNoisyOr::backward_param_iid(const graph::NodeValue& value) const {
+  assert(value.type.variable_type == graph::VariableType::BROADCAST_MATRIX);
+  if (in_nodes[0]->needs_gradient()) {
+    double param = in_nodes[0]->value._double;
+    double mexpm1m = -std::expm1(-param); // 1 - exp(-param)
+    double val_sum = (double)value._bmatrix.count();
+    int size = value._bmatrix.size();
+    in_nodes[0]->back_grad1._double += val_sum / mexpm1m - size;
+  }
+}
+
+void BernoulliNoisyOr::backward_param_iid(
+    const graph::NodeValue& value,
+    Eigen::MatrixXd& adjunct) const {
+  assert(value.type.variable_type == graph::VariableType::BROADCAST_MATRIX);
+  if (in_nodes[0]->needs_gradient()) {
+    double param = in_nodes[0]->value._double;
+    double mexpm1m = -std::expm1(-param); // 1 - exp(-param)
+    double sum_adjunct = adjunct.sum();
+    double sum_x_adjunct =
+        (value._bmatrix.cast<double>().array() * adjunct.array()).sum();
+    in_nodes[0]->back_grad1._double += sum_x_adjunct / mexpm1m - sum_adjunct;
+  }
 }
 
 } // namespace distribution
--- src/beanmachine/graph/distribution/bernoulli_noisy_or.h
+++ src/beanmachine/graph/distribution/bernoulli_noisy_or.h
@@ -17,6 +17,8 @@ class BernoulliNoisyOr : public Distribution {
   ~BernoulliNoisyOr() override {}
   bool _bool_sampler(std::mt19937& gen) const override;
   double log_prob(const graph::NodeValue& value) const override;
+  void log_prob_iid(const graph::NodeValue& value, Eigen::MatrixXd& log_probs)
+      const override;
   void gradient_log_prob_value(
       const graph::NodeValue& value,
       double& grad1,
@@ -25,6 +27,23 @@ class BernoulliNoisyOr : public Distribution {
       const graph::NodeValue& value,
       double& grad1,
       double& grad2) const override;
+  void backward_value(
+      const graph::NodeValue& /* value */,
+      graph::DoubleMatrix& /* back_grad */,
+      double /* adjunct */ = 1.0) const override {}
+  void backward_value_iid(
+      const graph::NodeValue& /* value */,
+      graph::DoubleMatrix& /* back_grad */) const override {}
+  void backward_value_iid(
+      const graph::NodeValue& /* value */,
+      graph::DoubleMatrix& /* back_grad */,
+      Eigen::MatrixXd& /* adjunct */) const override {}
+  void backward_param(const graph::NodeValue& value, double adjunct = 1.0)
+      const override;
+  void backward_param_iid(const graph::NodeValue& value) const override;
+  void backward_param_iid(
+      const graph::NodeValue& value,
+      Eigen::MatrixXd& adjunct) const override;
 };
 
 } // namespace distribution
