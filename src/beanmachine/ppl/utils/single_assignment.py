b4618c955bb2b3e75729b952fcb0cb5589a59f47
Walid Taha <wtaha@fb.com>
2021-02-03 12:00:29 -0800

    Assignment LHS is attribute ref
    Summary: This diff implements one of the rules for converting the left hand side (LHS) of an assignment form to SSA. The particular rule performs conversions such as a.b.c = z → x = a.b; x.c = z
    
    Differential Revision: D26213748
    
    fbshipit-source-id: 5c3dbbb19efefdbaf3fc8e73c1cde34ebbfd4e06

--- src/beanmachine/ppl/utils/single_assignment.py
+++ src/beanmachine/ppl/utils/single_assignment.py
@@ -1878,10 +1878,33 @@ class SingleAssignment:
             ]
         )
 
+    # TODO: We need to find a good way to order things in this file
+
     def _handle_left_value_all(self) -> Rule:
         """Put the left_value of an assignment in SSA form"""
         # TODO: Add the various rewrite rules into the following list
-        return first([])
+        return first([self._handle_left_value_attributeref()])
+
+    def _handle_left_value_attributeref(self) -> Rule:
+        """Rewrites like a.b.c = z → x = a.b; x.c = z"""
+        return PatternRule(
+            assign(targets=[attribute(value=_not_identifier)], value=name()),
+            self._transform_with_name(
+                "x",
+                lambda source_term: source_term.targets[0].value,
+                lambda source_term, new_name: ast.Assign(
+                    targets=[
+                        ast.Attribute(
+                            value=ast.Name(id=new_name, ctx=ast.Load()),
+                            attr=source_term.targets[0].attr,
+                            ctx=ast.Store(),
+                        )
+                    ],
+                    value=source_term.value,
+                ),
+            ),
+            "handle_left_value_attributeref",
+        )
 
     def single_assignment(self, node: ast.AST) -> ast.AST:
         return self._rules(node).expect_success()
