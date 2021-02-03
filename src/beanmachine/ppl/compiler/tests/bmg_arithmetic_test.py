22ae7c3dae96b2eebf60a1946f9c4e63ac4d0055
Eric Lippert <ericlippert@fb.com>
2021-02-03 06:58:46 -0800

    Add support for expm1 operator to BM->BMG compiler (#579)
    Summary:
    Pull Request resolved: https://github.com/facebookincubator/beanmachine/pull/579
    
    (Normally I split adding a new operator up into multiple diffs but we've been down this road so many times now that I feel OK about doing it all at once, even though that makes a rather large diff.)
    
    In this diff I add support to the Beanstalk compiler for the `expm1()` operator, which has the semantics of `exp(x) - 1`.
    
    We support this operator directly in BMG, and so it is straightforward to turn applications of this function in a model into the BMG node.
    
    Note that we do not detect instances of, say `rv().exp() - 1.0` and turn them into `expm1` nodes. We could do so by adding appropriate code to `fix_problems.py`. If `rv()` is of type negative real or positive real, it might be worthwhile to do so because we can get a more precise type analysis in these cases.
    
    Reviewed By: wtaha
    
    Differential Revision: D26187219
    
    fbshipit-source-id: ca876a746a29d5c0e7060f3845197822e7a4cd1b

---
+++ src/beanmachine/ppl/compiler/tests/bmg_arithmetic_test.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3
+# Copyright (c) Facebook, Inc. and its affiliates.
+# BM -> BMG compiler arithmetic tests
+
+import unittest
+
+import beanmachine.ppl as bm
+import torch
+from beanmachine.ppl.inference.bmg_inference import BMGInference
+from torch.distributions import Beta, HalfCauchy, Normal
+
+
+@bm.random_variable
+def beta():
+    return Beta(2.0, 2.0)
+
+
+@bm.random_variable
+def norm():
+    return Normal(0.0, 1.0)
+
+
+@bm.random_variable
+def hc():
+    return HalfCauchy(1.0)
+
+
+@bm.functional
+def expm1_prob():
+    return beta().expm1()
+
+
+@bm.functional
+def expm1_real():
+    return torch.expm1(norm())
+
+
+@bm.functional
+def expm1_negreal():
+    return torch.Tensor.expm1(-hc())
+
+
+class BMGArithmeticTest(unittest.TestCase):
+    def test_bmg_arithmetic_expm1(self) -> None:
+        self.maxDiff = None
+
+        observed = BMGInference().to_dot([expm1_prob()], {})
+        expected = """
+digraph "graph" {
+  N0[label=2.0];
+  N1[label=Beta];
+  N2[label=Sample];
+  N3[label=ToPosReal];
+  N4[label=ExpM1];
+  N5[label=Query];
+  N0 -> N1;
+  N0 -> N1;
+  N1 -> N2;
+  N2 -> N3;
+  N3 -> N4;
+  N4 -> N5;
+}"""
+        self.assertEqual(observed.strip(), expected.strip())
+
+        observed = BMGInference().to_dot([expm1_real()], {})
+        expected = """
+digraph "graph" {
+  N0[label=0.0];
+  N1[label=1.0];
+  N2[label=Normal];
+  N3[label=Sample];
+  N4[label=ExpM1];
+  N5[label=Query];
+  N0 -> N2;
+  N1 -> N2;
+  N2 -> N3;
+  N3 -> N4;
+  N4 -> N5;
+}"""
+        self.assertEqual(observed.strip(), expected.strip())
+
+        observed = BMGInference().to_dot([expm1_negreal()], {})
+        expected = """
+digraph "graph" {
+  N0[label=1.0];
+  N1[label=HalfCauchy];
+  N2[label=Sample];
+  N3[label="-"];
+  N4[label=ExpM1];
+  N5[label=Query];
+  N0 -> N1;
+  N1 -> N2;
+  N2 -> N3;
+  N3 -> N4;
+  N4 -> N5;
+}"""
+        self.assertEqual(observed.strip(), expected.strip())
