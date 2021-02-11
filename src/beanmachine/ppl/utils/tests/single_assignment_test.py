bd4627cec39092a03868d88eb669175a3bd930f1
Walid Taha <wtaha@fb.com>
2021-02-11 14:41:22 -0800

    Subscript rewrite may be too eager
    Summary: This diff simply gives an example of a rewrite that we may not wish to do. The comments on the rewrite rule suggest that the intention was to first check that the thing being sliced is itself a variable before this rule can be applied. But the rule itself does not do that check. The plan is to implement the fix in an upcoming diff.
    
    Differential Revision: D26412157
    
    fbshipit-source-id: 2afdbdfde6f7a828fd84e3a0d6a634fc1a18e9ad

--- src/beanmachine/ppl/utils/tests/single_assignment_test.py
+++ src/beanmachine/ppl/utils/tests/single_assignment_test.py
@@ -2661,3 +2661,20 @@ def f(x):
         self.check_rewrites(terms, self.s._handle_assign_subscript_slice_index_2())
         self.check_rewrites(terms, self.s._handle_assign_subscript_slice_all())
         self.check_rewrites(terms)
+
+    def test_assign_subscript_slice_index_too_soon(self) -> None:
+        """Test rewrites like a,b = c[d.e] → x = d.e; a,b = c[x]."""
+
+        terms = [
+            """
+def f(x):
+    a,b = c.c[d.e]""",
+            """
+def f(x):
+    a1 = d.e
+    a, b = c.c[a1]""",
+        ]
+
+        self.check_rewrites(terms, self.s._handle_assign_subscript_slice_index_2())
+        # self.check_rewrites(terms, self.s._handle_assign_subscript_slice_all())
+        # self.check_rewrites(terms)
