811f6e2c4315860a9ead6934b07727326531fc5e
Eric Lippert <ericlippert@fb.com>
2021-03-06 19:59:03 -0800

    Fix memoization of class constructors using a metaclass
    Summary:
    We wish to ensure the invariant that the `BooleanMatrix(1, 1)` object used to track the type information about graph nodes is reference equal to every such construction. I previously enforced this invariant by putting a memoize decorator on the `BooleanMatrix` class, but learned recently that this is wrong, wrong, wrong.
    
    The reason it is wrong is because when you memoize a class, you are basically saying that `BooleanMatrix` is no longer a class; it is the *function* returned by the memoize decorator, and that thing is not a valid argument to `instanceof`, it is not a valid base class, and so on.
    
    In an upcoming diff I will need to do `instanceof` testing on these objects, so I need to fix this now.
    
    The correct thing to do when memoizing a constructor is to make a memoizer metaclass. This is a tricky technique for the novice to intermediate Python programmer so I have left a short but detailed tutorial in the comments.
    
    Reviewed By: wtaha
    
    Differential Revision: D26792222
    
    fbshipit-source-id: 2edc12b7fddfa13e2f1b68e3c6b70bd8a7646cfc

--- src/beanmachine/ppl/utils/memoize.py
+++ src/beanmachine/ppl/utils/memoize.py
@@ -99,3 +99,70 @@ def memoize(f):
     else:
         f._wrapper = wrapper
     return wrapper
+
+
+# In Python, how do we memoize a constructor to ensure that instances of
+# a class with the same constructor arguments are reference-equal? We could
+# put the @memoize attribute on the class, but this leads to many problems.
+#
+# ASIDE: What problems? And why?
+#
+# A class is a function that constructs instances; a decorator is a function
+# from functions to functions. "@memoize class C: ..." passes the instance-
+# construction function to the decorator and assigns the result to C; this means
+# that C is no longer a *type*; it is the *function* returned by the decorator.
+# This in turn means that "instanceof(c, C)" no longer works because C is not a type.
+# Similarly C cannot be a base class because it is not a type. And so on.
+#
+# END ASIDE
+#
+# The correct way to do this in Python is to create a metaclass. A class is a factory
+# for instances; a metaclass is a factory for classes. We can create a metaclass which
+# produces classes that are memoized.
+#
+# The default metaclass in Python is "type"; if you call "type(name, bases, attrs)"
+# where name is the name of the new type, bases is a tuple of base types, and attrs
+# is a dictionary of name-value pairs, then you get back a new class with that name,
+# base classes, and attributes.  We can derive from type to make new metaclasses:
+
+
+class MemoizedClass(type):
+    # __new__ is called when the metaclass creates a new class.
+    # metacls is the "self" of the metaclass
+    # name is the name of the class we're creating
+    # bases is a tuple of base types
+    # attrs is a dictionary of attributes
+    def __new__(
+        metacls, name: str, bases: Tuple[type, ...], attrs: Dict[str, Any]
+    ) -> type:
+        # The memoized values will be stored in a per-class dictionary called
+        # _cache, so make sure that the attributes dictionary has that.
+        if "_cache" not in attrs:
+            attrs["_cache"] = {}
+        # That's the only special thing we need to do, so defer the actual
+        # type creation to the "type" metaclass -- our base type.
+        return super(MemoizedClass, metacls).__new__(metacls, name, bases, attrs)
+
+    # A class is a function which constructs instances; when that function is
+    # called to construct an instance, the __call__ handler is invoked in the
+    # metaclass. By default type.__call__ simply creates a new instance. We
+    # can replace that behavior by overriding the __call__ handler to do something
+    # else.
+    #
+    # cls is the class that we are trying to create an instance of; *args is
+    # the argument list passed to the constructor.
+    def __call__(cls, *args):
+        # TODO: We do not collect statistics on memoization use here.
+        # TODO: We do not canonicalize arguments as the memoizer does above.
+        if args not in cls._cache:
+            # This is the first time we've constructed this class with these
+            # arguments. Defer to the __call__ behavior of "type", which is the
+            # superclass of this metaclass.
+            new_instance = super(MemoizedClass, cls).__call__(*args)
+            cls._cache[args] = new_instance
+            return new_instance
+        return cls._cache[args]
+
+
+# You then use this as
+# class Foo(FooBase, metaclass=MemoizedClass): ...
