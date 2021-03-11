cf67375cdb2db48f69b8051b55c7c820cc4794f6
Feynman Liang <feynman.liang@gmail.com>
2021-03-10 16:16:40 -0800

    Handle multivariate and continuous constrained support distributions in mean-field variational inference (#478)
    Summary:
    ### Motivation
    Rebased on https://github.com/facebookincubator/beanmachine/pull/464; adds support for multivariate distributions and a test case for Bayesian robust regression.
    
    ### Changes proposed
    
    `VariationalApproximation` now initializes an independent product of `base_distribution`s, replicating arguments
    
    It is assumed (1) each node's `event_shape` does not change between `World`s, and (2) tensor computations in models respect an optional leading batch dimension semantic (could be improved with https://pytorch.org/docs/master/generated/torch.vmap.html).
    
    Pull Request resolved: https://github.com/facebookincubator/beanmachine/pull/478
    
    Test Plan:
    `py.test src/beanmachine/ppl/experimental/tests/vi/vi_test.py `
    
    ### Types of changes
    - [ ] Docs change / refactoring / dependency upgrade
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [x] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing functionality to change)
    
    ### Checklist
    - [x] My code follows the code style of this project.
    - [ ] My change requires a change to the documentation.
    - [ ] I have updated the documentation accordingly.
    - [x] I have read the **[CONTRIBUTING](https://github.com/facebookincubator/BeanMachine/blob/master/CONTRIBUTING.md)** document.
    - [x] I have added tests to cover my changes.
    - [x] All new and existing tests passed.
    - [x] The title of my pull request is a short description of the requested changes.
    
    Differential Revision: D25506632
    
    Pulled By: feynmanliang
    
    fbshipit-source-id: 647a0729ed8fad10d8a8f2870c833ae3932185b7

--- src/beanmachine/ppl/experimental/vi/variational_infer.py
+++ src/beanmachine/ppl/experimental/vi/variational_infer.py
@@ -1,8 +1,8 @@
 import copy
 import logging
 from abc import ABCMeta
-from collections import defaultdict
-from typing import Dict, List, Optional
+from functools import lru_cache
+from typing import Callable, Dict, List, Optional
 
 import torch
 import torch.distributions as dist
@@ -13,7 +13,7 @@ from tqdm.auto import tqdm
 from ...inference.abstract_infer import AbstractInference
 from ...model.rv_identifier import RVIdentifier
 from ...model.utils import LogLevel
-from .variational_approximation import VariationalApproximation
+from .mean_field_variational_approximation import MeanFieldVariationalApproximation
 
 
 LOGGER = logging.getLogger("beanmachine.vi")
@@ -28,18 +28,17 @@ class MeanFieldVariationalInference(AbstractInference, metaclass=ABCMeta):
     given `base_dist`.
     """
 
-    def infer(
+    def infer(  # noqa: C901
         self,
         queries: List[RVIdentifier],
         observations: Dict[RVIdentifier, Tensor],
         num_iter: int = 100,
-        num_flows: int = 8,
-        lr: float = 1e-2,
+        lr: float = 1e-3,
         base_dist: Optional[dist.Distribution] = None,
         base_args: Optional[dict] = None,
         random_seed: Optional[int] = None,
         num_elbo_mc_samples=100,
-    ) -> Dict[RVIdentifier, VariationalApproximation]:
+    ) -> Callable[[RVIdentifier], MeanFieldVariationalApproximation]:
         """
         Trains a set of mean-field variational approximation (one per site).
 
@@ -50,14 +49,14 @@ class MeanFieldVariationalInference(AbstractInference, metaclass=ABCMeta):
         :param queries: queried random variables
         :param observations: observations dict
         :param num_iter: number of  worlds to train over
-        :param num_flows: number of flow layers
         :param lr: learning rate
         :param base_dist: constructor fn for base distribution for flow
         :param base_args: arguments to base_dist (will optimize any `nn.Parameter`s)
         """
         if not base_dist:
             base_dist = dist.Normal
-            base_args = {"loc": torch.tensor(0.0), "scale": torch.tensor(1.0)}
+            base_args = {"loc": torch.tensor([0.0]), "scale": torch.tensor([1.0])}
+            # TODO: reinterpret batch dimension?
         if not base_args:
             base_args = {}
         try:
@@ -71,11 +70,15 @@ class MeanFieldVariationalInference(AbstractInference, metaclass=ABCMeta):
             self.queries_ = queries
             self.observations_ = observations
 
-            # TODO: handle dimension
-            vi_dicts = defaultdict(
-                lambda: VariationalApproximation(
-                    num_flows=num_flows,
+            vi_dicts = lru_cache(maxsize=None)(
+                lambda rvid: MeanFieldVariationalApproximation(
                     lr=lr,
+                    event_shape=self.world_.get_node_in_world_raise_error(  # pyre-fixme[16]
+                        rvid
+                    ).distribution.event_shape,
+                    target_support=self.world_.get_node_in_world_raise_error(  # pyre-fixme[16]
+                        rvid
+                    ).distribution.support,
                     base_dist=base_dist,
                     base_args=copy.deepcopy(base_args),
                 )
@@ -92,7 +95,7 @@ class MeanFieldVariationalInference(AbstractInference, metaclass=ABCMeta):
                 # iterate over latent sites x_s.
                 for rvid in latent_rvids:
                     node_var = nodes[rvid]
-                    v_approx = vi_dicts[rvid]
+                    v_approx = vi_dicts(rvid)
 
                     # decompose mean-field ELBO expectation E_x = E_\s E_s and
                     # MC approximate E_\s using previously sampled x_\s, i.e.
@@ -106,7 +109,6 @@ class MeanFieldVariationalInference(AbstractInference, metaclass=ABCMeta):
                         for child in node_var.children:
                             child_var = self.world_.get_node_in_world_raise_error(child)
                             log_prob += child_var.log_prob
-                        # reject the diff here to re-use world x ~ q_t
                         self.world_.reject_diff()
                         return log_prob
 
@@ -114,13 +116,20 @@ class MeanFieldVariationalInference(AbstractInference, metaclass=ABCMeta):
                     # samples x_{s,i} ~ q_t(x_s) i.e.
                     # ELBO ~= E_s log p(x_s, x_\s) / q(x_s)
                     #      ~= (1/N) \sum_i^N log p(x_{s,i}, x_\s) / q(x_{s,i})
-                    loss -= v_approx.elbo(_target_log_prob, num_elbo_mc_samples)
-                if not torch.isnan(loss):
-                    loss.backward()
+                    loss -= v_approx.elbo(
+                        _target_log_prob,
+                        num_elbo_mc_samples,
+                    )
+
+                if not torch.isnan(loss) and not torch.isinf(loss):
                     for rvid in latent_rvids:
-                        v_approx = vi_dicts[rvid]
-                        v_approx.optim.step()
+                        v_approx = vi_dicts(rvid)
                         v_approx.optim.zero_grad()
+                    loss.backward(retain_graph=True)
+                    for rvid in latent_rvids:
+                        v_approx = vi_dicts(rvid)
+                        v_approx.optim.step()
+                        v_approx.recompute_transformed_distribution()
                 else:
                     # TODO: caused by e.g. negative scales in `dist.Normal`;
                     # fix using pytorch's `constraint_registry` to account for
