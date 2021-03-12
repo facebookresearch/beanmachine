b3cefa1f049463671a6b426841f0220df3a646aa
Walid Taha <wtaha@fb.com>
2021-03-11 17:40:53 -0800

    Adding a tensorized CLARA CGM model
    Summary: As part of the sprint to get a production CLARA model through Beanstalk, we are using several example CLARA models to investigate the feasibility of an end-to-end demo. This diff adds a model that the Beanstalk compiler can support.
    
    Differential Revision: D26856263
    
    fbshipit-source-id: 8b0da4dc523ae5c1223b1c733e4aed30fd47ea21

---
+++ src/beanmachine/ppl/compiler/tests/clara_tensor_cgm_no_update_test.py
@@ -0,0 +1,308 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+# """End-to-end test for a CLARA CGM model"""
+import unittest
+
+import beanmachine.ppl as bm
+import torch.distributions as dist
+from beanmachine.ppl.inference.bmg_inference import BMGInference
+from torch import tensor
+
+
+## New
+
+# DATA
+NUM_CLASS = 2  # num_classes (For Dirichlet may be need at least two)
+# TODO: Clarify number of lablers is implicit
+NUM_ITEMS = 1  # number of items
+PREV_PRIOR = tensor([1.0, 0.0])  # prior on prevalence
+# PREV_PRIOR is a list of length NUM_CLASSES
+CONF_MATRIX_PRIOR = tensor([1.0, 0.0])  # prior on confusion matrix
+# CONF_MATRIX_PRIOR is a list of length NUM_CLASS
+# TODO: Does Dirichlet support 2d matrices?
+# TODO: Is it really necessary to reject dirichlet on tensor([1])?
+IDX_RATINGS = [[0]]  # indexed ratings that labelers assigned to items
+IDX_LABELERS = [[0]]  # indexed list of labelers who labeled items
+EXPERT_CONF_MATRIX = tensor(
+    [[1, 0], [0, 1]]
+)  # confusion matrix of an expert (if we have true ratings)
+# EXPERT_CONF_MATRIX is of size NUM_CLASS x NUM_CLASS
+# Row (first) index is true class, and column (second) index is observed
+IDX_TRUE_RATINGS = [0]
+# Of size NUM_ITEMS
+# Represents true class of items by a perfect labler
+# When information is missing, use value -1
+
+# MODEL
+
+
+@bm.random_variable
+def prevalence():
+    # Dirichlet distribution support is implemented in Beanstalk but not yet landed.
+    return dist.Dirichlet(PREV_PRIOR)
+
+
+@bm.random_variable
+def confusion_matrix(labeler, true_class):
+    return dist.Dirichlet(CONF_MATRIX_PRIOR)  # size: NUM_CLASSES
+
+
+# log of the unnormalized item probs
+# log P(true label of item i = k | labels)
+# shape: [NUM_ITEMS, NUM_CLASSES]
+@bm.functional
+def log_item_prob(i, k):
+    # Indexing into a simplex with a constant is implemented
+    # but not yet landed
+    prob = prevalence()[k].log()
+    for r in range(len(IDX_RATINGS[i])):
+        label = IDX_RATINGS[i][r]
+        labeler = IDX_LABELERS[i][r]
+        prob = prob + confusion_matrix(labeler, k)[label].log()
+    if IDX_TRUE_RATINGS[i] != 0:  # TODO: This should actuall be -1
+        prob = prob + EXPERT_CONF_MATRIX[k, IDX_TRUE_RATINGS[i]].log()
+    return prob
+
+
+# log of joint prob of labels, prev, conf_matrix
+@bm.random_variable
+def target():
+    joint_log_prob = 0
+    for i in range(NUM_ITEMS):
+        # logsumexp on a newly-constructed tensor with stochastic
+        # elements has limited support but this should work:
+        log_probs = tensor(
+            # TODO: Hard-coded k in {0,1}
+            # [log_item_prob(i, 0), log_item_prob(i, 1), log_item_prob(i, 2)]
+            [log_item_prob(i, 0), log_item_prob(i, 1)]
+        )
+        joint_log_prob = joint_log_prob + log_probs.logsumexp(0)
+    return dist.Bernoulli(joint_log_prob.exp())
+
+
+observations = {target(): tensor(1.0)}
+queries = [
+    log_item_prob(0, 0),  # Ideally, all the other elements too
+    prevalence(),
+    confusion_matrix(0, 0),  # Ideally, all the other elements too
+]
+ssrw = "SingleSiteRandomWalk"
+bmgi = "BMG inference"
+both = {ssrw, bmgi}
+# TODO: Replace 4th param of expecteds by more methodical calculation
+expecteds = [
+    (prevalence(), both, 0.5000, 0.001),
+    (confusion_matrix(0, 0), both, 0.5000, 0.001),
+    (log_item_prob(0, 0), {ssrw}, -1.3863, 0.5),
+    (log_item_prob(0, 0), {bmgi}, -1.0391, 0.5),
+]
+
+
+class ClaraTest(unittest.TestCase):
+    def test_clara_tensor_cgm_no_update_inference(self) -> None:
+        """Check BM and BMG inference both terminate"""
+
+        self.maxDiff = None
+        num_samples = 10
+
+        # First, let's see how the model fairs with Random Walk inference
+        inference = bm.SingleSiteRandomWalk()  # or NUTS
+        mcsamples = inference.infer(queries, observations, num_samples)
+
+        for rand_var, inferences, value, delta in expecteds:
+            if ssrw in inferences:
+                samples = mcsamples[rand_var]
+                observed = samples.mean()
+                expected = tensor([value])
+                self.assertAlmostEqual(first=observed, second=expected, delta=delta)
+
+        # Second, let's see how it fairs with the bmg inference
+        inference = BMGInference()
+        mcsamples = inference.infer(queries, observations, num_samples)
+
+        for rand_var, inferences, value, delta in expecteds:
+            if bmgi in inferences:
+                samples = mcsamples[rand_var]
+                observed = samples.mean()
+                expected = tensor([value])
+                self.assertAlmostEqual(first=observed, second=expected, delta=delta)
+
+    def test_clara_tensor_cgm_no_update_to_dot_cpp_python(self) -> None:
+        self.maxDiff = None
+        observed = BMGInference().to_dot(queries, observations)
+        expected = """
+digraph "graph" {
+  N00[label="[1.0,0.0]"];
+  N01[label=Dirichlet];
+  N02[label=Sample];
+  N03[label=Sample];
+  N04[label=Sample];
+  N05[label=0];
+  N06[label=index];
+  N07[label=Log];
+  N08[label=index];
+  N09[label=Log];
+  N10[label="+"];
+  N11[label=1];
+  N12[label=index];
+  N13[label=Log];
+  N14[label=index];
+  N15[label=Log];
+  N16[label="+"];
+  N17[label=LogSumExp];
+  N18[label=ToReal];
+  N19[label=Exp];
+  N20[label=ToProb];
+  N21[label=Bernoulli];
+  N22[label=Sample];
+  N23[label="Observation True"];
+  N24[label=Query];
+  N25[label=Query];
+  N26[label=Query];
+  N00 -> N01;
+  N01 -> N02;
+  N01 -> N03;
+  N01 -> N04;
+  N02 -> N06;
+  N02 -> N12;
+  N02 -> N25;
+  N03 -> N08;
+  N03 -> N26;
+  N04 -> N14;
+  N05 -> N06;
+  N05 -> N08;
+  N05 -> N14;
+  N06 -> N07;
+  N07 -> N10;
+  N08 -> N09;
+  N09 -> N10;
+  N10 -> N17;
+  N10 -> N24;
+  N11 -> N12;
+  N12 -> N13;
+  N13 -> N16;
+  N14 -> N15;
+  N15 -> N16;
+  N16 -> N17;
+  N17 -> N18;
+  N18 -> N19;
+  N19 -> N20;
+  N20 -> N21;
+  N21 -> N22;
+  N22 -> N23;
+}
+        """
+        self.assertEqual(observed.strip(), expected.strip())
+
+        observed = BMGInference().to_cpp(queries, observations)
+        expected = """
+graph::Graph g;
+Eigen::MatrixXd m0(2, 1)
+m0 << 1.0, 0.0;
+uint n0 = g.add_constant_pos_matrix(m0);
+uint n1 = g.add_distribution(
+  graph::DistributionType::DIRICHLET,
+  graph::ValueType(
+    graph::VariableType::COL_SIMPLEX_MATRIX,
+    graph::AtomicType::PROBABILITY,
+    2,
+    1
+  )
+  std::vector<uint>({n0}));
+uint n2 = g.add_operator(
+  graph::OperatorType::SAMPLE, std::vector<uint>({n1}));
+uint n3 = g.add_operator(
+  graph::OperatorType::SAMPLE, std::vector<uint>({n1}));
+uint n4 = g.add_operator(
+  graph::OperatorType::SAMPLE, std::vector<uint>({n1}));
+uint n5 = g.add_constant(0);
+uint n6 = g.add_operator(
+  graph::OperatorType::INDEX, std::vector<uint>({n2, n5}));
+uint n7 = g.add_operator(
+  graph::OperatorType::LOG, std::vector<uint>({n6}));
+uint n8 = g.add_operator(
+  graph::OperatorType::INDEX, std::vector<uint>({n3, n5}));
+uint n9 = g.add_operator(
+  graph::OperatorType::LOG, std::vector<uint>({n8}));
+uint n10 = g.add_operator(
+  graph::OperatorType::ADD, std::vector<uint>({n7, n9}));
+uint n11 = g.add_constant(1);
+uint n12 = g.add_operator(
+  graph::OperatorType::INDEX, std::vector<uint>({n2, n11}));
+uint n13 = g.add_operator(
+  graph::OperatorType::LOG, std::vector<uint>({n12}));
+uint n14 = g.add_operator(
+  graph::OperatorType::INDEX, std::vector<uint>({n4, n5}));
+uint n15 = g.add_operator(
+  graph::OperatorType::LOG, std::vector<uint>({n14}));
+uint n16 = g.add_operator(
+  graph::OperatorType::ADD, std::vector<uint>({n13, n15}));
+n17 = g.add_operator(
+  graph::OperatorType::LOGSUMEXP,
+  std::vector<uint>({n10, n16}));
+uint n18 = g.add_operator(
+  graph::OperatorType::TO_REAL, std::vector<uint>({n17}));
+uint n19 = g.add_operator(
+  graph::OperatorType::EXP, std::vector<uint>({n18}));
+uint n20 = g.add_operator(
+  graph::OperatorType::TO_PROBABILITY, std::vector<uint>({n19}));
+uint n21 = g.add_distribution(
+  graph::DistributionType::BERNOULLI,
+  graph::AtomicType::BOOLEAN,
+  std::vector<uint>({n20}));
+uint n22 = g.add_operator(
+  graph::OperatorType::SAMPLE, std::vector<uint>({n21}));
+g.observe([n22], true);
+g.query(n10);
+g.query(n2);
+g.query(n3);
+"""
+        self.assertEqual(observed.strip(), expected.strip())
+
+        observed = BMGInference().to_python(queries, observations)
+        expected = """
+from beanmachine import graph
+from torch import tensor
+g = graph.Graph()
+n0 = g.add_constant_pos_matrix(tensor([1.0,0.0]))
+n1 = g.add_distribution(
+  graph.DistributionType.DIRICHLET,
+  graph.ValueType(
+    graph.VariableType.COL_SIMPLEX_MATRIX,
+    graph.AtomicType.PROBABILITY,
+    2,
+    1,
+  ),
+  [n0],
+)
+n2 = g.add_operator(graph.OperatorType.SAMPLE, [n1])
+n3 = g.add_operator(graph.OperatorType.SAMPLE, [n1])
+n4 = g.add_operator(graph.OperatorType.SAMPLE, [n1])
+n5 = g.add_constant(0)
+n6 = g.add_operator(graph.OperatorType.INDEX, [n2, n5])
+n7 = g.add_operator(graph.OperatorType.LOG, [n6])
+n8 = g.add_operator(graph.OperatorType.INDEX, [n3, n5])
+n9 = g.add_operator(graph.OperatorType.LOG, [n8])
+n10 = g.add_operator(graph.OperatorType.ADD, [n7, n9])
+n11 = g.add_constant(1)
+n12 = g.add_operator(graph.OperatorType.INDEX, [n2, n11])
+n13 = g.add_operator(graph.OperatorType.LOG, [n12])
+n14 = g.add_operator(graph.OperatorType.INDEX, [n4, n5])
+n15 = g.add_operator(graph.OperatorType.LOG, [n14])
+n16 = g.add_operator(graph.OperatorType.ADD, [n13, n15])
+n17 = g.add_operator(
+  graph.OperatorType.LOGSUMEXP,
+  [n10, n16])
+n18 = g.add_operator(graph.OperatorType.TO_REAL, [n17])
+n19 = g.add_operator(graph.OperatorType.EXP, [n18])
+n20 = g.add_operator(graph.OperatorType.TO_PROBABILITY, [n19])
+n21 = g.add_distribution(
+  graph.DistributionType.BERNOULLI,
+  graph.AtomicType.BOOLEAN,
+  [n20])
+n22 = g.add_operator(graph.OperatorType.SAMPLE, [n21])
+g.observe(n22, True)
+g.query(n10)
+g.query(n2)
+g.query(n3)
+        """
+        self.assertEqual(observed.strip(), expected.strip())
