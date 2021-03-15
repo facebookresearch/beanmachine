6e271b458eeb2a94efaf5c72cdd93e285abc8d43
Eric Lippert <ericlippert@fb.com>
2021-03-15 15:57:02 -0700

    Implement compiler nodes for all constant matrix types
    Summary:
    BMG has nodes representing constant 2d real, negative real, positive real, probability, natural and Boolean matrices; this diff adds all the previously unsupported node types to the graph accumulator and implements conversion logic to create the kind of matrix we need for any particular purpose.
    
    The test case demonstrates that we can create an index which draws from a matrix of reals, negative reals, positive reals, probabilities or naturals in order to route an element of that matrix to an input expecting that type, where the index is a coin flip.
    
    {F482071548}
    
    I also made the code generation output more consistent: when dumping python or C++ code that creates a graph, we always format tensors that will be used to create constant matrixes as a 2-d matrix in column major form.
    
    Reviewed By: wtaha
    
    Differential Revision: D26932342
    
    fbshipit-source-id: 4ef550ab5e84cd80752c97ab32236a5328b9642d

--- src/beanmachine/ppl/compiler/tests/index_test.py
+++ src/beanmachine/ppl/compiler/tests/index_test.py
@@ -26,12 +26,12 @@ def real():
 
 @bm.random_variable
 def pos_real():
-    return Normal(0.0, tensor([1.5, 1.5])[flip()])
+    return Normal(0.0, tensor([1.5, 2.5])[flip()])
 
 
 @bm.random_variable
 def neg_real():
-    return Bernoulli(tensor([-1.5, -1.5])[flip()].log())
+    return Bernoulli(tensor([-1.5, -2.5])[flip()].exp())
 
 
 @bm.random_variable
@@ -39,23 +39,26 @@ def prob():
     return Bernoulli(tensor([0.5, 0.25])[flip()])
 
 
-@bm.functional
+@bm.random_variable
 def natural():
-    return Binomial(0.5, tensor([2, 3])[flip()])
+    return Binomial(tensor([2, 3])[flip()], 0.75)
 
 
 class IndexTest(unittest.TestCase):
     def test_index_constant_vector_stochastic_index(self) -> None:
         self.maxDiff = None
 
-        observed = BMGInference().to_dot([pos_real()], {})
+        observed = BMGInference().to_dot(
+            [pos_real(), real(), neg_real(), prob(), natural()],
+            {},
+        )
         expected = """
 digraph "graph" {
   N00[label=0.5];
   N01[label=Bernoulli];
   N02[label=Sample];
   N03[label=0.0];
-  N04[label="[1.5,1.5]"];
+  N04[label="[1.5,2.5]"];
   N05[label=1];
   N06[label=0];
   N07[label=if];
@@ -63,6 +66,29 @@ digraph "graph" {
   N09[label=Normal];
   N10[label=Sample];
   N11[label=Query];
+  N12[label="[1.5,-1.5]"];
+  N13[label=index];
+  N14[label=1.0];
+  N15[label=Normal];
+  N16[label=Sample];
+  N17[label=Query];
+  N18[label="[-1.5,-2.5]"];
+  N19[label=index];
+  N20[label=Exp];
+  N21[label=Bernoulli];
+  N22[label=Sample];
+  N23[label=Query];
+  N24[label="[0.5,0.25]"];
+  N25[label=index];
+  N26[label=Bernoulli];
+  N27[label=Sample];
+  N28[label=Query];
+  N29[label="[2,3]"];
+  N30[label=index];
+  N31[label=0.75];
+  N32[label=Binomial];
+  N33[label=Sample];
+  N34[label=Query];
   N00 -> N01;
   N01 -> N02;
   N02 -> N07;
@@ -71,15 +97,32 @@ digraph "graph" {
   N05 -> N07;
   N06 -> N07;
   N07 -> N08;
+  N07 -> N13;
+  N07 -> N19;
+  N07 -> N25;
+  N07 -> N30;
   N08 -> N09;
   N09 -> N10;
   N10 -> N11;
+  N12 -> N13;
+  N13 -> N15;
+  N14 -> N15;
+  N15 -> N16;
+  N16 -> N17;
+  N18 -> N19;
+  N19 -> N20;
+  N20 -> N21;
+  N21 -> N22;
+  N22 -> N23;
+  N24 -> N25;
+  N25 -> N26;
+  N26 -> N27;
+  N27 -> N28;
+  N29 -> N30;
+  N30 -> N32;
+  N31 -> N32;
+  N32 -> N33;
+  N33 -> N34;
 }
 """
         self.assertEqual(expected.strip(), observed.strip())
-
-        # TODO: real
-        # TODO: neg_real
-        # TODO: prob
-        # TODO: natural
-        # TODO: Boolean
