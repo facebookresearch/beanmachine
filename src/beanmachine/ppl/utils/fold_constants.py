808dd8b5811ca842bd046e280e1ab3ebe2cd5907
Eric Lippert <ericlippert@fb.com>
2021-04-07 08:26:06 -0700

    Continue removing dead code
    Summary: I'm continuing to remove the dead code that has resulted from removing the last tests for the all-module compiler workflow.
    
    Reviewed By: wtaha
    
    Differential Revision: D27400582
    
    fbshipit-source-id: b6f0ce7cf56d5a844e87dcb837098c37e6ecf3df

--- src/beanmachine/ppl/utils/fold_constants.py
+++ src/beanmachine/ppl/utils/fold_constants.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """Tools to transform Bean Machine programs to Bean Machine Graph"""
 
+# TODO: Delete this module
+
 import ast
 import math
 from typing import Any, Callable, Dict, List
