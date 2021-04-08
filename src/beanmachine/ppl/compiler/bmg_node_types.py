033afaca4253c296facd68e0c6bc25efbabd4875
Eric Lippert <ericlippert@fb.com>
2021-04-08 10:13:32 -0700

    Move all BMG graph creation logic to one class
    Summary: My refactoring to put all the graph creation logic in one place, rather than spread out amongst BMGInference, BMGraphBuilder and all the node classes, is now complete.
    
    Reviewed By: wtaha
    
    Differential Revision: D27430467
    
    fbshipit-source-id: eb0b12ebf0b3c0738a8e0dfd0839f5325ec00ef4

---
+++ src/beanmachine/ppl/compiler/bmg_node_types.py
@@ -0,0 +1,81 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+# TODO: For reasons unknown, Pyre is unable to find type information about
+# TODO: beanmachine.graph from beanmachine.ppl.  I'll figure out why later;
+# TODO: for now, we'll just turn off error checking in this module.
+# pyre-ignore-all-errors
+
+
+from typing import Any, Tuple
+
+import beanmachine.ppl.compiler.bmg_nodes as bn
+from beanmachine.graph import (
+    AtomicType,
+    DistributionType as dt,
+    FactorType as ft,
+    OperatorType,
+    ValueType,
+    VariableType,
+)
+
+
+_factor_types = {
+    bn.ExpProductFactorNode: ft.EXP_PRODUCT,
+}
+
+
+def factor_type(node: bn.FactorNode) -> ft:
+    return _factor_types[type(node)]
+
+
+_dist_types = {
+    bn.BernoulliLogitNode: (dt.BERNOULLI_LOGIT, AtomicType.BOOLEAN),
+    bn.BernoulliNode: (dt.BERNOULLI, AtomicType.BOOLEAN),
+    bn.BetaNode: (dt.BETA, AtomicType.PROBABILITY),
+    bn.BinomialNode: (dt.BINOMIAL, AtomicType.NATURAL),
+    bn.DirichletNode: (dt.DIRICHLET, None),
+    bn.FlatNode: (dt.FLAT, AtomicType.PROBABILITY),
+    bn.GammaNode: (dt.GAMMA, AtomicType.POS_REAL),
+    bn.HalfCauchyNode: (dt.HALF_CAUCHY, AtomicType.POS_REAL),
+    bn.NormalNode: (dt.NORMAL, AtomicType.REAL),
+    bn.StudentTNode: (dt.STUDENT_T, AtomicType.REAL),
+}
+
+
+def dist_type(node: bn.DistributionNode) -> Tuple[dt, Any]:
+    t = type(node)
+    if t is bn.DirichletNode:
+        element_type = ValueType(
+            VariableType.COL_SIMPLEX_MATRIX,
+            AtomicType.PROBABILITY,
+            node._required_columns,
+            1,
+        )
+        return dt.DIRICHLET, element_type
+    return _dist_types[t]
+
+
+_operator_types = {
+    bn.AdditionNode: OperatorType.ADD,
+    bn.ComplementNode: OperatorType.COMPLEMENT,
+    bn.ExpM1Node: OperatorType.EXPM1,
+    bn.ExpNode: OperatorType.EXP,
+    bn.IfThenElseNode: OperatorType.IF_THEN_ELSE,
+    bn.IndexNode: OperatorType.INDEX,
+    bn.Log1mexpNode: OperatorType.LOG1MEXP,
+    bn.LogNode: OperatorType.LOG,
+    bn.LogisticNode: OperatorType.LOGISTIC,
+    bn.LogSumExpNode: OperatorType.LOGSUMEXP,
+    bn.MultiAdditionNode: OperatorType.ADD,
+    bn.MultiplicationNode: OperatorType.MULTIPLY,
+    bn.NegateNode: OperatorType.NEGATE,
+    bn.PhiNode: OperatorType.PHI,
+    bn.PowerNode: OperatorType.POW,
+    bn.SampleNode: OperatorType.SAMPLE,
+    bn.ToRealNode: OperatorType.TO_REAL,
+    bn.ToPositiveRealNode: OperatorType.TO_POS_REAL,
+    bn.ToProbabilityNode: OperatorType.TO_PROBABILITY,
+}
+
+
+def operator_type(node: bn.OperatorNode) -> OperatorType:
+    return _operator_types[type(node)]
