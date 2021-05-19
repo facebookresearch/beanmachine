be3ade722c040f892839bc40028207633516c020
Walid Taha <wtaha@fb.com>
2021-05-18 18:58:47 -0700

    Extending tests to be better localize problem in single_site_ancestral_mh_conjugate_test_nightly.py
    Summary: This diff introduces a set of ten seeds to run the various tests on. This further supports the idea that the issue is with distant_normal_normal case and not necessarily the algorithm itself. That test consistently fails on all ten randomly selected random_seeds. The specification of the test is now that the failure is reproducible on all ten random_seed values.
    
    Differential Revision: D28519753
    
    fbshipit-source-id: eb1d08551b429a3812edc3ed6bcd6a061f3a22ac

--- src/beanmachine/ppl/inference/tests/single_site_ancestral_mh_conjugate_test_nightly.py
+++ src/beanmachine/ppl/inference/tests/single_site_ancestral_mh_conjugate_test_nightly.py
@@ -10,23 +10,54 @@ class SingleSiteAncestralMetropolisHastingsConjugateTest(
 ):
     def setUp(self):
         self.mh = bm.SingleSiteAncestralMetropolisHastings()
+        self.seeds = {
+            123456789,
+            975318642,
+            564738291,
+            129634860,
+            193543287,
+            978956376,
+            867394610,
+            885573652,
+            846194628,
+            918273645,
+        }
 
     def test_beta_binomial_conjugate_run(self):
-        self.beta_binomial_conjugate_run(self.mh)
+        for seed in self.seeds:
+            self.beta_binomial_conjugate_run(self.mh, random_seed=seed)
 
     def test_gamma_gamma_conjugate_run(self):
-        self.gamma_gamma_conjugate_run(self.mh)
+        for seed in self.seeds:
+            self.gamma_gamma_conjugate_run(self.mh, random_seed=seed)
 
     def test_gamma_normal_conjugate_run(self):
-        self.gamma_normal_conjugate_run(self.mh, num_samples=20000)
+        for seed in self.seeds:
+            self.gamma_normal_conjugate_run(
+                self.mh, num_samples=20000, random_seed=seed
+            )
 
     def test_normal_normal_conjugate_run(self):
-        self.normal_normal_conjugate_run(self.mh, num_samples=5000)
+        for seed in self.seeds:
+            self.normal_normal_conjugate_run(
+                self.mh, num_samples=5000, random_seed=seed
+            )
 
-    @unittest.skip("Expect to fail. N_eff is 5 @ 1K sample size.")
     def test_distant_normal_normal_conjugate_run(self):
-        # We don't expect ancestral to be able to converge fast for this model.
-        self.distant_normal_normal_conjugate_run(self.mh, num_samples=1000)
+        # We expect ancestral to be able to converge fast for this model.
+        # This test consistently (that is, for all seeds tested) fails to converge
+        for seed in self.seeds:
+            try:
+                self.distant_normal_normal_conjugate_run(
+                    self.mh, num_samples=1000, random_seed=seed
+                )
+            except AssertionError as e:
+                msg_expected = "2.7625932693481445 not greater than or equal to 30 : Effective sample size too small for normalcy assumption. random_seed"
+                msg_observed = str(e).split(" = ")[0]
+                self.assertEquals(msg_expected, msg_observed)
 
     def test_dirichlet_categorical_conjugate_run(self):
-        self.dirichlet_categorical_conjugate_run(self.mh, num_samples=10000)
+        for seed in self.seeds:
+            self.dirichlet_categorical_conjugate_run(
+                self.mh, num_samples=10000, random_seed=seed
+            )
