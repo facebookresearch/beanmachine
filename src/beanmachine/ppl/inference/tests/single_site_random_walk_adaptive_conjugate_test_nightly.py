e0da3236ed5adfda1a274b570310e907930b598d
Walid Taha <wtaha@fb.com>
2021-05-18 18:59:00 -0700

    Unskipping tests in single_site_random_walk_adaptive_conjugate_test_nightly.py
    Summary: The previously skipped tests in this file seem to pass, so, this diff unskips those tests.
    
    Differential Revision: D28520273
    
    fbshipit-source-id: c98e77226572885050e6c60c24be2a6be4e992ec

--- src/beanmachine/ppl/inference/tests/single_site_random_walk_adaptive_conjugate_test_nightly.py
+++ src/beanmachine/ppl/inference/tests/single_site_random_walk_adaptive_conjugate_test_nightly.py
@@ -11,14 +11,11 @@ class SingleSiteAdaptiveRandomWalkConjugateTest(
     def setUp(self):
         self.mh = bm.SingleSiteRandomWalk(step_size=5.0)
 
-    # TODO: Test known to produce NaN for N_eff - see T77199353
-    @unittest.skip("Known to fail. Investigating in T77865889.")
     def test_beta_binomial_conjugate_run(self):
         self.mh = bm.SingleSiteRandomWalk(step_size=1.0)
         self.beta_binomial_conjugate_run(
             self.mh, num_samples=3000, num_adaptive_samples=1600
         )
-        # self.assertTrue(False)
 
     def test_gamma_gamma_conjugate_run(self):
         self.mh = bm.SingleSiteRandomWalk(step_size=3.0)
