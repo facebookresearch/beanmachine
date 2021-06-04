6741089758df83782966905059f73c1e4fb4df8c
Rodrigo de Salvo Braz <rodrigobraz@fb.com>
2021-06-04 07:20:42 -0700

    - refactor, rename and improve documentation of compute_descendants
    Differential Revision: D28790569
    
    fbshipit-source-id: 401c855b9e98e7f08d01be9fa30d3fa5ba05c2f5

--- src/beanmachine/graph/cavi.cpp
+++ src/beanmachine/graph/cavi.cpp
@@ -54,7 +54,8 @@ void Graph::cavi(
       // log_prob. We will call these nodes the log_prob_nodes.
       std::vector<uint> det_desc;
       std::vector<uint> logprob_nodes;
-      std::tie(det_desc, logprob_nodes) = compute_descendants(node_id, supp);
+      std::tie(det_desc, logprob_nodes) =
+          get_nodes_up_to_immediate_stochastic_descendants(node_id, supp);
       // In order to compute the log_prob of these nodes we need to
       // materialize their ancestors both deterministic and stochastic.
       // The unobserved stochastic ancestors are to be sampled while the
