0da3d82a0d8fb020f5da0ba77c7a63c9e0555534
Yuhao Yin <yuhaoyin@fb.com>
2021-07-29 14:12:27 -0700

    add patsy flexible statistical formula parser (#916)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/beanmachine/pull/916
    
    Pull Request resolved: https://github.com/facebookresearch/beanmachine/pull/915
    
    added Patsy-based flexible statistical formula parser to HME package, supporting categorical fixed effects and data pre-processing functionalities.
    
    Reviewed By: ToddSmall
    
    Differential Revision: D29745886
    
    fbshipit-source-id: 489650a1c8bdc26051b78ed73b28b6368d6c6540

--- src/beanmachine/applications/hme/abstract_model.py
+++ src/beanmachine/applications/hme/abstract_model.py
@@ -9,9 +9,11 @@ import beanmachine.graph as bmgraph
 import beanmachine.ppl.diagnostics.common_statistics as bm_diag_util
 import numpy as np
 import pandas as pd
+from patsy import ModelDesc, dmatrix, dmatrices, build_design_matrices
 import torch  # usort: skip # noqa: F401
 
 from .configs import InferConfig, ModelConfig
+from .patsy_mixed import evaluate_formula, RandomEffectsTerm
 
 
 logger = logging.getLogger("hme")
@@ -31,6 +33,7 @@ class AbstractModel(object, metaclass=ABCMeta):
         self.model_config = model_config
         self.queries = {}
         self.query_map = {}
+        self.design_infos = []
         self.g = bmgraph.Graph()
 
     @staticmethod
@@ -86,9 +89,75 @@ class AbstractModel(object, metaclass=ABCMeta):
 
     @abstractmethod
     def build_graph(self) -> None:
-        """Creates a bmgraph.Graph member for the model."""
+        """Creates a bmgraph.Graph() member for the model."""
 
         pass
+
+    def parse_formula_patsy(self, formula: str) -> Tuple[List, List]:
+        """Parses statistical formula as well as performs data pre-processing given statistical formula.
+
+        :param formula: statistical formula establishing the relationship between response and predictor variables
+        :type formula: str
+        :return: A tuple of fixed and/or random effects lists
+        :rtype: (list, list)
+        """
+
+        model_desc = evaluate_formula(formula)
+
+        fe_termlists, re_termlists = [], []
+        for term in model_desc.rhs_termlist:
+            if isinstance(term, RandomEffectsTerm):
+                re_termlists.append(term)
+            else:
+                fe_termlists.append(term)
+
+        fe_desc = ModelDesc(
+            lhs_termlist=model_desc.lhs_termlist, rhs_termlist=fe_termlists
+        )
+
+        # TODO: add support for data pre-processing on random effects
+        if model_desc.lhs_termlist:
+            y_dm, X_dm = dmatrices(fe_desc, self.data, return_type="dataframe")
+            self.design_infos += [
+                y_dm.design_info,
+                X_dm.design_info,
+            ]  # for data pre-processing as well as stateful transforms on test data
+        else:
+            X_dm = dmatrix(fe_desc, self.data, return_type="dataframe")
+            self.design_infos += [X_dm.design_info]
+
+        fixed_effects = X_dm.design_info.column_names
+
+        # TODO: add support for random slope
+        # for now, asssume ret.expr.rhs_termlist = [Term([])], i.e., random intercept (1|...)
+        random_effects = []
+        for ret in re_termlists:
+            re_termlist = ret.factor.rhs_termlist
+            re_factorlist = [term.factors for term in re_termlist]
+
+            for re_factor_tuple in re_factorlist:
+                re_key = tuple(re_factor.code for re_factor in re_factor_tuple)
+                if len(re_key) == 1:
+                    random_effects.append(re_key[0])
+                else:
+                    random_effects.append(re_key)
+
+        return fixed_effects, random_effects
+
+    def _preprocess_data(self) -> None:
+        """Performs data pre-processing: including one-hot encoding on categorical fixed effects,
+        as well as user-specified transformations, e.g., centering and standardization, on the outcome
+        and/or predictor variables, also concatenates pre-processed data to self.data.
+        """
+        data_dm = pd.concat(
+            build_design_matrices(
+                self.design_infos, self.data, return_type="dataframe"
+            ),
+            axis=1,
+        )
+        self.preprocessed_data = pd.concat([self.data, data_dm], axis=1).loc[
+            :, lambda df: np.logical_not(df.columns.duplicated())
+        ]
 
     def set_queries(self, manual_queries: Optional[Dict[str, Any]] = None) -> None:
         """Sets query for the model. Only posterior samples for the queried random variables (parameters) will be returned.
@@ -108,9 +177,9 @@ class AbstractModel(object, metaclass=ABCMeta):
 
         :param prev_key: None, or model parameter names of fixed or random effects
         :type prev_key: any
-        :param queries: a mapping from model parameter to its bmgraph node, or just node itself
+        :param queries: a mapping from model parameter to its BMGraph node, or just node itself
         :type queries: any
-        :param query_map: a mapping from model parameter to query-index specified in the bmgraph
+        :param query_map: a mapping from model parameter to query-index specified in the graph
         :type query_map: dict
         """
         if isinstance(queries, dict):
