32bef5ad3ebac491d67f48e4b7f2dc152dc69f9c
Rodrigo de Salvo Braz <rodrigobraz@fb.com>
2021-08-19 00:14:21 -0700

    Improve Dirichlet Beta stepper clarity
    Summary: General clarity improvements in Dirichlet Beta stepper in preparation of unifying it more with default scalar stepper.
    
    Reviewed By: yucenli
    
    Differential Revision: D30265353
    
    fbshipit-source-id: 0bccfc991f4da0ba24643f74f917da164df97df9

--- src/beanmachine/graph/stepper/nmc_dirichlet_beta_single_site_stepper.cpp
+++ src/beanmachine/graph/stepper/nmc_dirichlet_beta_single_site_stepper.cpp
@@ -28,10 +28,9 @@ bool NMCDirichletBetaSingleSiteStepper::is_applicable_to(
 }
 
 /*
-We treat the K-dimensional Dirichlet sample as K independent Gamma samples
-divided by their sum. i.e. Let X_k ~ Gamma(alpha_k, 1), for k = 1, ..., K,
-Y_k = X_k / sum(X), then (Y_1, ..., Y_K) ~ Dirichlet(alphas). We store Y in
-the attribute value, and X in unconstrainted_value.
+We base the 2-dimensional Dirichlet-distributed (Y_1, Y_2)
+as a deterministic function of a Beta-distributed X:
+(Y_1, Y_2) = (X, 1 - X)
 */
 void NMCDirichletBetaSingleSiteStepper::step(
     Node* tgt_node,
@@ -41,37 +40,47 @@ void NMCDirichletBetaSingleSiteStepper::step(
   assert(tgt_node->value._matrix.size() == 2);
   auto src_node = static_cast<oper::StochasticOperator*>(tgt_node);
   // @lint-ignore CLANGTIDY
-  auto param_a = src_node->in_nodes[0]->in_nodes[0]->value._matrix.coeff(0);
-  auto param_b = src_node->in_nodes[0]->in_nodes[0]->value._matrix.coeff(1);
-  double old_X_k;
-  // Prepare gradients
+  // in_node[0] is the Dirichlet distribution from which this node is sampled.
+  // in_node[0]
+  auto dirichlet_distribution = src_node->in_nodes[0];
+  auto dirichlet_parameters_node = dirichlet_distribution->in_nodes[0];
+  auto dirichlet_parameters_matrix = dirichlet_parameters_node->value._matrix;
+  auto param_a = dirichlet_parameters_matrix.coeff(0);
+  auto param_b = dirichlet_parameters_matrix.coeff(1);
+
+  // Prepare gradients of Y wrt X.
+  // Those are used by descendants to compute the log prod gradient
+  // with respect to X.
   // Grad1 = (dY_1/dX_1, dY_2/dX_1)
   // where dY_1/dX_1 = 1
-  //       dY_j/dX_k = -1
-  // Grad2 = (d^2Y_1/dX^2_1, d^2Y_2/X^2_1)
-  // where d2Y_k/dX2_k = 0
-  //       d2Y_j/dX2_k = 0
-  old_X_k = src_node->value._matrix.coeff(0);
+  //       dY_2/dX_1 = -1
+  // Grad2 = ( (d/dX_1)^2 Y_1, (d/dX_1)^2 Y_2 )
+  // where (d/dX_1)^2 Y_1 = 0
+  //       (d/dX_1)^2 Y_2 = 0
   Eigen::MatrixXd Grad1(2, 1);
   Grad1 << 1, -1;
   src_node->Grad1 = Grad1;
-  *(src_node->Grad1.data() + 1) = -1;
   src_node->Grad2 = Eigen::MatrixXd::Zero(2, 1);
 
   // Propagate gradients
-  NodeValue old_value(AtomicType::PROBABILITY, old_X_k);
+  auto old_X = src_node->value._matrix.coeff(0);
+  NodeValue old_value(AtomicType::PROBABILITY, old_X);
   nmc->save_old_values(det_nodes);
   nmc->compute_gradients(det_nodes);
   double old_sto_affected_nodes_log_prob = nmc->compute_log_prob_of(sto_nodes);
   auto old_prop = create_proposer_dirichlet_beta(
       sto_nodes, tgt_node, param_a, param_b, old_value);
 
+  // We sample a new value for X and update Y as a result.
   NodeValue new_value = nmc->sample(old_prop);
   *(src_node->value._matrix.data()) = new_value._double;
   *(src_node->value._matrix.data() + 1) = 1 - new_value._double;
 
-  src_node->Grad1 = Grad1;
-  src_node->Grad2 = Eigen::MatrixXd::Zero(2, 1);
+  // In general we would have to update Grad_i given the new X,
+  // but since they are constant functions, they remain the same as before.
+  // Showing it here for completeness:
+  // src_node->Grad1 = Grad1;
+  // src_node->Grad2 = Eigen::MatrixXd::Zero(2, 1);
   nmc->eval(det_nodes);
   nmc->compute_gradients(det_nodes);
 
@@ -85,8 +94,8 @@ void NMCDirichletBetaSingleSiteStepper::step(
   bool accepted = util::flip_coin_with_log_prob(nmc->gen, logacc);
   if (!accepted) {
     nmc->restore_old_values(det_nodes);
-    *(src_node->value._matrix.data()) = old_X_k;
-    *(src_node->value._matrix.data() + 1) = 1 - old_X_k;
+    *(src_node->value._matrix.data()) = old_X;
+    *(src_node->value._matrix.data() + 1) = 1 - old_X;
   }
   // Gradients are must be cleared (equal to 0)
   // at the end of each iteration.
@@ -110,7 +119,7 @@ NMCDirichletBetaSingleSiteStepper::create_proposer_dirichlet_beta(
   for (Node* node : sto_nodes) {
     if (node == tgt_node) {
       double x = value._double;
-      // X_k ~ Beta(param_a, param_b)
+      // X ~ Beta(param_a, param_b)
       grad1 += (param_a - 1) / x - (param_b - 1) / (1 - x);
       grad2 += -(param_a - 1) / (x * x) - (param_b - 1) / ((1 - x) * (1 - x));
     } else {
