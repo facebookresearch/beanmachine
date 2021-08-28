83c56db550c4d176378a93dbd584282f2edcb936
Rodrigo de Salvo Braz <rodrigobraz@fb.com>
2021-08-27 18:00:31 -0700

    Create a default implementation of NMC single-site steppers.
    Summary: Now we move the default integrated `step` implementation to a default class from which implementations will derive. In this first diff, we made NMCScalarSingleSiteSteppe derive from this default class, so it no longer needs to define `step`, and instead simply reuses it.
    
    Differential Revision: D30328318
    
    fbshipit-source-id: f69a0c960acaa060c4bf3439d320b4d0e7a6c290

---
+++ src/beanmachine/graph/stepper/nmc_default_single_site_stepper.h
@@ -0,0 +1,40 @@
+// Copyright (c) Facebook, Inc. and its affiliates.
+#pragma once
+#include "beanmachine/graph/graph.h"
+#include "beanmachine/graph/proposer/proposer.h"
+#include "beanmachine/graph/stepper/nmc_single_site_stepper.h"
+
+namespace beanmachine {
+namespace graph {
+
+/*
+ * An abstract default implementation of NMC single-site stepper
+ * implementing the typical MH step.
+ * It uses a proposal provided by method get_proposal_distribution,
+ * whose implementation is left to sub-classes.
+ * Sub-classes must also implement methods is_applicable_to and
+ * get_step_profiler_event.
+ */
+class NMCDefaultSingleSiteStepper : public NMCSingleSiteStepper {
+ public:
+  NMCDefaultSingleSiteStepper(Graph* graph, NMC* nmc)
+      : NMCSingleSiteStepper(graph, nmc) {}
+
+  virtual bool is_applicable_to(graph::Node* tgt_node) = 0;
+
+  virtual void step(
+      graph::Node* tgt_node,
+      const std::vector<graph::Node*>& det_affected_nodes,
+      const std::vector<graph::Node*>& sto_affected_nodes) override;
+
+ protected:
+  virtual std::unique_ptr<proposer::Proposer> get_proposal_distribution(
+      Node* tgt_node,
+      const std::vector<Node*>& det_affected_nodes,
+      const std::vector<Node*>& sto_affected_nodes) = 0;
+
+  virtual ProfilerEvent get_step_profiler_event() = 0;
+};
+
+} // namespace graph
+} // namespace beanmachine
--- src/beanmachine/graph/stepper/nmc_scalar_single_site_stepper.h
+++ src/beanmachine/graph/stepper/nmc_scalar_single_site_stepper.h
@@ -2,28 +2,25 @@
 #pragma once
 #include "beanmachine/graph/graph.h"
 #include "beanmachine/graph/proposer/proposer.h"
-#include "beanmachine/graph/stepper/nmc_single_site_stepper.h"
+#include "beanmachine/graph/stepper/nmc_default_single_site_stepper.h"
 
 namespace beanmachine {
 namespace graph {
 
-class NMCScalarSingleSiteStepper : public NMCSingleSiteStepper {
+class NMCScalarSingleSiteStepper : public NMCDefaultSingleSiteStepper {
  public:
   NMCScalarSingleSiteStepper(Graph* graph, NMC* nmc)
-      : NMCSingleSiteStepper(graph, nmc) {}
+      : NMCDefaultSingleSiteStepper(graph, nmc) {}
 
   virtual bool is_applicable_to(graph::Node* tgt_node) override;
 
-  virtual void step(
-      graph::Node* tgt_node,
-      const std::vector<graph::Node*>& det_affected_nodes,
-      const std::vector<graph::Node*>& sto_affected_nodes) override;
-
- private:
-  std::unique_ptr<proposer::Proposer> get_proposal_distribution(
+ protected:
+  virtual std::unique_ptr<proposer::Proposer> get_proposal_distribution(
       Node* tgt_node,
       const std::vector<Node*>& det_affected_nodes,
-      const std::vector<Node*>& sto_affected_nodes);
+      const std::vector<Node*>& sto_affected_nodes) override;
+
+  virtual ProfilerEvent get_step_profiler_event() override;
 };
 
 } // namespace graph
