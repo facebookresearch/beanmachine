98f4600d7300ba14cc0e37b40b8b962e24392745
Rodrigo de Salvo Braz <rodrigobraz@fb.com>
2021-08-27 19:11:15 -0700

    Refactoring dirichlet beta to concentrate gradient updating to method creating proposal distribution
    Summary: Renaming variables to get closer to scalar stepper code.
    
    Reviewed By: yucenli
    
    Differential Revision: D30322994
    
    fbshipit-source-id: 3e7bae0205245a354e791591937647521cbce6c2

--- src/beanmachine/graph/stepper/nmc_dirichlet_beta_single_site_stepper.cpp
+++ src/beanmachine/graph/stepper/nmc_dirichlet_beta_single_site_stepper.cpp
@@ -28,7 +28,7 @@ bool NMCDirichletBetaSingleSiteStepper::is_applicable_to(
 }
 
 /*
-We base the 2-dimensional Dirichlet-distributed (Y_1, Y_2)
+We treat the 2-dimensional Dirichlet-distributed (Y_1, Y_2)
 as a deterministic function of a Beta-distributed X:
 (Y_1, Y_2) = (X, 1 - X)
 */
@@ -40,40 +40,40 @@ void NMCDirichletBetaSingleSiteStepper::step(
   assert(tgt_node->value._matrix.size() == 2);
   auto sto_tgt_node = static_cast<oper::StochasticOperator*>(tgt_node);
 
-  auto old_X = sto_tgt_node->value._matrix.coeff(0);
-  NodeValue old_value(AtomicType::PROBABILITY, old_X);
-  nmc->save_old_values(det_affected_nodes);
-  double old_sto_affected_nodes_log_prob =
-      nmc->compute_log_prob_of(sto_affected_nodes);
-  auto old_prop = get_proposal_distribution(
+  auto proposal_distribution_given_old_value = get_proposal_distribution(
       sto_tgt_node, det_affected_nodes, sto_affected_nodes);
 
   // We sample a new value for X and update Y as a result.
-  NodeValue new_value = nmc->sample(old_prop);
+  NodeValue new_value = nmc->sample(proposal_distribution_given_old_value);
+
+  // Proto-reversibly_set_and_propagate
+  auto old_X = sto_tgt_node->value._matrix.coeff(0);
+  NodeValue x_node_value(AtomicType::PROBABILITY, old_X);
+  nmc->save_old_values(det_affected_nodes);
+  double old_sto_affected_nodes_log_prob =
+      nmc->compute_log_prob_of(sto_affected_nodes);
   *(sto_tgt_node->value._matrix.data()) = new_value._double;
   *(sto_tgt_node->value._matrix.data() + 1) = 1 - new_value._double;
-
-  // In general we would have to update Grad_i given the new X,
-  // but since they are constant functions, they remain the same as before.
-  // Showing it here for completeness:
-  // sto_tgt_node->Grad1 = Grad1;
-  // sto_tgt_node->Grad2 = Eigen::MatrixXd::Zero(2, 1);
   nmc->eval(det_affected_nodes);
-  nmc->compute_gradients(det_affected_nodes);
 
   double new_sto_affected_nodes_log_prob =
       nmc->compute_log_prob_of(sto_affected_nodes);
-  auto new_prop = get_proposal_distribution(
+
+  auto proposal_distribution_given_new_value = get_proposal_distribution(
       sto_tgt_node, det_affected_nodes, sto_affected_nodes);
+
   double logacc = new_sto_affected_nodes_log_prob -
-      old_sto_affected_nodes_log_prob + new_prop->log_prob(old_value) -
-      old_prop->log_prob(new_value);
+      old_sto_affected_nodes_log_prob +
+      proposal_distribution_given_new_value->log_prob(x_node_value) -
+      proposal_distribution_given_old_value->log_prob(new_value);
+
   bool accepted = util::flip_coin_with_log_prob(nmc->gen, logacc);
   if (!accepted) {
     nmc->restore_old_values(det_affected_nodes);
     *(sto_tgt_node->value._matrix.data()) = old_X;
     *(sto_tgt_node->value._matrix.data() + 1) = 1 - old_X;
   }
+
   // Gradients are must be cleared (equal to 0)
   // at the end of each iteration.
   // Some code relies on that to decide whether a node
@@ -99,7 +99,6 @@ NMCDirichletBetaSingleSiteStepper::get_proposal_distribution(
   auto dirichlet_parameters_matrix = dirichlet_parameters_node->value._matrix;
   auto param_a = dirichlet_parameters_matrix.coeff(0);
   auto param_b = dirichlet_parameters_matrix.coeff(1);
-
 
   // Propagate gradients
   // Prepare gradients of Y wrt X.
