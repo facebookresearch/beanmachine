2201ba8bca2f926ad426f4c9bae813b0b208d415
Lily Li <yucenli@fb.com>
2021-09-01 11:06:24 -0700

    global MH and proposer class (#973)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/beanmachine/pull/973
    
    create base classes for global MH algorithms and proposers
    
    Reviewed By: wtaha, rodrigodesalvobraz
    
    Differential Revision: D26826844
    
    fbshipit-source-id: c981f68a6ff5c8d94ba59ac3e0485ee97a86d25b

---
+++ src/beanmachine/graph/global/global_mh.h
@@ -0,0 +1,22 @@
+#include "beanmachine/graph/global/global_proposer.h"
+#include "beanmachine/graph/graph.h"
+
+namespace beanmachine {
+namespace graph {
+
+class GlobalMH {
+ public:
+  Graph& graph;
+  GlobalState state;
+  std::unique_ptr<GlobalProposer> proposer;
+
+  GlobalMH(Graph& g, uint seed);
+  std::vector<std::vector<NodeValue>>& infer(int num_samples, uint seed);
+  virtual void prepare_graph() {}
+  virtual void initialize_proposer() {}
+  void single_mh_step(GlobalState& state);
+  virtual ~GlobalMH() {}
+};
+
+} // namespace graph
+} // namespace beanmachine
