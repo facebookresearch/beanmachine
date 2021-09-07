Correct spelling of Jacobian
Summary: I happened to notice that "Jacobian" was misspelled in a few spots.

Reviewed By: horizon-blue

Differential Revision: D30736525

fbshipit-source-id: 8a38b063707098b5d7ff9cc1b2fe128d495ff504

src/beanmachine/ppl/experimental/neutra/iafflow.py
@@ -103,7 +103,7 @@ class InverseAutoregressiveFlow(nn.Module):
         len_j = len(x)
         log_jacobian = torch.zeros(len_j)
         for layer_ in self.flows_:
-            # loop through the NN to add log_abs_jacobain to ELBO
+            # loop through the NN to add log_abs_jacobian to ELBO
             tmp = z_f[-1][:]
             x, log_d = layer_.forward(tmp)
             elbo += log_d