Correct spelling of Jacobian
Summary: I happened to notice that "Jacobian" was misspelled in a few spots.

Reviewed By: horizon-blue

Differential Revision: D30736525

fbshipit-source-id: 9b5e61e0b5f3cd577eb203681a421b712e53c022

src/beanmachine/ppl/experimental/neutra/tests/iaflayer_test.py
@@ -29,7 +29,7 @@ class IAFLayerTest(unittest.TestCase):
         z = x * torch.exp(loga) + (1 - torch.exp(loga)) * mu
         # calculate if x->z is correct.
         self.assertCountEqual(xtrhat.tolist(), z.tolist())
-        # check if jacobain is correct.
+        # check if jacobian is correct.
         self.assertEqual(ja, loga.sum())
 
         # Here we don't expect to have a stable inverse direction,