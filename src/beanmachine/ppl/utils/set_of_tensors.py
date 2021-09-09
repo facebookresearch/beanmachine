3c184b1269deb26d15521c3411238b6e39270a72
Eric Lippert <ericlippert@fb.com>
2021-09-09 11:40:13 -0700

    Fix bugs in SetOfTensors
    Summary:
    My SetOfTensors class now does what it says on the tin: makes a deduplicated set of tensors.
    
    I have not implemented all the features you'd typically find in a set; I only need construction, length, iteration and membership so that's all I've done.
    
    Because tensors do not implement hashing or equality in the manner necessary to do set membership, I've created a wrapper around a set of tuples or values where the tuples/values have the same "shape" as the corresponding tensor.  I do this because tuples do implement hashing and equality, so I can make a set of tensor equivalents and convert them back to tensors on demand.
    
    Reviewed By: wtaha
    
    Differential Revision: D30115798
    
    fbshipit-source-id: d2359eaa0ca4297bb748f40f3e01284f09536f21

--- src/beanmachine/ppl/utils/set_of_tensors.py
+++ src/beanmachine/ppl/utils/set_of_tensors.py
@@ -1,48 +1,36 @@
 # Copyright (c) Facebook, Inc. and its affiliates.
 
 import collections
-from typing import List
+from typing import Set, Tuple
 
+from beanmachine.ppl.utils.memoize import tensor_to_tuple
 from torch import Tensor, tensor
 
-# When constructing the support of various nodes we are often
-# having to remove duplicates from a set of possible values.
+# When constructing the support of various nodes we often
+# must remove duplicates from a set of possible values.
 # Unfortunately, it is not easy to do so with torch tensors.
-# This helper class implements a set of tensors.
-
-# TODO: This code is wrong and needs to be fixed. The "in" operator
-# used in the code below throws an exception due to the way that
-# tensors implement equality.  For example:
-#
-# tensor([1, 2]) in [ tensor([2, 1]) ]
-#
-# raises "Boolean value of Tensor with more than one value is ambiguous"
-#
-# We need to implement proper hashing and equality on tensors for the purposes
-# of this set.
-#
-# We should also implement a sublinear set rather than using a list.
+# This helper class implements a set of tensors using the same
+# technique as is used in the function call memoizer: we encode
+# the data in the tensor into a tuple with the same shape. The
+# tuple implements hashing and equality correctly, so we can put
+# it in a set.
 
 
 class SetOfTensors(collections.abc.Set):
-    """Tensors cannot be put into a normal set because tensors that compare as
-    equal do not hash to equal hashes. This is a linear-time set implementation.
-    Most of the time the sets will be very small."""
-
-    elements: List[Tensor]
+    _elements: Set[Tuple]
 
     def __init__(self, iterable):
-        self.elements = []
+        self._elements = set()
         for value in iterable:
             t = value if isinstance(value, Tensor) else tensor(value)
-            if t not in self.elements:
-                self.elements.append(t)
+            self._elements.add(tensor_to_tuple(t))
 
     def __iter__(self):
-        return iter(self.elements)
+        return (tensor(t) for t in self._elements)
 
     def __contains__(self, value):
-        return value in self.elements
+        t = value if isinstance(value, Tensor) else tensor(value)
+        return tensor_to_tuple(t) in self._elements
 
     def __len__(self):
-        return len(self.elements)
+        return len(self._elements)
