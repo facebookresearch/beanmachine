4911b038439c06888878d800e10ca12ae5cab0d2
Lily Li <yucenli@fb.com>
2021-09-28 09:16:55 -0700

    initial conjugate test design
    Summary:
    Initial prototype for framework to run conjugate tests. This diff is mostly to brainstorm the design.
    
    I created `conjugate_util_test` to build different conjugate models and then run inference and compare with the true posterior.
    
    I still need to add quite a few different conjugate tests such as gamma normal, dirichlet categorical, etc, and also increase the complexity of the tests (such as including more variables in a single test).
    
    I also want to follow Bean Machine python's style of checking the mean and variance to be within a certain confidence interval rather than using "delta."
    
    Reviewed By: rodrigodesalvobraz
    
    Differential Revision: D30812121
    
    fbshipit-source-id: 8124da78d647f039696ceb995ec7df256f39c7f1

---
+++ src/beanmachine/graph/global/tests/conjugate_util_test.h
@@ -0,0 +1,24 @@
+#include "beanmachine/graph/global/global_mh.h"
+#include "beanmachine/graph/graph.h"
+
+namespace beanmachine {
+namespace graph {
+
+void build_gamma_gamma_model(Graph& g);
+void test_gamma_gamma_model(
+    GlobalMH& mh,
+    int num_samples = 5000,
+    int seed = 17,
+    int num_warmup_samples = 0,
+    double delta = 0.01);
+
+void build_normal_normal_model(Graph& g);
+void test_normal_normal_model(
+    GlobalMH& mh,
+    int num_samples = 5000,
+    int seed = 17,
+    int num_warmup_samples = 0,
+    double delta = 0.01);
+
+} // namespace graph
+} // namespace beanmachine
--- src/beanmachine/graph/global/tests/hmc_test.cpp
+++ src/beanmachine/graph/global/tests/hmc_test.cpp
@@ -1,82 +1,24 @@
 #include <gtest/gtest.h>
 
 #include "beanmachine/graph/global/hmc.h"
+#include "beanmachine/graph/global/tests/conjugate_util_test.h"
 #include "beanmachine/graph/graph.h"
 
 using namespace beanmachine;
 using namespace graph;
 
 TEST(testglobal, global_hmc_normal_normal) {
-  /*
-  p1 ~ Normal(0, 1)
-  p2 ~ Normal(p1, 1)
-  p3 ~ Normal(p1, 1)
-  p2 observed as 0.5
-  p3 observed as 1.5
-  posterior is Normal(0.66.., 0.5)
-  */
   Graph g;
-  uint zero = g.add_constant(0.0);
-  uint one = g.add_constant_pos_real(1.0);
-
-  uint norm_dist = g.add_distribution(
-      DistributionType::NORMAL, AtomicType::REAL, {zero, one});
-  uint sample = g.add_operator(OperatorType::SAMPLE, {norm_dist});
-
-  uint norm_norm_dist = g.add_distribution(
-      DistributionType::NORMAL, AtomicType::REAL, {sample, one});
-  uint obs1 = g.add_operator(OperatorType::SAMPLE, {norm_norm_dist});
-  uint obs2 = g.add_operator(OperatorType::SAMPLE, {norm_norm_dist});
-
-  g.observe(obs1, 0.5);
-  g.observe(obs2, 1.5);
-  g.query(sample);
-
-  uint seed = 17;
+  build_normal_normal_model(g);
   HMC mh = HMC(g, 1.0, 1.0);
-  std::vector<std::vector<NodeValue>> samples = mh.infer(10000, seed);
-  EXPECT_EQ(samples.size(), 10000);
-
-  double mean = 0;
-  for (int i = 0; i < samples.size(); i++) {
-    mean += samples[i][0]._double;
-  }
-  mean /= samples.size();
-  EXPECT_NEAR(mean, 2.0 / 3, 0.01);
+  test_normal_normal_model(mh);
 }
 
 TEST(testglobal, global_hmc_gamma_gamma) {
-  /*
-  p1 ~ Gamma(2, 2)
-  p2 ~ Gamma(1.5, p1)
-  p2 observed as 2
-  posterior is Gamma(3.5, 4)
-  */
   Graph g;
-  uint onepointfive = g.add_constant_pos_real(1.5);
-  uint two = g.add_constant_pos_real(2.0);
-
-  uint gamma_dist = g.add_distribution(
-      DistributionType::GAMMA, AtomicType::POS_REAL, {two, two});
-  uint sample = g.add_operator(OperatorType::SAMPLE, {gamma_dist});
-
-  uint gamma_gamma_dist = g.add_distribution(
-      DistributionType::GAMMA, AtomicType::POS_REAL, {onepointfive, sample});
-  uint obs1 = g.add_operator(OperatorType::SAMPLE, {gamma_gamma_dist});
-
-  g.observe(obs1, 2.0);
-  g.query(sample);
-
-  uint seed = 17;
+  build_gamma_gamma_model(g);
   HMC mh = HMC(g, 1.0, 0.5);
-  std::vector<std::vector<NodeValue>> samples = mh.infer(2000, seed);
-  EXPECT_EQ(samples.size(), 2000);
-  double mean = 0;
-  for (int i = 0; i < samples.size(); i++) {
-    mean += samples[i][0]._double;
-  }
-  mean /= samples.size();
-  EXPECT_NEAR(mean, 0.875, 0.03);
+  test_gamma_gamma_model(mh);
 }
 
 TEST(testglobal, global_hmc_half_cauchy) {
