e35c8281248bb38d3e43bfcceafa60caf12c4492
Walid Taha <wtaha@fb.com>
2021-10-01 14:16:34 -0700

    Starting to add a new test to test that our new support for MATRIX_SCALE is symmetric at the Python level
    Summary: Currently this is just a diff that has made a copy of the _1 test as a _2 test as a placeholder for a symmetric variant of _1.
    
    Reviewed By: ericlippert
    
    Differential Revision: D31295210
    
    fbshipit-source-id: f3eb47ac6819b2dbf60923f637b7f50f32ba8758

--- src/beanmachine/ppl/compiler/tests/fix_matrix_scale_test.py
+++ src/beanmachine/ppl/compiler/tests/fix_matrix_scale_test.py
@@ -90,7 +90,143 @@ digraph "graph" {
 
         _ = BMGInference().infer(queries, observations, num_samples=num_samples)
 
+    def test_fix_matrix_scale_1_sym(self) -> None:
+        self.maxDiff = None
+        observations = {}
+        queries = [scaled()]
+        num_samples = 1000
+        num_chains = 1
+
+        # Sanity check to make sure the model is valid
+        nmc = bm.SingleSiteNewtonianMonteCarlo()
+        _ = nmc.infer(
+            queries=queries,
+            observations=observations,
+            num_samples=num_samples,
+            num_chains=num_chains,
+        )
+
+        observed = BMGInference().to_dot(queries, observations, after_transform=False)
+
+        # The model before optimization
+
+        expected = """
+digraph "graph" {
+  N0[label=0.0];
+  N1[label=1.0];
+  N2[label=Normal];
+  N3[label=Sample];
+  N4[label="[20,40]"];
+  N5[label="*"];
+  N6[label=Query];
+  N0 -> N2;
+  N1 -> N2;
+  N2 -> N3;
+  N3 -> N5;
+  N4 -> N5;
+  N5 -> N6;
+}
+"""
+        self.assertEqual(expected.strip(), observed.strip())
+
+        # After optimization:
+
+        observed = BMGInference().to_dot(queries, observations, after_transform=True)
+        expected = """
+digraph "graph" {
+  N0[label=0.0];
+  N1[label=1.0];
+  N2[label=Normal];
+  N3[label=Sample];
+  N4[label="[20,40]"];
+  N5[label=MatrixScale];
+  N6[label=Query];
+  N0 -> N2;
+  N1 -> N2;
+  N2 -> N3;
+  N3 -> N5;
+  N4 -> N5;
+  N5 -> N6;
+}"""
+        self.assertEqual(expected.strip(), observed.strip())
+
+        # The model runs on Bean Machine Graph
+
+        _ = BMGInference().infer(queries, observations, num_samples=num_samples)
+
     def test_fix_matrix_scale_2(self) -> None:
+        self.maxDiff = None
+        observations = {}
+        queries = [scaled2()]
+        num_samples = 1000
+        num_chains = 1
+
+        # Sanity check to make sure the model is valid
+        nmc = bm.SingleSiteNewtonianMonteCarlo()
+        _ = nmc.infer(
+            queries=queries,
+            observations=observations,
+            num_samples=num_samples,
+            num_chains=num_chains,
+        )
+
+        observed = BMGInference().to_dot(queries, observations, after_transform=False)
+
+        # The model before optimization
+        expected = """
+digraph "graph" {
+  N0[label=0.0];
+  N1[label=1.0];
+  N2[label=Normal];
+  N3[label=Sample];
+  N4[label=Tensor];
+  N5[label="*"];
+  N6[label=Query];
+  N0 -> N2;
+  N1 -> N2;
+  N2 -> N3;
+  N3 -> N4;
+  N3 -> N4;
+  N3 -> N5;
+  N4 -> N5;
+  N5 -> N6;
+}
+"""
+        self.assertEqual(expected.strip(), observed.strip())
+
+        # After optimization:
+
+        observed = BMGInference().to_dot(queries, observations, after_transform=True)
+        expected = """
+digraph "graph" {
+  N0[label=0.0];
+  N1[label=1.0];
+  N2[label=Normal];
+  N3[label=Sample];
+  N4[label=2];
+  N5[label=1];
+  N6[label=ToMatrix];
+  N7[label=MatrixScale];
+  N8[label=Query];
+  N0 -> N2;
+  N1 -> N2;
+  N2 -> N3;
+  N3 -> N6;
+  N3 -> N6;
+  N3 -> N7;
+  N4 -> N6;
+  N5 -> N6;
+  N6 -> N7;
+  N7 -> N8;
+}
+"""
+        self.assertEqual(expected.strip(), observed.strip())
+
+        # The model runs on Bean Machine Graph
+
+        _ = BMGInference().infer(queries, observations, num_samples=num_samples)
+
+    def test_fix_matrix_scale_2_sym(self) -> None:
         self.maxDiff = None
         observations = {}
         queries = [scaled2()]
