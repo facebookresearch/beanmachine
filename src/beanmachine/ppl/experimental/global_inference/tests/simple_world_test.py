8173f402e1375b5f9436a46885a7a00ea55814ba
Xiaoyan Wang <xiaoyan0@fb.com>
2021-10-12 11:49:27 -0700

    Update graph structure when new values are assigned
    Differential Revision: D31039733
    
    fbshipit-source-id: 36de2c6ef856177662bec2dc14eb0f0e02d36941

--- src/beanmachine/ppl/experimental/global_inference/tests/simple_world_test.py
+++ src/beanmachine/ppl/experimental/global_inference/tests/simple_world_test.py
@@ -25,12 +25,27 @@ class DiscreteModel:
         return dist.Normal(self.foo().float(), torch.tensor(1.0))
 
 
+class DynamicModel:
+    @bm.random_variable
+    def foo(self):
+        return dist.Bernoulli(0.5)
+
+    @bm.random_variable
+    def bar(self, i: int):
+        return dist.Normal(0.0, 1.0)
+
+    @bm.random_variable
+    def baz(self):
+        mu = self.bar(int(self.foo()))
+        return dist.Normal(mu, 1.0)
+
+
 def test_basic_operations():
     model = SampleModel()
     observations = {model.bar(): torch.rand(())}
     world = SimpleWorld(observations=observations)
     assert world.observations == observations
-    assert world._variables == {}
+    assert len(world.latent_nodes) == 0
     assert len(world) == 0
 
     with world:
@@ -38,17 +53,17 @@ def test_basic_operations():
 
     assert len(world) == 2
     assert model.bar() in world
-    assert world._variables[model.bar()].transform == dist.identity_transform
+    assert world.get_variable(model.bar()).transform == dist.identity_transform
     assert world.latent_nodes == {model.foo()}
 
     # edge connection
-    assert model.foo() in world._variables[model.bar()].parents
-    assert model.bar() in world._variables[model.foo()].children
-    assert len(world._variables[model.bar()].children) == 0
-    assert len(world._variables[model.foo()].parents) == 0
+    assert model.foo() in world.get_variable(model.bar()).parents
+    assert model.bar() in world.get_variable(model.foo()).children
+    assert len(world.get_variable(model.bar()).children) == 0
+    assert len(world.get_variable(model.foo()).parents) == 0
 
     transformed_foo = world.get_transformed(model.foo())
-    assert world._variables[model.foo()].transform.inv(transformed_foo) == world.call(
+    assert world.get_variable(model.foo()).transform.inv(transformed_foo) == world.call(
         model.foo()
     )
 
@@ -68,9 +83,8 @@ def test_log_prob():
     world1.call(model.bar())
     log_prob1 = world1.log_prob()
 
-    world2 = world1.copy()
     # set to a value with extremely small probability
-    world2.set_transformed(model.bar(), torch.tensor(100.0))
+    world2 = world1.replace_transformed({model.bar(): torch.tensor(100.0)})
     log_prob2 = world2.log_prob()
 
     assert log_prob1 > log_prob2
@@ -82,3 +96,22 @@ def test_enumerate():
     with world:
         model.bar()
     assert (torch.tensor([0.0, 1.0, 2.0]) == world.enumerate_node(model.foo())).all()
+
+
+def test_change_parents():
+    model = DynamicModel()
+    world = SimpleWorld(initialize_fn=lambda d: torch.zeros_like(d.sample()))
+    with world:
+        model.baz()
+
+    assert model.foo() in world.get_variable(model.baz()).parents
+    assert model.bar(0) in world.get_variable(model.baz()).parents
+    assert model.bar(1) not in world.get_variable(model.baz()).parents
+    assert model.baz() in world.get_variable(model.bar(0)).children
+
+    world2 = world.replace_transformed({model.foo(): torch.tensor(1.0)})
+
+    assert model.bar(0) not in world2.get_variable(model.baz()).parents
+    assert model.bar(1) in world2.get_variable(model.baz()).parents
+    assert model.baz() in world2.get_variable(model.bar(1)).children
+    assert model.baz() not in world2.get_variable(model.bar(0)).children
