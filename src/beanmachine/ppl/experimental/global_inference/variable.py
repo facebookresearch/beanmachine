d43ebabae1b468205c5cdc4dfc493cfdadd1334c
Xiaoyan Wang <xiaoyan0@fb.com>
2021-10-14 12:31:29 -0700

    Caching distribution object in world
    Differential Revision: D31493782
    
    fbshipit-source-id: 502fd93412cd6afd264e43b9e1240a42705769c1

--- src/beanmachine/ppl/experimental/global_inference/variable.py
+++ src/beanmachine/ppl/experimental/global_inference/variable.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import dataclasses
+from functools import cached_property
 from typing import Set
 
 import torch
@@ -12,8 +13,20 @@ from beanmachine.ppl.model.rv_identifier import RVIdentifier
 class Variable:
     transformed_value: torch.Tensor
     transform: dist.Transform
+    distribution: dist.Distribution
     parents: Set[RVIdentifier] = dataclasses.field(default_factory=set)
     children: Set[RVIdentifier] = dataclasses.field(default_factory=set)
+
+    @cached_property
+    def log_prob(self) -> torch.Tensor:
+        try:
+            y = self.transformed_value
+            x = self.transform.inv(y)
+            return self.distribution.log_prob(x) - self.transform.log_abs_det_jacobian(
+                x, y
+            )
+        except (RuntimeError, ValueError):
+            return torch.tensor(float("-inf"), device=self.transformed_value.device)
 
     def replace(self, **changes) -> Variable:
         """Return a new Variable object with fields replaced by the changes"""
