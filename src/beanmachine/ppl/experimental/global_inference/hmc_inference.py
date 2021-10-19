9eadd6a395650cea0245f42ac5fd6a3857cad9c0
Xiaoyan Wang <xiaoyan0@fb.com>
2021-10-19 11:07:19 -0700

    get_proposers: return a list of proposers from a given world (#1065)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/beanmachine/pull/1065
    
    This diff implements the spawning of proposers as previously discussed in [this Quip doc](https://fb.quip.com/WSnUAIDqxHyx#temp:C:UfTd7281592b8e24fd3a6185969d).
    
    Before the change, the `get_proposer` method is only invoked once per chain. However, since the number of nods in `World` can change between iterations, we might need to instantiate new proposers in the middle of an inference. Starting from this diff, `get_proposers` is going to be invoked once per iteration, and the inference classes are responsible for deciding how and when to instantiate a new proposer.
    
    The logic inside `Sampler` is also updated to support running a list of proposers. Since a proposer could be instantiate in the middle of an inference, the remaining number of adaptive iteration is also passed to `get_proposers` so that new proposers can be initialized appropriately.
    
    Not sure if pre-generate a list of proposers and execute each of them in random permutation order is the right way to go, but at least this is consistent with our [single site implementation](https://www.internalfb.com/code/fbsource/[7e3f468344bf4e456869f656e80342747d6bb740]/fbcode/beanmachine/beanmachine/ppl/inference/abstract_mh_infer.py?lines=332-334) :). I feel like the proposer generation logic is probably going to change quite a few times in the future as we figure out what's the best thing to do.
    
    Reviewed By: jpchen, neerajprad
    
    Differential Revision: D31311147
    
    fbshipit-source-id: 38e0530ca111917d2b877b03c2f03799ce1c8aaa

--- src/beanmachine/ppl/experimental/global_inference/hmc_inference.py
+++ src/beanmachine/ppl/experimental/global_inference/hmc_inference.py
@@ -1,6 +1,9 @@
-import dataclasses
+from typing import List
 
 from beanmachine.ppl.experimental.global_inference.base_inference import BaseInference
+from beanmachine.ppl.experimental.global_inference.proposer.base_proposer import (
+    BaseProposer,
+)
 from beanmachine.ppl.experimental.global_inference.proposer.hmc_proposer import (
     HMCProposer,
 )
@@ -10,27 +13,71 @@ from beanmachine.ppl.experimental.global_inference.proposer.nuts_proposer import
 from beanmachine.ppl.experimental.global_inference.simple_world import SimpleWorld
 
 
-@dataclasses.dataclass
 class GlobalHamiltonianMonteCarlo(BaseInference):
-    trajectory_length: float
-    initial_step_size: float = 1.0
-    adapt_step_size: bool = True
-    adapt_mass_matrix: bool = True
-    target_accept_prob: float = 0.8
+    def __init__(
+        self,
+        trajectory_length: float,
+        initial_step_size: float = 1.0,
+        adapt_step_size: bool = True,
+        adapt_mass_matrix: bool = True,
+        target_accept_prob: float = 0.8,
+    ):
+        self.trajectory_length = trajectory_length
+        self.initial_step_size = initial_step_size
+        self.adapt_step_size = adapt_step_size
+        self.adapt_mass_matrix = adapt_mass_matrix
+        self.target_accept_prob = target_accept_prob
+        self._proposer = None
 
-    def get_proposer(self, world: SimpleWorld) -> HMCProposer:
-        return HMCProposer(world, **dataclasses.asdict(self))
+    def get_proposers(
+        self, world: SimpleWorld, num_adaptive_sample: int
+    ) -> List[BaseProposer]:
+        if self._proposer is None:
+            self._proposer = HMCProposer(
+                world,
+                num_adaptive_sample,
+                self.trajectory_length,
+                self.initial_step_size,
+                self.adapt_step_size,
+                self.adapt_mass_matrix,
+                self.target_accept_prob,
+            )
+        return [self._proposer]
 
 
-@dataclasses.dataclass
 class GlobalNoUTurnSampler(BaseInference):
-    max_tree_depth: int = 10
-    max_delta_energy: float = 1000.0
-    initial_step_size: float = 1.0
-    adapt_step_size: bool = True
-    adapt_mass_matrix: bool = True
-    multinomial_sampling: bool = True
-    target_accept_prob: float = 0.8
+    def __init__(
+        self,
+        max_tree_depth: int = 10,
+        max_delta_energy: float = 1000.0,
+        initial_step_size: float = 1.0,
+        adapt_step_size: bool = True,
+        adapt_mass_matrix: bool = True,
+        multinomial_sampling: bool = True,
+        target_accept_prob: float = 0.8,
+    ):
+        self.max_tree_depth = max_tree_depth
+        self.max_delta_energy = max_delta_energy
+        self.initial_step_size = initial_step_size
+        self.adapt_step_size = adapt_step_size
+        self.adapt_mass_matrix = adapt_mass_matrix
+        self.multinomial_sampling = multinomial_sampling
+        self.target_accept_prob = target_accept_prob
+        self._proposer = None
 
-    def get_proposer(self, world: SimpleWorld) -> NUTSProposer:
-        return NUTSProposer(world, **dataclasses.asdict(self))
+    def get_proposers(
+        self, world: SimpleWorld, num_adaptive_sample: int
+    ) -> List[BaseProposer]:
+        if self._proposer is None:
+            self._proposer = NUTSProposer(
+                world,
+                num_adaptive_sample,
+                self.max_tree_depth,
+                self.max_delta_energy,
+                self.initial_step_size,
+                self.adapt_step_size,
+                self.adapt_mass_matrix,
+                self.multinomial_sampling,
+                self.target_accept_prob,
+            )
+        return [self._proposer]
