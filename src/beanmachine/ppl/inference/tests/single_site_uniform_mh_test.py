09675feec2785d479c1b76157a55fd6ac7a056d9
JP Chen <jpchen@fb.com>
2021-10-29 11:45:36 -0700

    Migrate single site uniform to new base_inference (#1096)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/beanmachine/pull/1096
    
    Migrate uniform proposer as it is currently implemented in BM. For continuous variables, it defaults to ancestral.
    
    Reviewed By: neerajprad, horizon-blue
    
    Differential Revision: D31991028
    
    fbshipit-source-id: c71df9bceb727273178eb434e1292165847d0c65

--- src/beanmachine/ppl/inference/tests/single_site_uniform_mh_test.py
+++ src/beanmachine/ppl/inference/tests/single_site_uniform_mh_test.py
@@ -4,8 +4,8 @@ import unittest
 import beanmachine.ppl as bm
 import torch
 import torch.distributions as dist
-from beanmachine.ppl.inference.proposer.single_site_uniform_proposer import (
-    SingleSiteUniformProposer,
+from beanmachine.ppl.experimental.global_inference.single_site_uniform_mh import (
+    SingleSiteUniformMetropolisHastings,
 )
 
 
@@ -30,35 +30,24 @@ class SingleSiteUniformMetropolisHastingsTest(unittest.TestCase):
 
     def test_single_site_uniform_mh_with_bernoulli(self):
         model = self.SampleBernoulliModel()
-        mh = bm.SingleSiteUniformMetropolisHastings()
+        mh = SingleSiteUniformMetropolisHastings()
         foo_key = model.foo()
         bar_key = model.bar()
-        mh.queries_ = [model.foo()]
-        mh.observations_ = {model.bar(): torch.tensor(0.0)}
-        mh._infer(10)
-        self.assertEqual(isinstance(mh.proposer_, SingleSiteUniformProposer), True)
-        # using _infer instead of infer, as world_ would be reset at the end
-        # infer
-        world_vars = mh.world_.variables_.vars()
-        self.assertEqual(foo_key in world_vars, True)
-        self.assertEqual(bar_key in world_vars, True)
-        self.assertEqual(foo_key in world_vars[bar_key].parent, True)
-        self.assertEqual(bar_key in world_vars[foo_key].children, True)
+        sampler = mh.sampler([foo_key], {bar_key: torch.tensor(0.0)}, num_samples=5)
+        for world in sampler:
+            self.assertTrue(foo_key in world)
+            self.assertTrue(bar_key in world)
+            self.assertTrue(foo_key in world.get_variable(bar_key).parents)
+            self.assertTrue(bar_key in world.get_variable(foo_key).children)
 
     def test_single_site_uniform_mh_with_categorical(self):
         model = self.SampleCategoricalModel()
-        mh = bm.SingleSiteUniformMetropolisHastings()
+        mh = SingleSiteUniformMetropolisHastings()
         foo_key = model.foo()
         bar_key = model.bar()
-        mh.queries_ = [model.foo()]
-        mh.observations_ = {model.bar(): torch.tensor(1.0)}
-        mh._infer(10)
-
-        self.assertEqual(isinstance(mh.proposer_, SingleSiteUniformProposer), True)
-        # using _infer instead of infer, as world_ would be reset at the end
-        # infer
-        world_vars = mh.world_.variables_.vars()
-        self.assertEqual(foo_key in world_vars, True)
-        self.assertEqual(bar_key in world_vars, True)
-        self.assertEqual(foo_key in world_vars[bar_key].parent, True)
-        self.assertEqual(bar_key in world_vars[foo_key].children, True)
+        sampler = mh.sampler([foo_key], {bar_key: torch.tensor(0.0)}, num_samples=5)
+        for world in sampler:
+            self.assertTrue(foo_key in world)
+            self.assertTrue(bar_key in world)
+            self.assertTrue(foo_key in world.get_variable(bar_key).parents)
+            self.assertTrue(bar_key in world.get_variable(foo_key).children)
