064c586ac80931ce87eab6618331c837e5f082fb
Feynman Tsing-Yang Liang <feynmanl@fb.com>
2021-11-08 17:13:29 -0800

    Removes stale docstrings from BetaDimensionTransform
    Summary: Removes stale docstrings as noted by jpchen in D32227430
    
    Reviewed By: horizon-blue
    
    Differential Revision: D32260801
    
    fbshipit-source-id: 0dd2efeaa4c0c927093bd2cd13ceed7226d8e964

--- src/beanmachine/ppl/world/utils.py
+++ src/beanmachine/ppl/world/utils.py
@@ -21,15 +21,9 @@ class BetaDimensionTransform(Transform):
         return isinstance(other, BetaDimensionTransform)
 
     def _call(self, x):
-        """
-        Abstract method to compute forward transformation.
-        """
         return torch.cat((x.unsqueeze(-1), (1 - x).unsqueeze(-1)), -1)
 
     def _inverse(self, y):
-        """
-        Abstract method to compute inverse transformation.
-        """
         return y[..., 0] / y.sum(dim=-1)
 
     def forward_shape(self, shape):
@@ -39,9 +33,6 @@ class BetaDimensionTransform(Transform):
         return shape[:-1]
 
     def log_abs_det_jacobian(self, x, y):
-        """
-        Computes the log det jacobian `log |dy/dx|` given input and output.
-        """
         return torch.zeros_like(x)
 
 
