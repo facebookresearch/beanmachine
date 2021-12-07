1d9f250a80609f0f269ab39df860618cab78e4fe
JP Chen <jpchen@fb.com>
2021-12-07 03:09:31 -0800

    world docstrings (#1156)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/beanmachine/pull/1156
    
    World docstrings
    
    Reviewed By: horizon-blue
    
    Differential Revision: D32791120
    
    fbshipit-source-id: cafe763422a88f3e8f2a5552ecde00a28a572f4a

--- src/beanmachine/ppl/diagnostics/__init__.py
+++ src/beanmachine/ppl/diagnostics/__init__.py
@@ -1,4 +1,9 @@
 # Copyright (c) Facebook, Inc. and its affiliates.
+"""
+Tools for supporting MCMC qiagnostics. Will be deprecated in a future release in favor of arviz integration.
+"""
+
+
 from beanmachine.ppl.diagnostics.diagnostics import Diagnostics
 
 
