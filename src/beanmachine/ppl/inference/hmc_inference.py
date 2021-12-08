1ca6a857b66cc186c04ca9b42b4b077bbb7418a0
JP Chen <jpchen@fb.com>
2021-12-07 18:47:44 -0800

    hmc nuts docs
    Summary: Add references and docs for hmc and nuts inference. I didn't document get_proposers since I specify at the class level it returns a single block proposer.
    
    Reviewed By: horizon-blue
    
    Differential Revision: D32890217
    
    fbshipit-source-id: e3619c1799eb65ad5984517f49775f6c515ed6ac

--- src/beanmachine/ppl/inference/hmc_inference.py
+++ src/beanmachine/ppl/inference/hmc_inference.py
@@ -15,6 +15,19 @@ from beanmachine.ppl.world import World
 
 
 class GlobalHamiltonianMonteCarlo(BaseInference):
+    """
+    Global (multi-site) Hamiltonian Monte Carlo [1] sampler. This global sampler blocks all of the
+    target random_variables in the World together and proposes them jointly.
+
+    [1] Neal, Radford. `MCMC Using Hamiltonian Dynamics`.
+
+    Args:
+        trajectory_length (float): Length of single trajectory.
+        initial_step_size (float): Defaults to 1.0.
+        adapt_step_size (bool): Whether to adapt the step size, Defaults to True,
+        adapt_mass_matrix (bool): Whether to adapt the mass matrix. Defaults to True,
+        target_accept_prob (float): Target accept prob. Increasing this value would lead to smaller step size. Defaults to 0.8.
+    """
     def __init__(
         self,
         trajectory_length: float,
@@ -51,6 +64,22 @@ class GlobalHamiltonianMonteCarlo(BaseInference):
 
 
 class GlobalNoUTurnSampler(BaseInference):
+    """
+    Global No U-turn sampler [1]. This sampler blocks multiple variables together in the World
+    and samples them jointly. This sampler adaptively sets the hyperparameters of the HMC kernel.
+
+    [1] Hoffman and Gelman. `The No-U-turn sampler: adaptively setting path lengths in Hamiltonian Monte Carlo`.
+    [2] Betancourt, Michael. `A Conceptual Introduction to Hamiltonian Monte Carlo`.
+
+    Args:
+        max_tree_depth (int): Maximum tree depth, defaults to 10.
+        max_delta_energy (float): Maximum delta energy (for numerical stability), defaults to 1000.
+        initial_step_size (float): Defaults to 1.0.
+        adapt_step_size (bool): Whether to adapt step size with Dual averaging as suggested in [1], defaults to True.
+        adapt_mass_matrix (bool) Whether to adapt mass matrix using Welford Scheme, defaults to True.
+        multinomial_sampling (bool): Whether to use multinomial sampling as in [2], defaults to True.
+        target_accept_prob (float): Target accept probability. Increasing this would lead to smaller step size. Defaults to 0.8.
+    """
     def __init__(
         self,
         max_tree_depth: int = 10,
