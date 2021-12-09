f47bcca4f7108b3b8919411550826a138002c203
Xiaoyan Wang <xiaoyan0@fb.com>
2021-12-08 22:55:11 -0800

    Update copyright headers for `beanmachine.graph`
    Differential Revision: D32978606
    
    fbshipit-source-id: fa94ff7ebb89c3809803d3b8a78f856dc686e6c7

--- src/beanmachine/graph/global/global_mh.h
+++ src/beanmachine/graph/global/global_mh.h
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 #pragma once
 #include "beanmachine/graph/global/proposer/global_proposer.h"
 #include "beanmachine/graph/graph.h"
--- src/beanmachine/graph/global/tests/conjugate_util_test.cpp
+++ src/beanmachine/graph/global/tests/conjugate_util_test.cpp
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 #include "beanmachine/graph/global/tests/conjugate_util_test.h"
 #include <gtest/gtest.h>
 #include "beanmachine/graph/graph.h"
