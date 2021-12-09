84c0193040b37ea7c2e5c1b5a9d8df64d8785a79
Xiaoyan Wang <xiaoyan0@fb.com>
2021-12-08 22:09:30 -0800

    Update copyright header for `ppl.inference`, `ppl.world`, and `ppl.model`
    Differential Revision: D32978224
    
    fbshipit-source-id: 8fc6fd93e0c19d5a0fe3211dd58dce9dac80a09c

--- src/beanmachine/ppl/inference/single_site_ancestral_mh.py
+++ src/beanmachine/ppl/inference/single_site_ancestral_mh.py
@@ -1,3 +1,8 @@
+# Copyright (c) Meta Platforms, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
 from beanmachine.ppl.inference.proposer.single_site_ancestral_proposer import (
     SingleSiteAncestralProposer,
 )
