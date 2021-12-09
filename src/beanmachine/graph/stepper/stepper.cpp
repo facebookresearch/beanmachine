b2a1a746f7c2c75c026973bbe89ba905ed9161f0
Xiaoyan Wang <xiaoyan0@fb.com>
2021-12-09 00:36:49 -0800

    Update copyright headers for `beanmachine.graph` (#1207)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/beanmachine/pull/1207
    
    See D32977813 for summary :)
    
    Reviewed By: jpchen
    
    Differential Revision: D32978606
    
    fbshipit-source-id: 5fa542a65be2503589f5206fdaa9071f30a12a6e

--- src/beanmachine/graph/stepper/stepper.cpp
+++ src/beanmachine/graph/stepper/stepper.cpp
@@ -1,3 +1,8 @@
-// (c) Facebook, Inc. and its affiliates. Confidential and proprietary.
+/*
+ * Copyright (c) Meta Platforms, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 
 #include "beanmachine/graph/stepper/stepper.h"
