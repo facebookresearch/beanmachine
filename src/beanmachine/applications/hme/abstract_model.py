097e6d1dc4fdc5627f36bf85bd7188c1f2b0fc82
Xiaoyan Wang <xiaoyan0@fb.com>
2021-12-08 22:12:41 -0800

    Update copyright headers for `beanmachine.applications`
    Summary: See D32977813 for summary :)
    
    Differential Revision: D32978505
    
    fbshipit-source-id: e57f1ea2a187c9751a0babdd017b781496ba8cdc

--- src/beanmachine/applications/hme/abstract_model.py
+++ src/beanmachine/applications/hme/abstract_model.py
@@ -1,4 +1,8 @@
-# Copyright(C) Facebook, Inc. and its affiliates. All Rights Reserved.
+# Copyright (c) Meta Platforms, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
 import logging
 import re as regx
 import time
