18ce1c598a5dca7a2d98ac60c8ba7ea87e95b692
Xiaoyan Wang <xiaoyan0@fb.com>
2021-12-08 22:17:09 -0800

    Update copyright headers for `beanmachine.ppl`
    Summary:
    See D32977813 for summary :)
    
    This diff finish updating the copyright headers for the rest of `beanmachine.ppl` module.
    
    Differential Revision: D32978318
    
    fbshipit-source-id: 6b03fc61bff92dfb5014bfa0c641e4bc5e38bd5d

--- src/beanmachine/ppl/utils/tests/item_counter_test.py
+++ src/beanmachine/ppl/utils/tests/item_counter_test.py
@@ -1,4 +1,8 @@
-# Copyright (c) Facebook, Inc. and its affiliates.
+# Copyright (c) Meta Platforms, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
 """Tests for item_counter.py"""
 import unittest
 
