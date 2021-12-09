28bf0d626c8b29b0e761b783c1cadb9b853eacdb
Xiaoyan Wang <xiaoyan0@fb.com>
2021-12-09 00:36:49 -0800

    Update copyright headers for rest of beanmachine (#1208)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/beanmachine/pull/1208
    
    See D32977813 for summary :)
    
    Reviewed By: jpchen
    
    Differential Revision: D32979001
    
    fbshipit-source-id: c764e9c3fd0f6cdb81c29dde8a670c2a36a83518

--- tutorials/utils/hearts.py
+++ tutorials/utils/hearts.py
@@ -1,3 +1,8 @@
+# Copyright (c) Meta Platforms, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
 """Module for the ``Mixture model using count data`` tutorial."""
 from numbers import Number
 from typing import Any, Dict, List, Union
