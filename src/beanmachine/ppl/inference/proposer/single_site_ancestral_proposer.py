dcc1634b8aa6b5505d00afd7472da25724254be3
Xiaoyan Wang <xiaoyan0@fb.com>
2021-12-08 21:51:41 -0800

    Update copyright header for `ppl.inference`, `ppl.world`, and `ppl.model`
    Summary: See D32977813 for summary :)
    
    Differential Revision: D32978224
    
    fbshipit-source-id: d47873d29fc25dbe7645354bd611c0ecbe5a9d28

--- src/beanmachine/ppl/inference/proposer/single_site_ancestral_proposer.py
+++ src/beanmachine/ppl/inference/proposer/single_site_ancestral_proposer.py
@@ -1,3 +1,8 @@
+# Copyright (c) Meta Platforms, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
 import torch.distributions as dist
 from beanmachine.ppl.inference.proposer.base_single_site_proposer import (
     BaseSingleSiteMHProposer,
--- src/beanmachine/ppl/model/__init__.py
+++ src/beanmachine/ppl/model/__init__.py
@@ -1,4 +1,8 @@
-# Copyright (c) Facebook, Inc. and its affiliates.
+# Copyright (c) Meta Platforms, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
 from beanmachine.ppl.model.rv_identifier import RVIdentifier
 from beanmachine.ppl.model.statistical_model import (
     StatisticalModel,
