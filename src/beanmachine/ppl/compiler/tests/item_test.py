713fcc1624f4032b5b6486998f5c48b9f4ad9067
Xiaoyan Wang <xiaoyan0@fb.com>
2021-12-08 22:55:11 -0800

    Update copyright headers for `beanmachine.ppl.compiler`
    Differential Revision: D32977813
    
    fbshipit-source-id: d35a3d94bca45f4139ccf32d0475776bdf80c958

--- src/beanmachine/ppl/compiler/tests/item_test.py
+++ src/beanmachine/ppl/compiler/tests/item_test.py
@@ -1,4 +1,8 @@
-# Copyright (c) Facebook, Inc. and its affiliates.
+# Copyright (c) Meta Platforms, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
 import unittest
 
 # The item() member function should be treated as an identity by the compiler
--- src/beanmachine/ppl/compiler/tests/tutorial_GMM_with_1_dimensions_and_4_components_test.py
+++ src/beanmachine/ppl/compiler/tests/tutorial_GMM_with_1_dimensions_and_4_components_test.py
@@ -1,4 +1,8 @@
-# Copyright (c) Facebook, Inc. and its affiliates.
+# Copyright (c) Meta Platforms, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
 """End-to-end test for 1D GMM with K > 2 number of components"""
 
 import logging
