a38fd2455c1944820179d918789c4768f8e6894e
Rodrigo de Salvo Braz <rodrigobraz@fb.com>
2021-12-26 17:20:29 -0800

    Replace DoubleMatrix multiplication by Eigen matrix by overloaded method.
    Summary:
    We still needed to use `DoubleMatrix._matrix` to multiply a `DoubleMatrix` containing a matrix by a matrix (usages of `_double` were already eliminated). Now we have `DoubleMatrix::times(DoubleMatrix, Matrix)` and `DoubleMatrix::times(Matrix,DoubleMatrix)` that take care of product by matrices in either case. This allowed the simplification of code that duplicate that for double and matrices cases.
    
    Note that it would have been better to have the method be `operator*` but that conflicts with Eigen's `operator*(double, Matrix)` and `operator*(Matrix, double)` because `DoubleMatrix` provides *implicit* conversion to double. I deemed the implicit conversion to be more useful than the occasional multiplication by a matrix so I kept it. See documentation of `DoubleMatrix::times` for more details.
    
    Differential Revision: D32905818
    
    fbshipit-source-id: 99c486bc3bcb38a1631b8df34ef5c8f73fcff6dd

--- src/beanmachine/graph/double_matrix.h
+++ src/beanmachine/graph/double_matrix.h
@@ -88,7 +88,23 @@ struct DoubleMatrix : public std::variant<double, MatrixProperty::Matrix> {
   explicit DoubleMatrix(Eigen::MatrixXd matrix)
       : VariantBaseClass(matrix), _matrix(*this) {}
 
-  operator double() const;
+  /* implicit */ operator double() const;
+
+  double& as_double() {
+    return std::get<double>(*this);
+  }
+
+  double as_double() const {
+    return std::get<double>(*this);
+  }
+
+  Matrix& as_matrix() {
+    return std::get<Matrix>(*this);
+  }
+
+  const Matrix& as_matrix() const {
+    return std::get<Matrix>(*this);
+  }
 
   DoubleMatrix& operator=(double d);
   DoubleMatrix& operator=(const Matrix& d);
@@ -98,6 +114,63 @@ struct DoubleMatrix : public std::variant<double, MatrixProperty::Matrix> {
   DoubleMatrix& operator+=(const Matrix& matrix);
   DoubleMatrix& operator+=(const DoubleMatrix& another);
 
+  // A substitute for operator*(DoubleMatrix, Matrix).
+  //
+  // One might ask why we need these method instead of operator*(DoubleMatrix,
+  // Matrix).
+  // The short story: This is needed because we provide implicit
+  // DoubleMatrix -> double conversion, which makes operator*(DoubleMatrix,
+  // Matrix) ambiguous with operator*(double, Matrix). While this could be
+  // avoided by disabling implicit double conversion, that would make it not
+  // possible to write things like:
+  // double x = double_matrix;
+  // equals(double_matrix, 0.9, 0.0001);
+  // which are much more common that multiplication by a matrix.
+  //
+  // The longer story:
+  // In fact, if we define operator*(DoubleMatrix, Matrix) directly,
+  // it does take precedence over implicit double conversion and there is no
+  // problem. However that hurts performance because it forces premature
+  // evaluation of matrix expression objects in Eigen to Matrix,
+  // as explained in
+  // https://eigen.tuxfamily.org/dox/TopicFunctionTakingEigenTypes.html. To
+  // avoid this performance hit we need to define template <typename Derived>
+  // operator*(DoubleMatrix, Eigen::MatrixBase<Derived>) but that does *not*
+  // take precedence over operator*(double, Matrix).
+  //
+  // Another possibility is to use a static_cast before multiplying,
+  // but this would prevent generic code that works for both doubles and
+  // matrices.
+  template <typename Derived>
+  static DoubleMatrix times(
+      const DoubleMatrix& dm,
+      const Eigen::MatrixBase<Derived>& matrix) {
+    switch (dm.index()) {
+      case 0:
+        return DoubleMatrix{std::get<double>(dm) * matrix};
+      case 1:
+        return DoubleMatrix{std::get<Matrix>(dm) * matrix};
+      default:
+        throw std::runtime_error(
+            "Multiplying DoubleMatrix that does not hold a value.");
+    }
+  }
+
+  // A substitute for operator*(Matrix, DoubleMatrix).
+  template <typename Derived>
+  static DoubleMatrix times(
+      const Eigen::MatrixBase<Derived>& matrix,
+      const DoubleMatrix& dm) {
+    switch (dm.index()) {
+      case 0:
+        return DoubleMatrix{matrix * std::get<double>(dm)};
+      case 1:
+        return DoubleMatrix{matrix * std::get<Matrix>(dm)};
+      default:
+        throw std::runtime_error(
+            "Multiplying DoubleMatrix that does not hold a value.");
+    }
+  }
   Array array();
   const ArrayOfConst array() const;
 };
@@ -108,13 +181,37 @@ DoubleMatrix operator*(const DoubleMatrix& double_matrix, double d);
 
 DoubleMatrix operator*(double d, const DoubleMatrix& double_matrix);
 
-DoubleMatrix::Matrix operator*(
+template <typename Derived>
+DoubleMatrix operator*(
     const DoubleMatrix& double_matrix,
-    const DoubleMatrix::Matrix& matrix);
+    const Eigen::MatrixBase<Derived>& matrix) {
+  switch (double_matrix.index()) {
+    case 0:
+      return DoubleMatrix{std::get<double>(double_matrix) * matrix};
+    case 1:
+      return DoubleMatrix{
+          std::get<DoubleMatrix::Matrix>(double_matrix) * matrix};
+    default:
+      throw std::runtime_error(
+          "Multiplying DoubleMatrix that does not hold a value.");
+  }
+}
 
-DoubleMatrix::Matrix operator*(
-    const DoubleMatrix::Matrix& matrix,
-    const DoubleMatrix& double_matrix);
+template <typename Derived>
+DoubleMatrix operator*(
+    const Eigen::MatrixBase<Derived>& matrix,
+    const DoubleMatrix& double_matrix) {
+  switch (double_matrix.index()) {
+    case 0:
+      return DoubleMatrix{matrix * std::get<double>(double_matrix)};
+    case 1:
+      return DoubleMatrix{
+          matrix * std::get<DoubleMatrix::Matrix>(double_matrix)};
+    default:
+      throw std::runtime_error(
+          "Multiplying DoubleMatrix that does not hold a value.");
+  }
+}
 
 DoubleMatrix operator*(const DoubleMatrix& dm1, const DoubleMatrix& dm2);
 
