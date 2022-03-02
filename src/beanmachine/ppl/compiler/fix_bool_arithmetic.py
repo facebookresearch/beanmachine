8fb72badb5e5a1ef97f2aad4c9d0725b608ee7c9
Eric Lippert <ericlippert@fb.com>
2022-03-01 17:37:22 -0800

    Continue refactoring graph fixing passes to use combinators
    Summary:
    I'm continuing the work I began in the previous diff. We have numerous difficulties in the "problem fixing" phase where the graph is gradually rewritten from its accumulated form into a valid BMG graph; these difficulties are the result of an inflexible approach to coordinating mutations. We can be both more flexible and more efficient by using a combinator-based approach.
    
    In the previous diff I started by refactoring the unsupported node fixer and unsupported node error reporter. In this diff:
    
    * continue refactoring the unsupported node fixer
    * the implementation of matrix scale fixing can now go back to its own module
    * refactor the Boolean comparison, add, multiply and power rewriters to use combinators
    * refactor the `(1+(-p)) --> complement(p)` rewriter
    
    These refactorings not only get us farther along the road of the ultimate goal to use a combinator-based approach, they fix a known issue; due to the way we chose to order mutations we were not correctly rewriting a complement if one of its operands was rewritten by the unsupported node rewriter. With the combinator-based approach of combining all the node rewriters together into one graph fixer, this issue goes away.  (However, issues involving failure to reach a fixpoint by repeated applications of fixers still exist; I'll address those in a later diff.)
    
    Reviewed By: feynmanliang
    
    Differential Revision: D34400702
    
    fbshipit-source-id: 94bc4e8ae1ec47a17fef110e7d6aab8086912261

--- src/beanmachine/ppl/compiler/fix_bool_arithmetic.py
+++ src/beanmachine/ppl/compiler/fix_bool_arithmetic.py
@@ -3,57 +3,29 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-from typing import Optional
-
 import beanmachine.ppl.compiler.bmg_nodes as bn
 from beanmachine.ppl.compiler.bm_graph_builder import BMGraphBuilder
-from beanmachine.ppl.compiler.fix_problem import ProblemFixerBase
+from beanmachine.ppl.compiler.fix_problem import (
+    node_fixer_first_match,
+    NodeFixer,
+    Inapplicable,
+    NodeFixerResult,
+    type_guard,
+)
 from beanmachine.ppl.compiler.lattice_typer import LatticeTyper
 
 
-class BoolArithmeticFixer(ProblemFixerBase):
-    def __init__(self, bmg: BMGraphBuilder, typer: LatticeTyper) -> None:
-        ProblemFixerBase.__init__(self, bmg, typer)
+class BoolArithmeticFixer:
+    _bmg: BMGraphBuilder
+    _typer: LatticeTyper
 
-    def _needs_fixing(self, n: bn.BMGNode) -> bool:
-        assert isinstance(self._typer, LatticeTyper)
+    def __init__(self, bmg: BMGraphBuilder, typer: LatticeTyper) -> None:
+        self._bmg = bmg
+        self._typer = typer
+
+    def _fix_multiplication(self, n: bn.MultiplicationNode) -> NodeFixerResult:
         # We can simplify 1*anything, 0*anything or bool*anything
         # to anything, 0, or an if-then-else respectively.
-        if isinstance(n, bn.MultiplicationNode):
-            assert len(n.inputs) == 2
-            return self._typer.is_bool(  # pyre-ignore
-                n.inputs[0]
-            ) or self._typer.is_bool(n.inputs[1])
-        # We can simplify 0+anything.
-        if isinstance(n, bn.AdditionNode):
-            assert len(n.inputs) == 2
-            return bn.is_zero(n.inputs[0]) or bn.is_zero(n.inputs[1])
-
-        # We can simplify anything**bool.
-        if isinstance(n, bn.PowerNode):
-            return self._typer.is_bool(n.right)
-
-        # TODO: We could support b ** n where b is bool and n is a natural
-        # constant. If n is the constant zero then the result is just
-        # the Boolean constant true; if n is a non-zero constant then
-        # b ** n is simply b.
-        #
-        # NOTE: We CANNOT support b ** n where b is bool and n is a
-        # non-constant natural and the result is bool. That would
-        # have the semantics of "if b then true else n == 0" but we do
-        # not have an equality operator on naturals in BMG.
-
-        return False
-
-    def _get_replacement(self, n: bn.BMGNode) -> Optional[bn.BMGNode]:
-        if isinstance(n, bn.MultiplicationNode):
-            return self._fix_multiplication(n)
-        if isinstance(n, bn.AdditionNode):
-            return self._fix_addition(n)
-        assert isinstance(n, bn.PowerNode)
-        return self._fix_power(n)
-
-    def _fix_multiplication(self, n: bn.MultiplicationNode) -> bn.BMGNode:
         assert len(n.inputs) == 2
         if bn.is_zero(n.inputs[0]):
             return n.inputs[0]
@@ -63,21 +35,37 @@ class BoolArithmeticFixer(ProblemFixerBase):
             return n.inputs[1]
         if bn.is_one(n.inputs[1]):
             return n.inputs[0]
-        zero = self._bmg.add_constant(0.0)
-        if self._typer.is_bool(n.inputs[0]):  # pyre-ignore
+        if self._typer.is_bool(n.inputs[0]):
+            zero = self._bmg.add_constant(0.0)
             return self._bmg.add_if_then_else(n.inputs[0], n.inputs[1], zero)
-        assert self._typer.is_bool(n.inputs[1])
-        return self._bmg.add_if_then_else(n.inputs[1], n.inputs[0], zero)
+        if self._typer.is_bool(n.inputs[1]):
+            zero = self._bmg.add_constant(0.0)
+            return self._bmg.add_if_then_else(n.inputs[1], n.inputs[0], zero)
+        return Inapplicable
 
-    def _fix_addition(self, n: bn.AdditionNode) -> bn.BMGNode:
+    def _fix_addition(self, n: bn.AdditionNode) -> NodeFixerResult:
+        # We can simplify 0+anything.
         assert len(n.inputs) == 2
         if bn.is_zero(n.inputs[0]):
             return n.inputs[1]
-        assert bn.is_zero(n.inputs[1])
-        return n.inputs[0]
+        if bn.is_zero(n.inputs[1]):
+            return n.inputs[0]
+        return Inapplicable
 
-    def _fix_power(self, n: bn.PowerNode) -> bn.BMGNode:
+    def _fix_power(self, n: bn.PowerNode) -> NodeFixerResult:
         # x ** b  -->   if b then x else 1
-        assert self._typer.is_bool(n.right)  # pyre-ignore
-        one = self._bmg.add_constant(1.0)
-        return self._bmg.add_if_then_else(n.right, n.left, one)
+        if self._typer.is_bool(n.right):
+            one = self._bmg.add_constant(1.0)
+            return self._bmg.add_if_then_else(n.right, n.left, one)
+        return Inapplicable
+
+
+def bool_arithmetic_fixer(bmg: BMGraphBuilder, typer: LatticeTyper) -> NodeFixer:
+    baf = BoolArithmeticFixer(bmg, typer)
+    return node_fixer_first_match(
+        [
+            type_guard(bn.AdditionNode, baf._fix_addition),
+            type_guard(bn.MultiplicationNode, baf._fix_multiplication),
+            type_guard(bn.PowerNode, baf._fix_power),
+        ]
+    )
