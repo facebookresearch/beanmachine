051fe61aae9f2c5d968b6466543f9d2ba62bb4ce
Eric Lippert <ericlippert@fb.com>
2022-03-01 21:44:25 -0800

    Continue refactoring graph fixing passes to use combinators
    Summary:
    I'm continuing the work I began in the previous diff. We have numerous difficulties in the "problem fixing" phase where the graph is gradually rewritten from its accumulated form into a valid BMG graph; these difficulties are the result of an inflexible approach to coordinating mutations. We can be both more flexible and more efficient by using a combinator-based approach.
    
    In the previous diff I started by refactoring the unsupported node fixer and unsupported node error reporter. In this diff:
    
    * continue refactoring the unsupported node fixer
    * the implementation of matrix scale fixing can now go back to its own module
    * refactor the Boolean comparison, add, multiply and power rewriters to use combinators
    * refactor the `(1+(-p)) --> complement(p)` rewriter
    
    These refactorings not only get us farther along the road of the ultimate goal to use a combinator-based approach, they fix a known issue; due to the way we chose to order mutations we were not correctly rewriting a complement if one of its operands was rewritten by the unsupported node rewriter. With the combinator-based approach of combining all the node rewriters together into one graph fixer, this issue goes away.  (However, issues involving failure to reach a fixpoint by repeated applications of fixers still exist; I'll address those in a later diff.)
    
    Reviewed By: feynmanliang
    
    Differential Revision: D34400702
    
    fbshipit-source-id: 5b0ff70526f7e6dc512147c638f4a11a33ea3629

--- src/beanmachine/ppl/compiler/fix_bool_comparisons.py
+++ src/beanmachine/ppl/compiler/fix_bool_comparisons.py
@@ -3,35 +3,43 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-from typing import Optional
-
 import beanmachine.ppl.compiler.bmg_nodes as bn
 from beanmachine.ppl.compiler.bm_graph_builder import BMGraphBuilder
 from beanmachine.ppl.compiler.bmg_types import Boolean
-from beanmachine.ppl.compiler.fix_problem import ProblemFixerBase
+from beanmachine.ppl.compiler.fix_problem import (
+    Inapplicable,
+    NodeFixer,
+    NodeFixerResult,
+    node_fixer_first_match,
+    type_guard,
+)
 from beanmachine.ppl.compiler.lattice_typer import LatticeTyper
 
 
-class BoolComparisonFixer(ProblemFixerBase):
+class BoolComparisonFixer:
     """This class takes a Bean Machine Graph builder and replaces all comparison
     operators whose operands are bool with semantically equivalent IF nodes."""
 
-    def __init__(self, bmg: BMGraphBuilder, typer: LatticeTyper) -> None:
-        ProblemFixerBase.__init__(self, bmg, typer)
+    _bmg: BMGraphBuilder
+    _typer: LatticeTyper
 
-    def _needs_fixing(self, n: bn.BMGNode) -> bool:
-        return (
-            isinstance(n, bn.ComparisonNode)
-            and self._typer.is_bool(n.left)  # pyre-ignore
-            and self._typer.is_bool(n.right)
+    def __init__(self, bmg: BMGraphBuilder, typer: LatticeTyper) -> None:
+        self._bmg = bmg
+        self._typer = typer
+
+    def _both_bool(self, n: bn.BMGNode) -> bool:
+        return self._typer.is_bool(n.left) and self._typer.is_bool(  # pyre-ignore
+            n.right  # pyre-ignore
         )
 
-    def _replace_bool_equals(self, node: bn.EqualNode) -> bn.BMGNode:
+    def _replace_bool_equals(self, node: bn.EqualNode) -> NodeFixerResult:
         # 1 == y        -->  y
         # x == 1        -->  x
         # 0 == y        -->  not y
         # x == 0        -->  not x
         # x == y        -->  if x then y else not y
+        if not self._both_bool(node):
+            return Inapplicable
         if bn.is_one(node.left):
             return node.right
         if bn.is_one(node.right):
@@ -43,12 +51,14 @@ class BoolComparisonFixer(ProblemFixerBase):
         alt = self._bmg.add_complement(node.right)
         return self._bmg.add_if_then_else(node.left, node.right, alt)
 
-    def _replace_bool_not_equals(self, node: bn.NotEqualNode) -> bn.BMGNode:
+    def _replace_bool_not_equals(self, node: bn.NotEqualNode) -> NodeFixerResult:
         # 1 != y        -->  not y
         # x != 1        -->  not x
         # 0 != y        -->  y
         # x != 0        -->  x
         # x != y        -->  if x then not y else y
+        if not self._both_bool(node):
+            return Inapplicable
         if bn.is_one(node.left):
             return self._bmg.add_complement(node.right)
         if bn.is_one(node.right):
@@ -60,12 +70,14 @@ class BoolComparisonFixer(ProblemFixerBase):
         cons = self._bmg.add_complement(node.right)
         return self._bmg.add_if_then_else(node.left, cons, node.right)
 
-    def _replace_bool_gte(self, node: bn.GreaterThanEqualNode) -> bn.BMGNode:
+    def _replace_bool_gte(self, node: bn.GreaterThanEqualNode) -> NodeFixerResult:
         # 1 >= y        -->  true
         # x >= 1        -->  x
         # 0 >= y        -->  not y
         # x >= 0        -->  true
         # x >= y        -->  if x then true else not y
+        if not self._both_bool(node):
+            return Inapplicable
         if bn.is_one(node.left):
             return self._bmg.add_constant_of_type(True, Boolean)
         if bn.is_one(node.right):
@@ -78,12 +90,14 @@ class BoolComparisonFixer(ProblemFixerBase):
         alt = self._bmg.add_complement(node.right)
         return self._bmg.add_if_then_else(node.left, cons, alt)
 
-    def _replace_bool_gt(self, node: bn.GreaterThanNode) -> bn.BMGNode:
+    def _replace_bool_gt(self, node: bn.GreaterThanNode) -> NodeFixerResult:
         # 1 > y        -->  not y
         # x > 1        -->  false
         # 0 > y        -->  false
         # x > 0        -->  x
         # x > y        -->  if x then not y else false
+        if not self._both_bool(node):
+            return Inapplicable
         if bn.is_one(node.left):
             return self._bmg.add_complement(node.right)
         if bn.is_one(node.right):
@@ -96,12 +110,14 @@ class BoolComparisonFixer(ProblemFixerBase):
         alt = self._bmg.add_constant_of_type(False, Boolean)
         return self._bmg.add_if_then_else(node.left, cons, alt)
 
-    def _replace_bool_lte(self, node: bn.LessThanEqualNode) -> bn.BMGNode:
+    def _replace_bool_lte(self, node: bn.LessThanEqualNode) -> NodeFixerResult:
         # 1 <= y        -->  y
         # x <= 1        -->  true
         # 0 <= y        -->  true
         # x <= 0        -->  not x
         # x <= y        -->  if x then y else true
+        if not self._both_bool(node):
+            return Inapplicable
         if bn.is_one(node.left):
             return node.right
         if bn.is_one(node.right):
@@ -113,12 +129,14 @@ class BoolComparisonFixer(ProblemFixerBase):
         alt = self._bmg.add_constant_of_type(True, Boolean)
         return self._bmg.add_if_then_else(node.left, node.right, alt)
 
-    def _replace_bool_lt(self, node: bn.LessThanNode) -> bn.BMGNode:
+    def _replace_bool_lt(self, node: bn.LessThanNode) -> NodeFixerResult:
         # 1 < y        -->  false
         # x < 1        -->  not x
         # 0 < y        -->  y
         # x < 0        -->  false
         # x < y        -->  if x then false else y
+        if not self._both_bool(node):
+            return Inapplicable
         if bn.is_one(node.left):
             return self._bmg.add_constant_of_type(False, Boolean)
         if bn.is_one(node.right):
@@ -130,20 +148,19 @@ class BoolComparisonFixer(ProblemFixerBase):
         cons = self._bmg.add_constant_of_type(False, Boolean)
         return self._bmg.add_if_then_else(node.left, cons, node.right)
 
-    def _get_replacement(self, n: bn.BMGNode) -> Optional[bn.BMGNode]:
-        # TODO: Should we treat "x is y" the same as "x == y" when they are
-        # bools, or should that be an error?
-        if isinstance(n, bn.EqualNode):
-            return self._replace_bool_equals(n)
-        if isinstance(n, bn.NotEqualNode):
-            return self._replace_bool_not_equals(n)
-        if isinstance(n, bn.GreaterThanEqualNode):
-            return self._replace_bool_gte(n)
-        if isinstance(n, bn.GreaterThanNode):
-            return self._replace_bool_gt(n)
-        if isinstance(n, bn.LessThanEqualNode):
-            return self._replace_bool_lte(n)
-        if isinstance(n, bn.LessThanNode):
-            return self._replace_bool_lt(n)
 
-        return None
+def bool_comparison_fixer(bmg: BMGraphBuilder, typer: LatticeTyper) -> NodeFixer:
+    bcf = BoolComparisonFixer(bmg, typer)
+    # TODO: Should we treat "x is y" the same as "x == y" when they are
+    # bools, or should that be an error?
+
+    return node_fixer_first_match(
+        [
+            type_guard(bn.EqualNode, bcf._replace_bool_equals),
+            type_guard(bn.GreaterThanEqualNode, bcf._replace_bool_gte),
+            type_guard(bn.GreaterThanNode, bcf._replace_bool_gt),
+            type_guard(bn.LessThanEqualNode, bcf._replace_bool_lte),
+            type_guard(bn.LessThanNode, bcf._replace_bool_lt),
+            type_guard(bn.NotEqualNode, bcf._replace_bool_not_equals),
+        ]
+    )
--- src/beanmachine/ppl/compiler/fix_problems.py
+++ src/beanmachine/ppl/compiler/fix_problems.py
@@ -3,20 +3,20 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-from typing import List, Set, Type
+from typing import Callable, List, Set
 
 import beanmachine.ppl.compiler.profiler as prof
 from beanmachine.ppl.compiler.bm_graph_builder import BMGraphBuilder
 from beanmachine.ppl.compiler.error_report import ErrorReport
-from beanmachine.ppl.compiler.fix_additions import AdditionFixer
+from beanmachine.ppl.compiler.fix_additions import addition_fixer
 from beanmachine.ppl.compiler.fix_beta_conjugate_prior import (
     BetaBernoulliConjugateFixer,
     BetaBinomialConjugateFixer,
 )
-from beanmachine.ppl.compiler.fix_bool_arithmetic import BoolArithmeticFixer
-from beanmachine.ppl.compiler.fix_bool_comparisons import BoolComparisonFixer
+from beanmachine.ppl.compiler.fix_bool_arithmetic import bool_arithmetic_fixer
+from beanmachine.ppl.compiler.fix_bool_comparisons import bool_comparison_fixer
 from beanmachine.ppl.compiler.fix_logsumexp import LogSumExpFixer
-from beanmachine.ppl.compiler.fix_matrix_scale import MatrixScaleFixer
+from beanmachine.ppl.compiler.fix_matrix_scale import matrix_scale_fixer
 from beanmachine.ppl.compiler.fix_multiary_ops import (
     MultiaryAdditionFixer,
     MultiaryMultiplicationFixer,
@@ -26,38 +26,41 @@ from beanmachine.ppl.compiler.fix_normal_conjugate_prior import (
 )
 from beanmachine.ppl.compiler.fix_observations import ObservationsFixer
 from beanmachine.ppl.compiler.fix_observe_true import ObserveTrueFixer
+from beanmachine.ppl.compiler.fix_problem import (
+    GraphFixer,
+    node_fixer_first_match,
+    ancestors_first_graph_fixer,
+)
 from beanmachine.ppl.compiler.fix_requirements import RequirementsFixer
 from beanmachine.ppl.compiler.fix_unsupported import (
-    UnsupportedNodeFixer,
+    unsupported_node_fixer,
     UnsupportedNodeReporter,
 )
 from beanmachine.ppl.compiler.fix_vectorized_models import VectorizedModelFixer
 from beanmachine.ppl.compiler.lattice_typer import LatticeTyper
 
 
-# Some notes on ordering:
-#
-# AdditionFixer needs to run before RequirementsFixer. Why?
-# The requirement fixing pass runs from leaves to roots, inserting
-# conversions as it goes. If we have add(1, negate(p)) then we need
-# to turn that into complement(p), but if we process the add *after*
-# we process the negate(p) then we will already have generated
-# add(1, negate(to_real(p)).  Better to turn it into complement(p)
-# and orphan the negate(p) early.
-#
-# TODO: Add other notes on ordering constraints here.
-
 # TODO[Walid]: Investigate ways to generalize transformations such as MatrixScale to
 # work with multiary multiplications.
 
-_standard_fixer_types: List[Type] = [
+
+def arithmetic_graph_fixer(bmg: BMGraphBuilder, typer: LatticeTyper) -> GraphFixer:
+    node_fixer = node_fixer_first_match(
+        [
+            addition_fixer(bmg, typer),
+            bool_arithmetic_fixer(bmg, typer),
+            bool_comparison_fixer(bmg, typer),
+            matrix_scale_fixer(bmg, typer),
+            unsupported_node_fixer(bmg, typer),
+        ]
+    )
+    return ancestors_first_graph_fixer(bmg, typer, node_fixer)
+
+
+_standard_fixer_types: List[Callable] = [
     VectorizedModelFixer,
-    BoolArithmeticFixer,
-    AdditionFixer,
-    BoolComparisonFixer,
-    UnsupportedNodeFixer,
+    arithmetic_graph_fixer,
     UnsupportedNodeReporter,
-    MatrixScaleFixer,
     MultiaryAdditionFixer,
     LogSumExpFixer,
     MultiaryMultiplicationFixer,
@@ -79,8 +82,9 @@ def fix_problems(
     bmg: BMGraphBuilder, skip_optimizations: Set[str] = default_skip_optimizations
 ) -> ErrorReport:
     bmg._begin(prof.fix_problems)
+
     typer = LatticeTyper()
-    fixer_types: List[Type] = []
+    fixer_types: List[Callable] = []
     for fixer_type in _standard_fixer_types:
         if fixer_type.__name__ not in skip_optimizations:
             fixer_types.append(fixer_type)
@@ -91,9 +95,13 @@ def fix_problems(
     for fixer_type in fixer_types:
         bmg._begin(fixer_type.__name__)
         fixer = fixer_type(bmg, typer)
-        fixer.fix_problems()
+        if hasattr(fixer, "fix_problems"):
+            fixer.fix_problems()
+            errors = fixer.errors
+        else:
+            _, errors = fixer()
         bmg._finish(fixer_type.__name__)
-        errors = fixer.errors
+
         if errors.any():
             break
     bmg._finish(prof.fix_problems)
