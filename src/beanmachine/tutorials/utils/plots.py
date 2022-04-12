aed1e59b1e677d2fd3537ede14a6da2eb8f89780
ndmlny-qs <amaloney@quansight.com>
2022-04-12 09:25:11 -0500

    fix new line lint error

--- src/beanmachine/tutorials/utils/plots.py
+++ src/beanmachine/tutorials/utils/plots.py
@@ -2,6 +2,7 @@
 #
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
+
 """Basic plotting methods used in the tutorials."""
 from itertools import cycle
 from typing import Any, Dict, List, Optional, Tuple, Union
