fd2cb5a0d6c3da135c47ccdf0103b55ce7280d82
Winnie Zhang <chenyun@fb.com>
2022-04-27 15:33:47 -0700

    Change base_single_site_proposer to base_single_site_mh_proposer (#1421)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/beanmachine/pull/1421
    
    Changed the file name `base_single_site_proposer` to `base_single_site_mh_proposer` to avoid confusion, since `base_single_site_proposer` implements the Metropolis-Hastings algorithm
    I also changed a comment in `compositional_infer` since the sentence seemed to have a some grammatical errors in it.
    
    Reviewed By: horizon-blue
    
    Differential Revision: D35941469
    
    fbshipit-source-id: c16846f20256f9f7b592a191bf68b78859522bb2

--- src/beanmachine/ppl/inference/single_site_inference.py
+++ src/beanmachine/ppl/inference/single_site_inference.py
@@ -9,7 +9,7 @@ from beanmachine.ppl.inference.base_inference import BaseInference
 from beanmachine.ppl.inference.proposer.base_proposer import (
     BaseProposer,
 )
-from beanmachine.ppl.inference.proposer.base_single_site_proposer import (
+from beanmachine.ppl.inference.proposer.base_single_site_mh_proposer import (
     BaseSingleSiteMHProposer,
 )
 from beanmachine.ppl.model.rv_identifier import RVIdentifier
