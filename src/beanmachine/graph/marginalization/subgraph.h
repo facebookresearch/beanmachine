cdcce58447fc40288e60b0255d4fd277f89626c9
Lily Li <yucenli@fb.com>
2022-06-30 11:16:42 -0700

    refactor marginalize graph for readability (#1522)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/beanmachine/pull/1522
    
    This diff is to address comments left in Diffs 6-8.
    Due to the big refactor in Diff 8 D34287403, it would have been a lot of work to update the diffs prior to that, since I would need to redo everything for the refactor.
    
    I have gone back to Diffs 6-8 and resolved the comments which I've addressed. I've also copy and pasted all of the relevant comments so it'll be easier to review.
    
    In summary, this diff
    1. splits `marginalize_graph` into many helper functions
    2. removes "COPY" node and instead uses
    a. CONSTANT nodes for parents in the subgraph
    b. SAMPLE nodes for stochastic children of MarginalDistribution
    3. adds a test with upstream parents
    
    Reviewed By: rodrigodesalvobraz
    
    Differential Revision: D34718130
    
    fbshipit-source-id: bce1c75038770c3afd9b1517a4405a8a2f0482b2

--- src/beanmachine/graph/marginalization/subgraph.h
+++ src/beanmachine/graph/marginalization/subgraph.h
@@ -14,10 +14,10 @@ namespace graph {
 class SubGraph : public Graph {
  public:
   explicit SubGraph(Graph& g);
+  std::set<uint> get_node_ids();
   void add_node_by_id(uint node_id);
   bool has_node(uint node_id);
-  void link_copy_node(Node* node, Node* copy_node);
-  void move_nodes_from_graph(uint insertion_index = 0, uint insertion_size = 0);
+  void move_nodes_from_graph_and_reindex();
 
  private:
   Graph& graph;
