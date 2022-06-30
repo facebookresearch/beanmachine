d5fdf75843f2c69d91dd936d284d0207bb00c891
Lily Li <yucenli@fb.com>
2022-06-30 11:15:13 -0700

    add marginalize_graph function and delete MarginalizedGraph class
    Summary: Refactor: MarginalizedGraph class originally contained a copy of the graph. Now that we are modifying the graph itself, it makes more sense for this to be a function rather than a class.
    
    Differential Revision: D34287403
    
    fbshipit-source-id: d2be5465ccdfd6119c91c0bd769b9f51c68460a2

--- src/beanmachine/graph/marginalization/marginalized_graph.cpp
+++ src/beanmachine/graph/marginalization/marginalized_graph.cpp
@@ -15,27 +15,59 @@
 namespace beanmachine {
 namespace graph {
 
-MarginalizedGraph::MarginalizedGraph(Graph& g) : Graph(g) {
-  // iterate over discrete variables and rewrite graph
+void _connect_parent_to_marginal_distribution(
+    distribution::DummyMarginal* node,
+    Node* parent) {
+  // check that parent is not already in list of parents
+  // check that parent is not part of subgraph
+  if ((std::find(node->in_nodes.begin(), node->in_nodes.end(), parent) ==
+       node->in_nodes.end()) and
+      !node->subgraph_ptr.get()->has_node(parent->index)) {
+    node->in_nodes.push_back(parent);
+    parent->out_nodes.push_back(node);
+  }
 }
 
-void MarginalizedGraph::marginalize(uint discrete_sample_node_id) {
-  Node* discrete_sample = get_node(discrete_sample_node_id);
+void _move_children_if(
+    Node* current_parent,
+    Node* new_parent,
+    std::function<bool(Node*)> condition) {
+  // move children from current_parent to new_parent
+  uint i = 0;
+  while (i < current_parent->out_nodes.size()) {
+    Node* child = current_parent->out_nodes[i];
+    // only move children which meet condition
+    if (condition(child)) {
+      // remove child and current_parent connection
+      current_parent->out_nodes.erase(current_parent->out_nodes.begin() + i);
+      auto child_in_nodes_position = child->in_nodes.erase(std::find(
+          child->in_nodes.begin(), child->in_nodes.end(), current_parent));
+      // add child and new_parent connection
+      new_parent->out_nodes.push_back(child);
+      // add new_parent in same place as old_parent
+      child->in_nodes.insert(child_in_nodes_position, new_parent);
+    } else {
+      i++;
+    }
+  }
+}
+
+void marginalize_graph(Graph& g, uint discrete_sample_node_id) {
+  Node* discrete_sample = g.get_node(discrete_sample_node_id);
   Node* discrete_distribution = discrete_sample->in_nodes[0];
 
-  std::unique_ptr<SubGraph> subgraph_ptr = std::make_unique<SubGraph>(*this);
+  std::unique_ptr<SubGraph> subgraph_ptr = std::make_unique<SubGraph>(g);
   SubGraph* subgraph = subgraph_ptr.get();
 
   // list of all additional nodes ptrs to add to `nodes` of current graph
   std::vector<std::unique_ptr<Node>> marginal_distribution_and_copy_chidren;
 
   // compute nodes up to and including stochastic children of discrete_sample
-  std::set<uint> ordered_support_node_ids =
-      compute_full_ordered_support_node_ids();
+  std::set<uint> supp_ids = g.compute_full_ordered_support_node_ids();
   std::vector<uint> det_node_ids;
   std::vector<uint> sto_node_ids;
   std::tie(det_node_ids, sto_node_ids) =
-      compute_children(discrete_sample->index, ordered_support_node_ids);
+      g.compute_children(discrete_sample->index, supp_ids);
 
   // create MarginalizedDistribution
   std::unique_ptr<distribution::DummyMarginal>
@@ -66,22 +98,22 @@ void MarginalizedGraph::marginalize(uint discrete_sample_node_id) {
   // parents for MarginalDistribution
   // add parents of discrete_distribution
   for (Node* discrete_parent : discrete_distribution->in_nodes) {
-    connect_parent_to_marginal_distribution(
+    _connect_parent_to_marginal_distribution(
         marginal_distribution, discrete_parent);
   }
   // add parents of deterministic children
   for (uint det_node_id : det_node_ids) {
-    Node* det_node = get_node(det_node_id);
+    Node* det_node = g.get_node(det_node_id);
     for (Node* det_parent : det_node->in_nodes) {
-      connect_parent_to_marginal_distribution(
+      _connect_parent_to_marginal_distribution(
           marginal_distribution, det_parent);
     }
   }
   // add parents of stochastic children
   for (uint sto_node_id : sto_node_ids) {
-    Node* sto_node = get_node(sto_node_id);
+    Node* sto_node = g.get_node(sto_node_id);
     for (Node* sto_parent : sto_node->in_nodes) {
-      connect_parent_to_marginal_distribution(
+      _connect_parent_to_marginal_distribution(
           marginal_distribution, sto_parent);
     }
   }
@@ -90,11 +122,11 @@ void MarginalizedGraph::marginalize(uint discrete_sample_node_id) {
   // add all children of discrete sample's stochastic children
   for (uint id : sto_node_ids) {
     // create "COPY" of child nodes as output of subgraph
-    Node* node = get_node(id);
+    Node* node = g.get_node(id);
     std::unique_ptr<CopyNode> copy_node = std::make_unique<CopyNode>(node);
     subgraph->link_copy_node(node, copy_node.get());
     // only move children that are not in subgraph
-    move_children_if(node, copy_node.get(), [&](Node* child) {
+    _move_children_if(node, copy_node.get(), [&](Node* child) {
       return !subgraph->has_node(child->index);
     });
     // create link between marginal_distribution and copy_node
@@ -117,7 +149,7 @@ void MarginalizedGraph::marginalize(uint discrete_sample_node_id) {
     std::unique_ptr<CopyNode> copy_node = std::make_unique<CopyNode>(parent);
     subgraph->link_copy_node(parent, copy_node.get());
     // only move children that are in subgraph to copy_node
-    move_children_if(parent, copy_node.get(), [&](Node* child) {
+    _move_children_if(parent, copy_node.get(), [&](Node* child) {
       return subgraph->has_node(child->index);
     });
     subgraph->nodes.push_back(std::move(copy_node));
@@ -127,8 +159,8 @@ void MarginalizedGraph::marginalize(uint discrete_sample_node_id) {
 
   // insert additional nodes at "marginalized_node_index"
   for (uint i = 0; i < marginal_distribution_and_copy_chidren.size(); i++) {
-    nodes.insert(
-        std::next(nodes.begin(), marginal_distribution_index + i),
+    g.nodes.insert(
+        std::next(g.nodes.begin(), marginal_distribution_index + i),
         std::move(marginal_distribution_and_copy_chidren[i]));
   }
   subgraph->move_nodes_from_graph(
@@ -136,41 +168,5 @@ void MarginalizedGraph::marginalize(uint discrete_sample_node_id) {
       marginal_distribution_and_copy_chidren.size());
 }
 
-void MarginalizedGraph::connect_parent_to_marginal_distribution(
-    distribution::DummyMarginal* node,
-    Node* parent) {
-  // check that parent is not already in list of parents
-  // check that parent is not part of subgraph
-  if ((std::find(node->in_nodes.begin(), node->in_nodes.end(), parent) ==
-       node->in_nodes.end()) and
-      !node->subgraph_ptr.get()->has_node(parent->index)) {
-    node->in_nodes.push_back(parent);
-    parent->out_nodes.push_back(node);
-  }
-}
-
-void MarginalizedGraph::move_children_if(
-    Node* current_parent,
-    Node* new_parent,
-    std::function<bool(Node*)> condition) {
-  // move children from current_parent to new_parent
-  uint i = 0;
-  while (i < current_parent->out_nodes.size()) {
-    Node* child = current_parent->out_nodes[i];
-    // only move children which meet condition
-    if (condition(child)) {
-      // remove child and current_parent connection
-      current_parent->out_nodes.erase(current_parent->out_nodes.begin() + i);
-      auto child_in_nodes_position = child->in_nodes.erase(std::find(
-          child->in_nodes.begin(), child->in_nodes.end(), current_parent));
-      // add child and new_parent connection
-      new_parent->out_nodes.push_back(child);
-      // add new_parent in same place as old_parent
-      child->in_nodes.insert(child_in_nodes_position, new_parent);
-    } else {
-      i++;
-    }
-  }
-}
 } // namespace graph
 } // namespace beanmachine
--- src/beanmachine/graph/marginalization/marginalized_graph.h
+++ src/beanmachine/graph/marginalization/marginalized_graph.h
@@ -13,20 +13,7 @@
 namespace beanmachine {
 namespace graph {
 
-class MarginalizedGraph : public Graph {
- public:
-  explicit MarginalizedGraph(Graph& g);
-  void marginalize(uint discrete_node_id);
-
- private:
-  void connect_parent_to_marginal_distribution(
-      distribution::DummyMarginal* node,
-      Node* parent);
-  void move_children_if(
-      Node* current_parent,
-      Node* new_parent,
-      std::function<bool(Node*)> condition);
-};
+void marginalize_graph(Graph& g, uint discrete_sample_node_id);
 
 } // namespace graph
 } // namespace beanmachine
