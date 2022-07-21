5bdc46efd5b6d53afaa965d69c3643721c7a4b66
Himaghna Bhattacharjee <himaghna@fb.com>
2022-07-21 16:53:34 -0700

    Restructure bart_model to move LeafMean and NoiseSD to separate file
    Summary:
    We are implementing Bayesian Additive Regression Trees (BART) and its variants.
    
    In this diff:
    We are refactoring to move ```LeafMean``` and ```NoiseStandardDeviation``` to a new file, ```scalar_samplers```. The associated tests are refactored too. No new logic is implemented.
    
    Why: Some BART variants like xBART reuse LeafMean samplers inside their recursion loop. In such a case, it is useful to decouple the BART model implementation from the samplers since it helps avoid cyclic imports in typing.
    
    Differential Revision: D38060273
    
    fbshipit-source-id: 5e1fcd6834a7b91087a9f76824e9b319528a7c3c

--- src/beanmachine/ppl/experimental/tests/bart/bart_model_test.py
+++ src/beanmachine/ppl/experimental/tests/bart/bart_model_test.py
@@ -8,7 +8,6 @@ import torch
 
 from beanmachine.ppl.experimental.causal_inference.models.bart.bart_model import (
     BART,
-    NoiseStandardDeviation,
     XBART,
 )
 
@@ -16,23 +15,6 @@ from beanmachine.ppl.experimental.causal_inference.models.bart.bart_model import
 @pytest.fixture
 def X():
     return torch.Tensor([[3.0, 1.0], [4.0, 1.0], [1.5, 1.0], [-1.0, 1.0]])
-
-
-@pytest.fixture
-def residual(X):
-    return X * 0.1
-
-
-@pytest.fixture
-def sigma():
-    return NoiseStandardDeviation(prior_concentration=0.1, prior_rate=0.2)
-
-
-def test_sigma_sampling(sigma, X, residual):
-    prev_val = sigma.val
-    sample = sigma.sample(X=X, residual=residual)
-    assert not prev_val == sigma.val
-    assert sigma.val == sample
 
 
 @pytest.fixture
