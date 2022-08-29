58799d62b2e170ec9295e5f647ff08a540746700
Andy Maloney <amaloney@quansight.com>
2022-08-29 16:26:57 -0500

    Add copyright notice

--- src/beanmachine/ppl/diagnostics/tools/typing/marginal1d.py
+++ src/beanmachine/ppl/diagnostics/tools/typing/marginal1d.py
@@ -1,3 +1,8 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
 """Marginal 1D diagnostic tool types for a Bean Machine model."""
 from typing import Any, Dict, List, TypedDict
 
