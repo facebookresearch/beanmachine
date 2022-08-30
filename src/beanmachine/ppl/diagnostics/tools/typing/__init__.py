3d4aa71bc90a4e26351d6450b690681c5090dec7
Andy Maloney <amaloney@quansight.com>
2022-08-30 17:03:30 -0500

    Make flake8 ignore py37 TypedDict unused import

--- src/beanmachine/ppl/diagnostics/tools/typing/__init__.py
+++ src/beanmachine/ppl/diagnostics/tools/typing/__init__.py
@@ -10,4 +10,4 @@ import sys
 if sys.version_info >= (3, 8):
     from typing import TypedDict
 else:
-    from typing_extensions import TypedDict
+    from typing_extensions import TypedDict  # noqa: F401
