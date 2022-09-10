fc6148d514e084bc3a642449e5fec56a2ae95e66
Rodrigo de Salvo Braz <rodrigobraz@fb.com>
2022-09-10 05:02:28 -0700

    NMC against NUTS testing utility (#1649)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/beanmachine/pull/1649
    
    Defines a testing function that runs both NMC and NUTS on a Graph and tests the results for consistency.
    
    Reviewed By: gafter
    
    Differential Revision: D39218567
    
    fbshipit-source-id: 74eb34301259011098de2c425fe9cade61929b1b

--- src/beanmachine/graph/util.h
+++ src/beanmachine/graph/util.h
@@ -198,5 +198,22 @@ Returns the means of samples.
 std::vector<double> compute_means(
     std::vector<std::vector<graph::NodeValue>> samples);
 
+/*
+Runs both NMC and NUTS on given graph for a number of rounds
+with a given number of samples (warmup samples is used only for NUTS),
+and calls a tester function on the means of the first query
+variable obtained by both algorithms.
+The seed is provided as a nullary function (so it can vary across rounds).
+Also prints the obtained means and the measured maximum difference over all
+rounds.
+*/
+void test_nmc_against_nuts(
+    graph::Graph& graph,
+    int num_rounds,
+    int num_samples,
+    int warmup_samples,
+    std::function<unsigned()> seed_getter,
+    std::function<void(double, double)> tester);
+
 } // namespace util
 } // namespace beanmachine
