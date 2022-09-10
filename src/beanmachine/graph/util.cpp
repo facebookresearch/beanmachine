6803a5fa061d39100d99e98a694afd99d2505490
Rodrigo de Salvo Braz <rodrigobraz@fb.com>
2022-09-10 08:50:11 -0700

    NMC against NUTS testing utility (#1649)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/beanmachine/pull/1649
    
    Defines a testing function that runs both NMC and NUTS on a Graph and tests the results for consistency.
    
    Reviewed By: gafter
    
    Differential Revision: D39218567
    
    fbshipit-source-id: 7bc150e64fc7ffd3c16baed979470c6a3f5fa9b9

--- src/beanmachine/graph/util.cpp
+++ src/beanmachine/graph/util.cpp
@@ -9,7 +9,12 @@
 #include <cmath>
 
 #include <boost/math/special_functions/polygamma.hpp>
+#include <stdexcept>
+
+#include "beanmachine/graph/global/nuts.h"
+
 #include <Eigen/Core>
+#include <iostream>
 #include "beanmachine/graph/graph.h"
 #include "beanmachine/graph/util.h"
 
@@ -166,6 +171,46 @@ std::vector<double> compute_means(
     means[i] = compute_mean_at_index(samples, i);
   }
   return means;
+}
+
+void test_nmc_against_nuts(
+    graph::Graph& graph,
+    int num_rounds,
+    int num_samples,
+    int warmup_samples,
+    std::function<unsigned()> seed_getter,
+    std::function<void(double, double)> tester) {
+  using namespace std;
+  if (graph.queries.empty()) {
+    throw invalid_argument(
+        "test_nmc_against_nuts requires at least one query in graph.");
+  }
+  auto measured_max_abs_mean_diff = 0.0;
+  for (int i = 0; i != num_rounds; i++) {
+    auto seed = seed_getter();
+
+    auto means_nmc =
+        graph.infer_mean(num_samples, graph::InferenceType::NMC, seed);
+
+    graph::NUTS nuts = graph::NUTS(graph);
+    auto samples = nuts.infer(num_samples, seed, warmup_samples);
+    auto means_nuts = compute_means(samples);
+
+    assert(!means_nmc.empty());
+    assert(!means_nuts.empty());
+
+    tester(means_nmc[0], means_nuts[0]);
+
+    auto abs_diff = std::abs(means_nmc[0] - means_nuts[0]);
+    if (abs_diff > measured_max_abs_mean_diff) {
+      measured_max_abs_mean_diff = abs_diff;
+    }
+
+    cout << "NMC  result: " << means_nmc[0] << endl;
+    cout << "NUTS result: " << means_nuts[0] << endl;
+  }
+  cout << "Measured max absolute difference: " << measured_max_abs_mean_diff
+       << endl;
 }
 
 } // namespace beanmachine::util
