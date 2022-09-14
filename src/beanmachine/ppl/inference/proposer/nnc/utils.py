1bf419ce94dbcf75e70c02257d2a20f85c3a210d
Xiaoyan Wang <xiaoyan0@fb.com>
2022-09-14 14:13:50 -0700

    Make RVIdentifier sortable (for now) (#1667)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/beanmachine/pull/1667
    
    As reported in [#1665](https://github.com/facebookresearch/beanmachine/issues/1665), when trying to compile with NNC, functorch (v2.1.0) will attempt to sort [the keys of a dictionary before flattening it](https://github.com/pytorch/functorch/blob/v0.2.1/functorch/_src/aot_autograd.py#L42), where it will raise an exception because Bean Machine's RVIdentifier type is not sortable.
    
    This sorting of dictionary keys is removed in the latest functorch, though until then, as a workaround, we can make `RVIdentifier` sortable for now by defining a `__lt__` operator for it.
    
    Reviewed By: feynmanliang
    
    Differential Revision: D39486353
    
    fbshipit-source-id: b356d1799767e8a0719693b9b63835f09fd158a9

--- src/beanmachine/ppl/inference/proposer/nnc/utils.py
+++ src/beanmachine/ppl/inference/proposer/nnc/utils.py
@@ -7,7 +7,6 @@ import warnings
 
 import torch
 import torch.jit
-import torch.utils._pytree as pytree
 from functorch.compile import nnc_jit
 
 
@@ -22,18 +21,5 @@ warnings.warn(
 # pyre-fixme[16]: Module `_C` has no attribute `_jit_set_texpr_reductions_enabled`.
 torch._C._jit_set_texpr_reductions_enabled(True)
 
-
-# override default dict flatten (which requires keys to be sortable)
-def _dict_flatten(d):
-    keys = list(d.keys())
-    values = [d[key] for key in keys]
-    return values, keys
-
-
-def _dict_unflatten(values, context):
-    return {key: value for key, value in zip(context, values)}
-
-
-pytree._register_pytree_node(dict, _dict_flatten, _dict_unflatten)
 
 __all__ = ["nnc_jit"]
