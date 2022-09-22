e0deb95cb5c9b262317605b3d9834dff8fc6a25c
Neal Gafter <nmgafter@meta.com>
2022-09-22 16:48:34 -0700

    minibmg: fluent graph factory (#1693)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/beanmachine/pull/1693
    
    The is a much more convenient way for creating graphs in C++.
    
    I think of it as the Bean Machine language implemented on top of C++.
    
    Differential Revision: https://internalfb.com/D39733618
    
    fbshipit-source-id: cb71998a8e75ce9b4ff2e323991a322717ca2122

---
+++ minibmg/fluent_factory.h
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#pragma once
+
+#include "beanmachine/minibmg/ad/traced.h"
+#include "beanmachine/minibmg/graph.h"
+#include "beanmachine/minibmg/node.h"
+
+namespace beanmachine::minibmg {
+
+// For values
+using Value = Traced;
+
+// For distributions
+class Distribution {
+ public:
+  Nodep node;
+  /* implicit */ Distribution(Nodep node) : node{node} {
+    if (node->type != Type::DISTRIBUTION) {
+      throw std::invalid_argument("node is not a value");
+    }
+  }
+  inline Operator op() const {
+    return node->op;
+  }
+};
+
+Distribution normal(Value mean, Value stddev);
+
+Distribution beta(Value a, Value b);
+
+Distribution bernoulli(Value p);
+
+Value sample(const Distribution& d);
+
+class Graph::FluentFactory {
+ public:
+  void observe(const Traced& sample, double value);
+  void query(const Traced& value);
+  Graph build();
+
+ private:
+  std::vector<Nodep> queries;
+  std::list<std::pair<Nodep, double>> observations;
+};
+
+} // namespace beanmachine::minibmg
