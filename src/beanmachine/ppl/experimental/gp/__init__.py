f5326f5ee1fe20c0f32af4ff25eb3f5c5a3b3013
Feynman Tsing-Yang Liang <feynmanl@meta.com>
2022-10-04 08:44:41 -0700

    beanmachine + gpytorch._priors integration (#1708)
    Summary:
    Adds two utilites to integrate beanmachine with gpytorch in a manner analgous to pyro's integration (in fact, we use their `.to_pyro_random_module` and `load_from_pyro_samples` APIs).
    
    Also switches GP inference to GlobalNUTS; we still cannot enable NNC unless T132914532 yields a positive result.
    
    Updates tutorials and tests.
    
    Pull Request resolved: https://github.com/facebookresearch/beanmachine/pull/1708
    
    Differential Revision: D39871962
    
    fbshipit-source-id: 7b255a09e6ff57dc3960c44e08f01588c4568c85

--- src/beanmachine/ppl/experimental/gp/__init__.py
+++ src/beanmachine/ppl/experimental/gp/__init__.py
@@ -3,8 +3,74 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-from beanmachine.ppl.experimental.gp.kernels import all_kernels
-from beanmachine.ppl.experimental.gp.likelihoods import all_likelihoods
+import beanmachine.ppl as bm
+import gpytorch
+from beanmachine.ppl.world import get_world_context
 
 
-__all__ = all_likelihoods + all_kernels
+def _trace_bm(module, name_to_rv=None, is_tracing=True, memo=None, prefix=""):
+    if memo is None:
+        memo = set()
+    if name_to_rv is None:
+        name_to_rv = {}
+    if hasattr(module, "_priors"):
+        for prior_name, (prior, closure, setting_closure) in module._priors.items():
+            if prior is not None and prior not in memo:
+                if setting_closure is None:
+                    raise RuntimeError(
+                        "Cannot perform fully Bayesian inference without a setting_closure for each prior,"
+                        f" but the following prior had none: {prior_name}, {prior}."
+                    )
+                memo.add(prior_name)
+                prior = prior.expand(closure(module).shape)
+                rv_name = prefix + ("." if prefix else "") + prior_name
+                if is_tracing:
+                    # tracing pass, no enclosing World
+                    def f():
+                        return prior
+
+                    f.__name__ = rv_name
+                    rv = bm.random_variable(f)
+                    name_to_rv[rv_name] = rv()
+                else:
+                    # sampling pass, must be enclosed by World
+                    world = get_world_context()
+                    assert (
+                        world is not None
+                    ), "Expected enclosing World context for bm.random_variable priors"
+                    value = world.update_graph(name_to_rv[rv_name])
+                    setting_closure(module, value)
+
+    for mname, module_ in module.named_children():
+        submodule_prefix = prefix + ("." if prefix else "") + mname
+        _, child_name_to_rv = _trace_bm(
+            module=module_,
+            name_to_rv=name_to_rv,
+            is_tracing=is_tracing,
+            memo=memo,
+            prefix=submodule_prefix,
+        )
+        name_to_rv.update(child_name_to_rv)
+
+    return module, name_to_rv
+
+
+def make_prior_random_variables(
+    module,
+    name_to_rv=None,
+    memo=None,
+    prefix="",
+):
+    """
+    Recurses through `module` and its childrens' `._priors`, creating `bm.random_variable`s
+    for each prior. Returns a map from prior names to `random_variable`s.
+    """
+    return _trace_bm(module, name_to_rv=None, is_tracing=True)[1]
+
+
+def bm_sample_from_prior(model, name_to_rv) -> gpytorch.module.Module:
+    """
+    Samples from `model` with parameters drawn by invoking the
+    `random_variable` to their prior in `name_to_rv`.
+    """
+    return _trace_bm(model, name_to_rv, is_tracing=False)[0]
--- src/beanmachine/ppl/experimental/gp/models.py
+++ src/beanmachine/ppl/experimental/gp/models.py
@@ -3,7 +3,6 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-import beanmachine.ppl as bm
 import gpytorch as gpt
 import torch
 from botorch.models.gpytorch import GPyTorchModel
@@ -22,38 +21,14 @@ class SimpleGP(gpt.models.ExactGP, GPyTorchModel):
         super().__init__(x_train, y_train, likelihood)
         self.mean = mean
         self.kernel = kernel
-        self.likelihood = likelihood
-        self.bm_forward = bm.random_variable(self.forward)
-
-    def __call__(self, *args, **kwargs):
-        if self.training:
-            return self.bm_forward(*args, **kwargs)
-        return super().__call__(*args, **kwargs)
 
     def forward(self, data, *args, **kwargs):
         """
         Default forward definining a GP prior. Should be overridden by child class.
         """
-        if self.training:
-            mean = self.mean().expand(data.shape[:-1])
-        else:
-            mean = self.mean.expand(data.shape[:-1])
+        mean = self.mean(data)
         cov = self.kernel(data)
         return gpt.distributions.MultivariateNormal(mean, cov)
-
-    def train(self, mode=True):
-        """
-        In train mode, calling this module returns a BM random variable of
-        the GP prior. In eval mode, the module's methods default to the
-        function signature of the parent class `~gpytorch.models.ExactGP`.
-        """
-        if mode:
-            [x.train() for x in self.children()]
-            super().train()
-        else:
-            # convert kernels or models to eval mode
-            [x.eval() for x in self.children()]
-            super().train(False)
 
     def bm_load_samples(self, rv_dict):
         """
@@ -63,30 +38,20 @@ class SimpleGP(gpt.models.ExactGP, GPyTorchModel):
         :param rv_dict: Python dict keyed on module name and valued by tensor
         whose shape is (num_samples, sample_shape)
         """
-        # load the kernel parameters
-        self.initialize(**rv_dict)
-        # broadcast train and targets with num_samples
-        num_samples = next(iter(rv_dict.values())).shape[0]
-        self.train_inputs = tuple(
-            tri.unsqueeze(0).expand(num_samples, *tri.shape)
-            for tri in self.train_inputs
-        )
-        self.train_targets = self.train_targets.unsqueeze(0).expand(
-            num_samples, *self.train_targets.shape
-        )
+        self.pyro_load_from_samples(rv_dict)
 
 
 class BoTorchGP(SimpleGP, GPyTorchModel):
     """
     Experimental module that is compatible with BoTorch.
 
-       samples = nuts.infer(queries, obs, num_samples).get_chain(0)
-       gp.eval()
-       gp.bm_load_samples({kernel.lengthscale=samples[lengthscale_prior()]})
+    samples = nuts.infer(queries, obs, num_samples).get_chain(0)
+    gp.eval()
+    gp.bm_load_samples({kernel.lengthscale=samples[lengthscale_prior()]})
 
-       from botorch.acquisition.objective import IdentityMCObjective
-       acqf = get_acquisition_function("qEI", gp, IdentityMCObjective(), x_train)
-       new_point = acqf(new_input).mean()
+    from botorch.acquisition.objective import IdentityMCObjective
+    acqf = get_acquisition_function("qEI", gp, IdentityMCObjective(), x_train)
+    new_point = acqf(new_input).mean()
     """
 
     def __init__(self, x_train, y_train, *args, **kwargs):
@@ -113,6 +78,7 @@ class BoTorchGP(SimpleGP, GPyTorchModel):
                 "Running in eval mode but one of the parameters is still"
                 "a BM random variable. Did you `bm_load_samples`? \n" + str(e)
             )
+
         if observation_noise is not False:
             if torch.is_tensor(observation_noise):
                 # TODO: Make sure observation noise is transformed correctly
